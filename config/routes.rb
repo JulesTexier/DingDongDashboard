bee6f15213a89beb07b2aae5ee44ed4d35c1c99b
fr3d100 <f.bonnand@gmail.com>
2020-03-09 16:42:49 +0100

    Format file

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => "/api-docs"
+  mount Rswag::Api::Engine => "/api-docs"
 
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
+  root "static#home"
 
-  root 'static#home'
-
-  namespace 'api' do 
-    namespace 'v1' do 
-      get '/subscribers/fb/:facebook_id' => 'subscribers#show_facebook_id'
-      resources :subscribers do 
-        get '/get/props/last/:x/days' => 'subscribers#props_x_days'
+  namespace "api" do
+    namespace "v1" do
+      get "/subscribers/fb/:facebook_id" => "subscribers#show_facebook_id"
+      resources :subscribers do
+        get "/get/props/last/:x/days" => "subscribers#props_x_days"
         # get '/send/props/last/:x/days' => 'subscribers#send_props_x_days'
         # get '/send/props/morning' => 'subscribers#send_props_morning'
         # get '/send/props/:property_id/details' => 'subscribers#send_prop_details'
@@ -21,13 +20,12 @@ Rails.application.routes.draw do
       resources :favorites, only: [:create, :destroy]
 
       # Manychat routes
-      post '/manychat/s/:subscriber_id/update' => 'manychat#update_subscriber'
-      get '/manychat/s/:subscriber_id/send/props/last/:x/days' => 'manychat#send_props_x_days'
-      get '/manychat/s/:subscriber_id/send/props/morning' => 'manychat#send_props_morning'
-      get '/manychat/s/:subscriber_id/send/props/:property_id/details' => 'manychat#send_prop_details'
-      get '/manychat/s/:subscriber_id/send/props/favorites' => 'manychat#send_props_favorites'
-      get '/manychat/s/:subscriber_id/send/last/:x/props' => 'manychat#get_x_last_props'
-
+      post "/manychat/s/:subscriber_id/update" => "manychat#update_subscriber"
+      get "/manychat/s/:subscriber_id/send/props/last/:x/days" => "manychat#send_props_x_days"
+      get "/manychat/s/:subscriber_id/send/props/morning" => "manychat#send_props_morning"
+      get "/manychat/s/:subscriber_id/send/props/:property_id/details" => "manychat#send_prop_details"
+      get "/manychat/s/:subscriber_id/send/props/favorites" => "manychat#send_props_favorites"
+      get "/manychat/s/:subscriber_id/send/last/:x/props" => "manychat#get_x_last_props"
     end
   end
 
