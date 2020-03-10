c81230637ad8d9690df62873f8ca10b5b27a6154
Segrelove <mlesegret@gmail.com>
2020-03-10 15:52:12 +0100

    we know scrap every links of figarimmo

--- app/services/scrapers/scraper_figaro.rb
+++ app/services/scrapers/scraper_figaro.rb
@@ -13,7 +13,12 @@ class ScraperFigaro < Scraper
     xml.each do |item|
       begin
         hashed_property = {}
-        hashed_property[:link] = "https://immobilier.lefigaro.fr" + access_xml_link(item, "a.js-link-ei", "href")[0].to_s
+        link = access_xml_link(item, "a.js-link-ei", "href")[0].to_s
+        if link.to_s.strip.empty?
+          hashed_property[:link] = access_xml_link(item, "a.js-link-plf", "href")[0].to_s
+        else
+          hashed_property[:link] = "https://immobilier.lefigaro.fr" + link
+        end
         hashed_property[:surface] = regex_gen(access_xml_text(item, "h2"), '(\d+(,?)(\d*))(.)(m)').to_float_to_int_scrp
         hashed_property[:area] = access_xml_text(item, "h2 > a > span").area_translator_scrp
         hashed_property[:rooms_number] = regex_gen(access_xml_text(item, "h2"), '(\d+)(.?)(pi(è|e)ce(s?))').to_float_to_int_scrp
@@ -21,7 +26,7 @@ class ScraperFigaro < Scraper
         hashed_properties.push(extract_each_flat(hashed_property)) if is_property_clean(hashed_property)
       rescue StandardError => e
         puts "\nError for #{@source}, skip this one."
-        puts "It could be a bad link, a bad xml extraction or a new website unscrappable yet.\n\n"
+        puts "It could be a bad link or a bad xml extraction\n\n"
         next
       end
     end
@@ -32,21 +37,34 @@ class ScraperFigaro < Scraper
 
   def extract_each_flat(prop)
     flat_data = {}
-    html = fetch_static_page(prop[:link])
-    flat_data[:link] = prop[:link]
     flat_data[:surface] = prop[:surface]
     flat_data[:area] = prop[:area]
     flat_data[:rooms_number] = prop[:rooms_number]
     flat_data[:price] = prop[:price]
-    flat_data[:bedrooms_number] = regex_gen(access_xml_array_to_text(html, "ul.unstyled.flex").specific_trim_scrp("\n\r\t"), '(\d+)(.?)(chambre(s?))').to_int_scrp
-    flat_data[:description] = access_xml_text(html, "p.description.js-description").specific_trim_scrp("\n").strip
-    flat_data[:flat_type] = regex_gen(access_xml_text(html, "div.container-h1 > h1"), "((a|A)ppartement|(A|a)ppartements|(S|s)tudio|(S|s)tudette|(C|c)hambre|(M|m)aison)")
-    flat_data[:agency_name] = access_xml_text(html, "span.agency-name")
-    flat_data[:floor] = perform_floor_regex(flat_data[:description])
-    flat_data[:has_elevator] = perform_elevator_regex(flat_data[:description])
-    flat_data[:provider] = "Agence"
-    flat_data[:source] = @source
-    flat_data[:images] = access_xml_link(html, "a.image-link.default-image-background", "href")
+    flat_data[:link] = prop[:link]
+    if flat_data[:link].include?("https://proprietes.lefigaro.fr")
+      html = fetch_static_page(flat_data[:link])
+      flat_data[:bedrooms_number] = regex_gen(access_xml_array_to_text(html, "ul").tr("\n\r\t", ""), '(\d+)(.?)(chambre(s?))').to_int_scrp
+      flat_data[:description] = access_xml_text(html, "p#js-description").specific_trim_scrp("\n").strip
+      flat_data[:flat_type] = regex_gen(access_xml_text(html, "div.main-title-annonce > h1 > span"), "((a|A)ppartement|(A|a)ppartements|(S|s)tudio|(S|s)tudette|(C|c)hambre|(M|m)aison)")
+      flat_data[:agency_name] = access_xml_text(html, "span.societe.js-societe").tr("\n\t", "")
+      flat_data[:floor] = perform_floor_regex(flat_data[:description])
+      flat_data[:has_elevator] = perform_elevator_regex(flat_data[:description])
+      flat_data[:provider] = "Agence"
+      flat_data[:source] = @source
+      flat_data[:images] = access_xml_link(html, "#js-picture-main", "src")
+    else
+      html = fetch_static_page(flat_data[:link])
+      flat_data[:bedrooms_number] = regex_gen(access_xml_array_to_text(html, "ul.unstyled.flex").specific_trim_scrp("\n\r\t"), '(\d+)(.?)(chambre(s?))').to_int_scrp
+      flat_data[:description] = access_xml_text(html, "p.description.js-description").specific_trim_scrp("\n").strip
+      flat_data[:flat_type] = regex_gen(access_xml_text(html, "div.container-h1 > h1"), "((a|A)ppartement|(A|a)ppartements|(S|s)tudio|(S|s)tudette|(C|c)hambre|(M|m)aison)")
+      flat_data[:agency_name] = access_xml_text(html, "span.agency-name").tr("\n\t", "")
+      flat_data[:floor] = perform_floor_regex(flat_data[:description])
+      flat_data[:has_elevator] = perform_elevator_regex(flat_data[:description])
+      flat_data[:provider] = "Agence"
+      flat_data[:source] = @source
+      flat_data[:images] = access_xml_link(html, "a.image-link.default-image-background", "href")
+    end
     return flat_data
   end
 end
