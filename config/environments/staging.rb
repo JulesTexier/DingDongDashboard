7fd2578c2084bd1e4609e479f5c4670d125d9d84
fr3d100 <f.bonnand@gmail.com>
2020-03-11 07:46:32 +0100

    Remove staging env

--- config/environments/staging.rb
+++
@@ -1,7 +0,0 @@
-# Just use the production settings
-require File.expand_path('../production.rb', __FILE__)
-
-Rails.application.configure do
-  # Here override any defaults
-  config.serve_static_files = true
-end
