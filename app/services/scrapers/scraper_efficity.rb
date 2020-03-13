de3edde4edbb30b559905e0ebd392c12cb351fe2
Segrelove <mlesegret@gmail.com>
2020-03-13 13:01:23 +0100

    new efficity scraper, good work fredo

--- app/services/scrapers/scraper_efficity.rb
+++ app/services/scrapers/scraper_efficity.rb
@@ -13,11 +13,11 @@ class ScraperEfficity < Scraper
     xml.each do |item|
       begin
         hashed_property = {}
-        
+
         hashed_property[:link] = "https://www.efficity.com" + access_xml_link(item, "a", "href")[0].to_s
         hashed_property[:surface] = regex_gen(item.text, '(\d+(,?)(\d*))(.)(m)').to_float_to_int_scrp
-        hashed_property[:area] = regex_gen(access_xml_text(item,'a > figcaption > h3 > span > span'), '(75)$*\d+{3}')
-        hashed_property[:price] = regex_gen(access_xml_text(item, 'a > div > div > strong > span'), '(\d)(.*)(€)').to_int_scrp
+        hashed_property[:area] = regex_gen(access_xml_text(item, "a > figcaption > h3 > span > span"), '(75)$*\d+{3}')
+        hashed_property[:price] = regex_gen(access_xml_text(item, "a > div > div > strong > span"), '(\d)(.*)(€)').to_int_scrp
         hashed_properties.push(extract_each_flat(hashed_property)) if is_property_clean(hashed_property)
       rescue StandardError => e
         puts "\nError for #{@source}, skip this one."
@@ -25,7 +25,6 @@ class ScraperEfficity < Scraper
         next
       end
     end
-    puts "*"*10
     enrich_then_insert(hashed_properties)
   end
 
@@ -37,11 +36,11 @@ class ScraperEfficity < Scraper
     flat_data[:link] = prop[:link]
     flat_data[:surface] = prop[:surface]
     flat_data[:area] = prop[:area]
-    flat_data[:rooms_number] = regex_gen(access_xml_text(html,'#nom-bien'), '(\d+)(.?)(pi(è|e)ce(s?))').to_float_to_int_scrp
+    flat_data[:rooms_number] = regex_gen(access_xml_text(html, "#nom-bien"), '(\d+)(.?)(pi(è|e)ce(s?))').to_float_to_int_scrp
     flat_data[:price] = prop[:price]
     flat_data[:bedrooms_number] = regex_gen(access_xml_text(html, ".resume-picto"), '(\d+)(.?)(chambre(s?))').to_int_scrp
     flat_data[:description] = access_xml_text(html, "div.detail-desc-text").strip
-    flat_data[:flat_type] = access_xml_text(html,'#nom-bien').split('|')[0].tr(' ','')
+    flat_data[:flat_type] = access_xml_text(html, "#nom-bien").split("|")[0].tr(" ", "")
     flat_data[:agency_name] = "Efficity"
     flat_data[:floor] = perform_floor_regex(flat_data[:description])
     flat_data[:has_elevator] = perform_elevator_regex(flat_data[:description])
@@ -49,7 +48,7 @@ class ScraperEfficity < Scraper
     flat_data[:provider] = "Agence"
     flat_data[:source] = @source
     flat_data[:images] = access_xml_link(html, ".cbp-lightbox", "href")
-    flat_data[:images].collect! {|img| img.clean_img_link_https}
+    flat_data[:images].collect! { |img| img.clean_img_link_https }
     return flat_data
   end
 end
