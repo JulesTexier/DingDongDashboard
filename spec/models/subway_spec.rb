64eb4c88d6c9a6263121dd685b5c4bf956682462
fr3d100 <f.bonnand@gmail.com>
2020-03-17 11:15:46 +0100

    Init first Subway model testing

--- spec/models/subway_spec.rb
+++ spec/models/subway_spec.rb
@@ -1,5 +1,21 @@
-# require 'rails_helper'
+require 'rails_helper'
+require 'json'
 
-# RSpec.describe Subway, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Subway, type: :model do
+
+    before(:each) do 
+        @subways = []
+        obj = JSON.parse(File.read("./db/data/subways.json"))
+        obj["stations"].each do |hsh|
+            @subways.push(FactoryBot.create(:subway, name: hsh["metro"], line: hsh["ligne"]))
+        end
+    end
+
+    it "is valid with valid attributes" do
+        expect(@subways[0]).to be_a(Subway)
+        expect(@subways[0]).to be_valid
+        puts @subways[0].id
+    end
+  
+
+end
