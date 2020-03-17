fe449f352ec5ac1321c51038a2de904d09785b1d
fr3d100 <f.bonnand@gmail.com>
2020-03-17 18:36:54 +0100

    Updated Subscriber model tests : testing all is_matching_property? scenarii

--- spec/models/subscriber_spec.rb
+++ spec/models/subscriber_spec.rb
@@ -35,6 +35,59 @@ RSpec.describe Subscriber, type: :model do
         end 
       end
 
+    context "Property matching"
+      subscriber =  FactoryBot.build(:subscriber, max_price: 500000, min_surface: 20, min_rooms_number: 1, min_floor: 2, min_elevator_floor: 4)
+      subscriber.areas << Area.new(name: "75010")
+      property = FactoryBot.build(:property, price: subscriber.max_price, surface: subscriber.min_surface, area: subscriber.areas.first.name, rooms_number: subscriber.min_rooms_number, floor: nil, has_elevator: nil )
+
+      it 'should match user and property !' do
+        expect(subscriber.is_matching_property?(property)).to eq(true)
+      end
+
+      it 'should NOT match user and property because of surface !' do
+        property.surface = subscriber.min_surface - 1
+        expect(subscriber.is_matching_property?(property)).to eq(false)
+      end
+      it 'should NOT match user and property because of area !' do
+        property.area = "75001"
+        expect(subscriber.is_matching_property?(property)).to eq(false)
+      end
+      it 'should NOT match user and property because of rooms_number !' do
+        property.area = subscriber.min_rooms_number - 1
+        expect(subscriber.is_matching_property?(property)).to eq(false)
+      end
+      it 'should NOT match user and property because of price !' do
+        property.price = subscriber.max_price + 1
+        expect(subscriber.is_matching_property?(property)).to eq(false)
+      end
+
+      # Floor constraints
+      it 'should match user and property (known floor)' do
+        property = FactoryBot.build(:property, price: subscriber.max_price, surface: subscriber.min_surface, area: subscriber.areas.first.name, rooms_number: subscriber.min_rooms_number, floor: subscriber.min_floor, has_elevator: true )
+        expect(subscriber.is_matching_property?(property)).to eq(true)
+      end
+      it 'should NOT match user and property because of floor !' do
+        property.floor = subscriber.min_floor - 1
+        expect(subscriber.is_matching_property?(property)).to eq(false)
+      end
+
+      # Elevator constraints
+      it 'should match user and property (known elevator presence)' do
+        property = FactoryBot.build(:property, price: subscriber.max_price, surface: subscriber.min_surface, area: subscriber.areas.first.name, rooms_number: subscriber.min_rooms_number, floor: subscriber.min_elevator_floor, has_elevator: true )
+        expect(subscriber.is_matching_property?(property)).to eq(true)
+      end
+      it 'should match user and property (known elevator abscence but min_elevator_floor <)' do
+        property.has_elevator = false
+        property.floor = subscriber.min_elevator_floor - 1
+        expect(subscriber.is_matching_property?(property)).to eq(true)
+      end
+      it 'should NOT match user and property (known elevator absence)' do
+        property.has_elevator = false
+        property.floor = subscriber.min_elevator_floor
+        expect(subscriber.is_matching_property?(property)).to eq(false)
+      end
+
+      
     end
 
 end
