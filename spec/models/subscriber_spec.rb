cff23b4cda424922cca9687e29b5fdf5b6bb8c06
fr3d100 <f.bonnand@gmail.com>
2020-03-17 16:34:46 +0100

    Test about validations, associations and default values have been written

--- spec/models/subscriber_spec.rb
+++ spec/models/subscriber_spec.rb
@@ -2,24 +2,37 @@ require 'rails_helper'
 
 RSpec.describe Subscriber, type: :model do
 
-    before(:each) do 
-      @subscriber = FactoryBot.create(:subscriber)
-    end
-
-    context "validation" do
-
-      it "is valid with valid attributes" do
-        expect(@subscriber).to be_a(Subscriber)
-        expect(@subscriber).to be_valid
+    describe Subscriber do 
+      it "has a valid factory" do
+        expect(build(:subscriber)).to be_valid
       end
-  
-      describe "#facebook_id" do
-        it "should not be valid without facebook_id" do
-          bad_user = FactoryBot.build(:subscriber, facebook_id: nil)
-          expect(bad_user).not_to be_valid
-          # test très sympa qui permet de vérifier que la fameuse formule user.errors retourne bien un hash qui contient une erreur concernant le first_name. 
-          expect(bad_user.errors.include?(:facebook_id)).to eq(true)
+
+      context "validations" do
+        before { FactoryBot.build(:subscriber) }
+        it do 
+          should validate_uniqueness_of(:facebook_id).case_insensitive
+          should validate_presence_of(:facebook_id)
+          should validate_presence_of(:firstname)
+          should validate_presence_of(:email)
+          should validate_presence_of(:phone)
+        end 
+      end
+
+      context "default values" do 
+        it 'should define user as active by default' do 
+          subscriber = FactoryBot.create(:subscriber)
+          expect(subscriber.is_active).to eq(true)
+          expect(subscriber.min_floor).to eq(0)
         end
+      end
+
+      context "associations" do
+        before { FactoryBot.build(:subscriber) }
+        it do 
+          should have_many(:fav_properties).through(:favorites).class_name('Property')
+          should have_many(:areas).through(:selected_areas).class_name('Area')
+          should have_many(:districts).through(:selected_districts).class_name('District')
+        end 
       end
 
     end
