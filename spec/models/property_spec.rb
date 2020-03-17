10da8b687ce8ee425b0b9927f392b602c3cd28e2
fr3d100 <f.bonnand@gmail.com>
2020-03-17 11:57:27 +0100

    Init property testing

--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -1,28 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Property, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+	before(:each) do 
+		@property = FactoryBot.create(:property)
+	end
 
-    before(:each) do 
-      @property = FactoryBot.create(:property)
-    end
+	it "has a valid factory" do
+		expect(build(:property)).to be_valid
+	end
+		
+	context "validation" do
 
-    context "validation" do
-
-      it "is valid with valid attributes" do
-        expect(@property).to be_a(Property)
-        expect(@property).to be_valid
-      end
-  
-    #   describe "#source" do
-    #     it "should not be valid without source" do
-    #       bad_property = FactoryBot.build(:property, source: nil)
-    #       expect(bad_property).not_to be_valid
-          
-    #       # test très sympa qui permet de vérifier que la fameuse formule user.errors retourne bien un hash qui contient une erreur concernant le first_name. 
-    #       expect(bad_user.errors.include?(:property)).to eq(true)
-    #     end
-    #   end
-
-    end
+			it "is valid with valid attributes" do
+				expect(@property).to be_a(Property)
+			end
+	
+			describe "#price" do
+				it { expect(@property).to validate_presence_of(:price) }
+			end
+	
+			describe "#surface" do
+			it { expect(@property).to validate_presence_of(:surface) }
+			end
+	
+			describe "#rooms_number" do
+			it { expect(@property).to validate_presence_of(:rooms_number) }
+			end
+	
+		end
 
 end
--- test/factories/properties.rb
+++ test/factories/properties.rb
@@ -2,11 +2,9 @@ FactoryBot.define do
   factory :property do
     price { 600000 }
     surface { 60 }
-    rooms_number { 1 }
-    area { "75001" }
+    rooms_number { 1 }
+    area { "75010" }
+    source { "Ding Dong" }
     link { "https://hellodingdong.com" }
-    source { "DingDong" }
-    floor { 1 }
-    has_elevator { true }
   end
 end
