1472f9d6da0a89c2b597758449a010005d502a58
fr3d100 <f.bonnand@gmail.com>
2020-03-19 10:30:53 +0100

    Write down tests for update_subscriber method

--- spec/controllers/api/v1/manychat_controller_spec.rb
+++ spec/controllers/api/v1/manychat_controller_spec.rb
@@ -2,21 +2,54 @@ require "rails_helper"
 require "dotenv/load"
 
 RSpec.describe Api::V1::ManychatController, type: :controller do
-  describe "A subscriber should receive his favorites" do
-    context "#send_favorites" do
-      before :all do
-        @sub = FactoryBot.create(:subscriber_fred)
-      end
-      it "should contain an authorization token" do
-        get :send_props_favorites, params: { subscriber_id: @sub.id }
-        expect(response.body).to eq("HTTP Token: Access denied.\n")
-      end
 
-      it "should have 200 https status" do
-        request.headers.merge!({ 'Authorization': "Bearer " + ENV["BEARER_TOKEN"] })
-        get :send_props_favorites, params: { subscriber_id: @sub.id }
-        expect(response).to have_http_status(:success)
+  describe "Subscriber operations" do 
+    describe "Update subscriber atributes form manychat" do 
+      context "#update_subscriber" do 
+        before :all do
+          @sub = FactoryBot.create(:subscriber)
+        end
+        it "should contain an authorization token" do
+          post :update_subscriber, params: {subscriber_id: @sub.id}
+          expect(response.body).to eq("HTTP Token: Access denied.\n")
+        end
+        it "should have 200 https status" do
+          request.headers.merge!({ 'Authorization': "Bearer " + ENV["BEARER_TOKEN"] })
+          post :update_subscriber, params: {subscriber_id: @sub.id}
+          expect(response).to have_http_status(:success)
+        end
+        it "should respond 404 status id subscriber is not found" do
+          request.headers.merge!({ 'Authorization': "Bearer " + ENV["BEARER_TOKEN"] })
+          post :update_subscriber, params: {subscriber_id: 9999}
+          expect(response).to have_http_status(404)
+        end
+        it "update subscriber firstname" do
+          request.headers.merge!({ 'Authorization': "Bearer " + ENV["BEARER_TOKEN"] })
+          sub = @sub
+          sub.firstname = 'Paul'
+          post :update_subscriber, params: {subscriber_id: @sub.id, subscriber: sub}
+          expect(@sub.firstname).to eq('Paul')
+        end
       end
     end
   end
+
+
+  # describe "A subscriber should receive his favorites" do
+  #   context "#send_favorites" do
+  #     before :all do
+  #       @sub = FactoryBot.create(:subscriber_fred)
+  #     end
+  #     it "should contain an authorization token" do
+  #       get :send_props_favorites, params: { subscriber_id: @sub.id }
+  #       expect(response.body).to eq("HTTP Token: Access denied.\n")
+  #     end
+
+  #     it "should have 200 https status" do
+  #       request.headers.merge!({ 'Authorization': "Bearer " + ENV["BEARER_TOKEN"] })
+  #       get :send_props_favorites, params: { subscriber_id: @sub.id }
+  #       expect(response).to have_http_status(:success)
+  #     end
+  #   end
+  # end
 end
