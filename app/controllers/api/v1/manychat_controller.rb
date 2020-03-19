465c4fb37b1b93641747d8c7db6cb79340a61bdc
fr3d100 <f.bonnand@gmail.com>
2020-03-19 10:48:55 +0100

    Remove facebook_id as a permitted updatable field from api call

--- app/controllers/api/v1/manychat_controller.rb
+++ app/controllers/api/v1/manychat_controller.rb
@@ -137,6 +137,6 @@ class Api::V1::ManychatController < ApplicationController
   end
 
   def subscriber_params
-    params.permit(:firstname, :lastname, :email, :phone, :facebook_id, :is_active)
+    params.permit(:firstname, :lastname, :email, :phone, :is_active)
   end
 end
