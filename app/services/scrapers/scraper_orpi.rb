757f9766271d74e49ebacc85cd072af4797bae6d
Segrelove <mlesegret@gmail.com>
2020-03-19 11:56:21 +0100

    changed architecture of scraper, dryer, better, and added a limit for out test, if limit is passed as a parameter, it stops after said limit

--- app/services/scrapers/scraper_orpi.rb
+++ app/services/scrapers/scraper_orpi.rb
@@ -1,16 +1,20 @@
 class ScraperOrpi < Scraper
-  attr_accessor :url, :properties, :source, :xml_first_page
+  attr_accessor :url, :properties, :source, :main_page_cls, :type, :waiting_cls, :multi_page, :page_nbr
 
   def initialize
     @url = "https://www.orpi.com/recherche/buy?transaction=buy&resultUrl=&realEstateTypes%5B0%5D=maison&realEstateTypes%5B1%5D=appartement&locations%5B0%5D%5Bvalue%5D=paris&locations%5B0%5D%5Blabel%5D=Paris&agency=&minSurface=10&maxSurface=400&nbRooms%5B0%5D=1&nbRooms%5B1%5D=2&nbRooms%5B2%5D=3&nbRooms%5B3%5D=4&nbRooms%5B4%5D=5&nbRooms%5B5%5D=6&newBuild=true&oldBuild=true&minPrice=100000&maxPrice=6000000&sort=date-down&layoutType=list&nbBedrooms=&page=&minLotSurface=&maxLotSurface=&minStoryLocation=&maxStoryLocation="
     @source = "Orpi"
-    @xml_first_page = "div.c-box__inner.c-box__inner--sm"
+    @main_page_cls = "div.c-box__inner.c-box__inner--sm"
+    @type = "Dynamic"
+    @waiting_cls = "o-grid"
+    @multi_page = false
+    @page_nbr = 1
+    @properties = []
   end
 
-  def extract_first_page
-    xml = fetch_main_page(@url, @xml_first_page, "Dynamic", "o-grid")
-    hashed_properties = []
-    xml.each do |item|
+  def launch(limit = nil)
+    i = 0
+    fetch_main_page(self).each do |item|
       begin
         hashed_property = {}
         hashed_property[:link] = "https://www.orpi.com" + access_xml_link(item, "a.u-link-unstyled.c-overlay__link", "href")[0].to_s
@@ -18,47 +22,40 @@ class ScraperOrpi < Scraper
         hashed_property[:area] = "750" + access_xml_text(item, "p.u-mt-sm").tr("^0-9", "")
         hashed_property[:rooms_number] = regex_gen(access_xml_array_to_text(item, "a.u-link-unstyled.c-overlay__link"), '(\d+)(.?)(pi(è|e)ce(s?))').to_float_to_int_scrp
         hashed_property[:price] = regex_gen(access_xml_text(item, "div:nth-child(1) > div:nth-child(2)"), '(\d)(.*)(€)').to_int_scrp
-        hashed_properties.push(extract_each_flat(hashed_property)) if is_property_clean(hashed_property)
+        if is_property_clean(hashed_property)
+          html = fetch_static_page(hashed_property[:link])
+          hashed_property[:bedrooms_number] = regex_gen(access_xml_array_to_text(html, "div#collapse-details-panel").specific_trim_scrp("\n\r\t"), '(\d+)(.?)(chambre(s?))').to_int_scrp
+          hashed_property[:description] = access_xml_text(html, "div.o-container > p:nth-child(2)").specific_trim_scrp("\n\r").strip
+          hashed_property[:flat_type] = regex_gen(access_xml_text(html, "span.u-text-xl"), "((a|A)ppartement|(A|a)ppartements|(S|s)tudio|(S|s)tudette|(C|c)hambre|(M|m)aison)").capitalize
+          hashed_property[:floor] = perform_floor_regex(hashed_property[:description])
+          hashed_property[:has_elevator] = perform_elevator_regex(hashed_property[:description])
+          hashed_property[:subway_ids] = perform_subway_regex(hashed_property[:description])
+          hashed_property[:provider] = "Particulier"
+          hashed_property[:source] = @source
+          hashed_property[:images] = []
+          html.css("img.u-cover.u-flex-item-auto").each do |img|
+            chr_array = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+            html.css("a.c-btn.c-btn--tertiary").each do |nbr_item|
+              nbr = nbr_item.text.gsub("Voir les ", "").gsub("photos", "").to_i
+              j = 0
+              nbr.times do
+                new_img = img[:src].gsub("a--", chr_array[j] + "--")
+                hashed_property[:images].push(new_img)
+                j += 1
+              end
+            end
+          end
+          @properties.push(hashed_property) ##testing purpose
+          enrich_then_insert_v2(hashed_property)
+          i += 1
+          break if i == limit
+        end
       rescue StandardError => e
         puts "\nError for #{@source}, skip this one."
         puts "It could be a bad link or a bad xml extraction.\n\n"
         next
       end
     end
-    enrich_then_insert(hashed_properties)
-  end
-
-  private
-
-  def extract_each_flat(prop)
-    flat_data = {}
-    html = fetch_static_page(prop[:link])
-    flat_data[:link] = prop[:link]
-    flat_data[:surface] = prop[:surface]
-    flat_data[:area] = prop[:area]
-    flat_data[:rooms_number] = prop[:rooms_number]
-    flat_data[:price] = prop[:price]
-    flat_data[:bedrooms_number] = regex_gen(access_xml_array_to_text(html, "div#collapse-details-panel").specific_trim_scrp("\n\r\t"), '(\d+)(.?)(chambre(s?))').to_int_scrp
-    flat_data[:description] = access_xml_text(html, "div.o-container > p:nth-child(2)").specific_trim_scrp("\n\r").strip
-    flat_data[:flat_type] = regex_gen(access_xml_text(html, "span.u-text-xl"), "((a|A)ppartement|(A|a)ppartements|(S|s)tudio|(S|s)tudette|(C|c)hambre|(M|m)aison)").capitalize
-    flat_data[:floor] = perform_floor_regex(flat_data[:description])
-    flat_data[:has_elevator] = perform_elevator_regex(flat_data[:description])
-    flat_data[:subway_ids] = perform_subway_regex(flat_data[:description])
-    flat_data[:provider] = "Particulier"
-    flat_data[:source] = @source
-    flat_data[:images] = []
-    html.css("img.u-cover.u-flex-item-auto").each do |img|
-      chr_array = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-      html.css("a.c-btn.c-btn--tertiary").each do |nbr_item|
-        nbr = nbr_item.text.gsub("Voir les ", "").gsub("photos", "").to_i
-        i = 0
-        nbr.times do
-          new_img = img[:src].gsub("a--", chr_array[i] + "--")
-          flat_data[:images].push(new_img)
-          i += 1
-        end
-      end
-    end
-    return flat_data
+    return @properties
   end
 end
--- app/services/scrapers/scraper_super_immo.rb
+++ app/services/scrapers/scraper_super_immo.rb
@@ -1,16 +1,20 @@
 class ScraperSuperImmo < Scraper
-  attr_accessor :url, :properties, :source, :xml_first_page
+  attr_accessor :url, :properties, :source, :main_page_cls, :type, :waiting_cls, :multi_page, :page_nbr
 
   def initialize
     @url = "https://www.superimmo.com/achat/ile-de-france/paris?sort=created_at"
     @source = "SuperImmo"
-    @xml_first_page = "section > div.media-body"
+    @main_page_cls = "section > div.media-body"
+    @type = "Static"
+    @waiting_cls = nil
+    @multi_page = false
+    @page_nbr = 1
+    @properties = []
   end
 
-  def extract_first_page
-    xml = fetch_main_page(@url, @xml_first_page)
-    hashed_properties = []
-    xml.each do |item|
+  def launch(limit = nil)
+    i = 0
+    fetch_main_page(self).each do |item|
       begin
         hashed_property = {}
         hashed_property[:link] = "https://superimmo.com" + access_xml_link(item, "p > a", "href")[0].to_s
@@ -18,36 +22,29 @@ class ScraperSuperImmo < Scraper
         hashed_property[:area] = regex_gen(item.text, '(75)$*\d+{3}')
         hashed_property[:rooms_number] = regex_gen(item.text, '(\d+)(.?)(pi(è|e)ce(s?))').to_float_to_int_scrp
         hashed_property[:price] = regex_gen(item.text, '(\d)(.*)(€)').to_int_scrp
-        hashed_properties.push(extract_each_flat(hashed_property)) if is_property_clean(hashed_property)
+        if is_property_clean(hashed_property)
+          html = fetch_static_page(hashed_property[:link])
+          hashed_property[:bedrooms_number] = regex_gen(access_xml_text(html, "h1"), '(\d+)(.?)(chambre(s?))').to_int_scrp
+          hashed_property[:description] = access_xml_text(html, "p.description").strip
+          hashed_property[:flat_type] = access_xml_text(html, "#itemprop-appartements")
+          hashed_property[:agency_name] = access_xml_text(html, "header > div.media-body > b")
+          hashed_property[:floor] = perform_floor_regex(hashed_property[:description])
+          hashed_property[:has_elevator] = perform_elevator_regex(hashed_property[:description])
+          hashed_property[:subway_ids] = perform_subway_regex(hashed_property[:description])
+          hashed_property[:provider] = "Agence"
+          hashed_property[:source] = @source
+          hashed_property[:images] = access_xml_link(html, "a.fancybox img", "src")
+          @properties.push(hashed_property) ##testing purpose
+          enrich_then_insert_v2(hashed_property)
+          i += 1
+          break if i == limit
+        end
       rescue StandardError => e
         puts "\nError for #{@source}, skip this one."
         puts "It could be a bad link or a bad xml extraction.\n\n"
         next
       end
     end
-    enrich_then_insert(hashed_properties)
-  end
-
-  private
-
-  def extract_each_flat(prop)
-    flat_data = {}
-    html = fetch_static_page(prop[:link])
-    flat_data[:link] = prop[:link]
-    flat_data[:surface] = prop[:surface]
-    flat_data[:area] = prop[:area]
-    flat_data[:rooms_number] = prop[:rooms_number]
-    flat_data[:price] = prop[:price]
-    flat_data[:bedrooms_number] = regex_gen(access_xml_text(html, "h1"), '(\d+)(.?)(chambre(s?))').to_int_scrp
-    flat_data[:description] = access_xml_text(html, "p.description").strip
-    flat_data[:flat_type] = access_xml_text(html, "#itemprop-appartements")
-    flat_data[:agency_name] = access_xml_text(html, "header > div.media-body > b")
-    flat_data[:floor] = perform_floor_regex(flat_data[:description])
-    flat_data[:has_elevator] = perform_elevator_regex(flat_data[:description])
-    flat_data[:subway_ids] = perform_subway_regex(flat_data[:description])
-    flat_data[:provider] = "Agence"
-    flat_data[:source] = @source
-    flat_data[:images] = access_xml_link(html, "a.fancybox img", "src")
-    return flat_data
+    return @properties
   end
 end
