ec9f52dbd7b9621d04af81a58da7e0488c8dc117
fr3d100 <f.bonnand@gmail.com>
2020-03-19 09:45:48 +0100

    Update test logic

--- spec/controllers/api/v1/manychat_controller_spec.rb
+++ spec/controllers/api/v1/manychat_controller_spec.rb
@@ -1,26 +1,22 @@
-require 'rails_helper'
+require "rails_helper"
 require "dotenv/load"
 
 RSpec.describe Api::V1::ManychatController, type: :controller do
+  describe "A subscriber should receive his favorites" do
+    context "#send_favorites" do
+      before :all do
+        @sub = FactoryBot.create(:subscriber_fred)
+      end
+      it "should contain an authorization token" do
+        get :send_props_favorites, params: { subscriber_id: @sub.id }
+        expect(response.body).to eq("HTTP Token: Access denied.\n")
+      end
 
-    describe "A subscriber should receive his favorites" do 
-
-        sub = FactoryBot.create(:subscriber_fred)
-
-        context "#send_favorites" do 
-           
-            it 'should contain an authorization token' do 
-                get :send_props_favorites, params: { subscriber_id: sub.id }
-                expect(response.body).to eq("HTTP Token: Access denied.\n")         
-            end
-
-            it 'should have 200 https status' do
-                request.headers.merge!({ 'Authorization':  "Bearer " + ENV["BEARER_TOKEN"]})
-                get :send_props_favorites, params: { subscriber_id: sub.id }
-                expect(response).to have_http_status(:success)
-            end
-        end
-
+      it "should have 200 https status" do
+        request.headers.merge!({ 'Authorization': "Bearer " + ENV["BEARER_TOKEN"] })
+        get :send_props_favorites, params: { subscriber_id: @sub.id }
+        expect(response).to have_http_status(:success)
+      end
     end
-
+  end
 end
