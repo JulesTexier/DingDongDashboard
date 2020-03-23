a3f7106a5347e74932d2235ded61a76926b74ec5
Segrelove <mlesegret@gmail.com>
2020-03-23 18:01:20 +0100

    added retry for two tests and better properties for factory bot

--- test/factories/properties.rb
+++ test/factories/properties.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     rooms_number { 2 }
     area { "75018" }
     source { "Ding Dong" }
+    description { "Ici c'est vraiment un super endroit ahhaha" }
     images { ["https://image.com", "cache/forever", "https://icic-est-paris.com"] }
     link { "https://hellodingdong.com" }
   end
