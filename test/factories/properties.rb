034d5750e743d2deb7f5c0c1366b4137d3554f1f
Segrelove <mlesegret@gmail.com>
2020-03-24 15:46:19 +0100

    added tests

--- test/factories/properties.rb
+++ test/factories/properties.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     rooms_number { 2 }
     area { "75018" }
     source { "Ding Dong" }
-    description { "Ici c'est vraiment un super endroit ahhaha" }
+    description { "À 50 mètres du M°Jules Joffrin et de la mairie, dans immeuble pierre de taille        , chaleureux 2 pièces de 37,20 m² comprenant entrée, séjour, cuisine séparée, chambre, WC séparés, salle de bains, cave. Parquets, moulures, cheminée. 1er étage vue sur l’église. À rafraichir. EXCLUSIVITÉ ACOPA." }
     images { ["https://image.com", "cache/forever", "https://icic-est-paris.com"] }
     link { "https://hellodingdong.com" }
   end
