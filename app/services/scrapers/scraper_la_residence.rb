e1b7e8b6799a0d7924388e1f107daa7ab8d26593
Segrelove <mlesegret@gmail.com>
2020-03-26 22:30:37 +0100

    changed synthax with rufo, the code is cool :D

--- app/services/scrapers/scraper_la_residence.rb
+++ app/services/scrapers/scraper_la_residence.rb
@@ -17,29 +17,29 @@ class ScraperLaResidence < Scraper
     fetch_main_page(self).each do |item|
       begin
         hashed_property = {}
-        hashed_property[:link] = "https://www.laresidence.fr/" + access_xml_link(item,'div:first-child > a','href')[0].to_s
-        hashed_property[:surface] = regex_gen(access_xml_text(item,'.vignetteBienTitre > h2'), '(\d+(,?)(\d*))(.)(m)').to_float_to_int_scrp
-        hashed_property[:area] = regex_gen(access_xml_text(item,'.vignetteBienTitre > h2').area_translator_scrp, '(75)$*\d+{3}')
-        hashed_property[:bedrooms_number] = regex_gen(access_xml_text(item,'.vignetteBienTexte'), '(\d+)(.?)(chambre(s?))').to_float_to_int_scrp
+        hashed_property[:link] = "https://www.laresidence.fr/" + access_xml_link(item, "div:first-child > a", "href")[0].to_s
+        hashed_property[:surface] = regex_gen(access_xml_text(item, ".vignetteBienTitre > h2"), '(\d+(,?)(\d*))(.)(m)').to_float_to_int_scrp
+        hashed_property[:area] = regex_gen(access_xml_text(item, ".vignetteBienTitre > h2").area_translator_scrp, '(75)$*\d+{3}')
+        hashed_property[:bedrooms_number] = regex_gen(access_xml_text(item, ".vignetteBienTexte"), '(\d+)(.?)(chambre(s?))').to_float_to_int_scrp
         hashed_property[:rooms_number] = hashed_property[:bedrooms_number] + 1
-        hashed_property[:price] = access_xml_text(item, '.vignetteBienPrixFAI').to_int_scrp
-
+        hashed_property[:price] = access_xml_text(item, ".vignetteBienPrixFAI").to_int_scrp
         if is_property_clean(hashed_property)
           html = fetch_static_page(hashed_property[:link])
-          hashed_property[:rooms_number] = regex_gen(access_xml_text(html, '.ficheAffaireWrapper> h1').strip, '(\d+)(.?)(pi(è|e)ce(s?))').to_float_to_int_scrp
+          hashed_property[:rooms_number] = regex_gen(access_xml_text(html, ".ficheAffaireWrapper> h1").strip, '(\d+)(.?)(pi(è|e)ce(s?))').to_float_to_int_scrp
           if hashed_property[:rooms_number] == 1
-             hashed_property[:bedrooms_number] = 0
+            hashed_property[:bedrooms_number] = 0
           end
-          hashed_property[:description] = access_xml_text(html, '#descriptif_anchor > p').strip.gsub(/[^[:print:]]/,'')
-          hashed_property[:flat_type] = get_type_flat(access_xml_text(html, '.ficheAffaireWrapper> h1').strip)
+          hashed_property[:description] = access_xml_text(html, "#descriptif_anchor > p").strip.gsub(/[^[:print:]]/, "")
+          hashed_property[:flat_type] = get_type_flat(access_xml_text(html, ".ficheAffaireWrapper> h1").strip)
           hashed_property[:agency_name] = @source
           hashed_property[:floor] = perform_floor_regex(hashed_property[:description])
           hashed_property[:has_elevator] = perform_elevator_regex(hashed_property[:description])
           hashed_property[:subway_ids] = perform_subway_regex(hashed_property[:description])
           hashed_property[:provider] = "Agence"
           hashed_property[:source] = @source
-          hashed_property[:images] = access_xml_link(html, '.swiper-slide3 > a > img', 'src')
+          hashed_property[:images] = access_xml_link(html, ".swiper-slide3 > a > img", "src")
           @properties.push(hashed_property) ##testing purpose
+          puts JSON.pretty_generate(hashed_property)
           enrich_then_insert_v2(hashed_property)
           i += 1
           break if i == limit
