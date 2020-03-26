cbf962ad5e613f2a5978a0330399a1eb8d73614a
Segrelove <mlesegret@gmail.com>
2020-03-26 16:16:51 +0100

    fred is on fire

--- app/services/scrapers/scraper_paris_montmartre_immobilier.rb
+++ app/services/scrapers/scraper_paris_montmartre_immobilier.rb
@@ -17,17 +17,16 @@ class ScraperParisMontmartreImmobilier < Scraper
     fetch_main_page(self).each do |item|
       begin
         hashed_property = {}
-        hashed_property[:link] = access_xml_link(item, '.property-title>a','href')[0].to_s
+        hashed_property[:link] = access_xml_link(item, ".property-title>a", "href")[0].to_s
         hashed_property[:surface] = regex_gen(item.text, '(\d+(,?)(\d*))(.)(m)').to_float_to_int_scrp
-        hashed_property[:area] = regex_gen(access_xml_text(item, '.property-title>a'), '(PARIS(.?)(\d+))').to_int_scrp.to_s.district_generator
+        hashed_property[:area] = regex_gen(access_xml_text(item, ".property-title>a"), '(PARIS(.?)(\d+))').to_int_scrp.to_s.district_generator
         hashed_property[:rooms_number] = regex_gen(access_xml_text(item, ".property-title"), '(\d+)(.?)(pi(è|e)ce(s?))').to_float_to_int_scrp
         hashed_property[:price] = regex_gen(item.text, '(\d)(.*)(€)').to_int_scrp
         if is_property_clean(hashed_property)
           html = fetch_static_page(hashed_property[:link])
-          detail = access_xml_text(html, "#detail").strip.tr(' ','')
+          detail = access_xml_text(html, "#detail").strip.tr(" ", "")
           hashed_property[:bedrooms_number] = regex_gen(detail, 'Chambre(s?):(\d)*').to_int_scrp
           hashed_property[:description] = access_xml_text(html, "#description>p").strip
-        #   hashed_property[:flat_type] = access_xml_text(html, "#itemprop-appartements") // No easy way to get it ...
           hashed_property[:agency_name] = @source
           hashed_property[:floor] = regex_gen(detail, 'Etage(s?):(\d)*').to_int_scrp
           hashed_property[:has_elevator] = perform_elevator_regex(hashed_property[:description])
@@ -43,6 +42,8 @@ class ScraperParisMontmartreImmobilier < Scraper
       rescue StandardError => e
         puts "\nError for #{@source}, skip this one."
         puts "It could be a bad link or a bad xml extraction.\n\n"
+        puts e.message
+        puts e.backtrace
         next
       end
     end
