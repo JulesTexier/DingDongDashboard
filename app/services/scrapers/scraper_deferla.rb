63119f0bbdc018e6ed3c2d09a73a4afab1329c4d
Segrelove <mlesegret@gmail.com>
2020-03-26 09:51:50 +0100

    added backtrace in error handling to see why watir fails in prod

--- app/services/scrapers/scraper_deferla.rb
+++ app/services/scrapers/scraper_deferla.rb
@@ -43,6 +43,8 @@ class ScraperDeferla < Scraper
       rescue StandardError => e
         puts "\nError for #{@source}, skip this one."
         puts "It could be a bad link or a bad xml extraction.\n\n"
+        puts e.message
+        puts e.backtrace
         next
       end
     end
