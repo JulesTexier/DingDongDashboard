408431c2de231c38b46e6410a535bfe870fc4372
fr3d100 <f.bonnand@gmail.com>
2020-04-01 16:59:00 +0200

    Update Orpi scraper with Json method

--- app/services/scrapers/scraper_orpi.rb
+++ app/services/scrapers/scraper_orpi.rb
@@ -1,51 +1,42 @@
 class ScraperOrpi < Scraper
-  attr_accessor :url, :properties, :source, :main_page_cls, :type, :waiting_cls, :multi_page, :page_nbr, :wait, :click_args
+  attr_accessor :url, :properties, :source, :main_page_cls, :type, :waiting_cls, :multi_page, :page_nbr, :wait, :click_args, :http_type
 
   def initialize
-    @url = "https://www.orpi.com/recherche/buy?transaction=buy&resultUrl=&realEstateTypes%5B0%5D=maison&realEstateTypes%5B1%5D=appartement&locations%5B0%5D%5Bvalue%5D=paris&locations%5B0%5D%5Blabel%5D=Paris&agency=&minSurface=10&maxSurface=400&nbRooms%5B0%5D=1&nbRooms%5B1%5D=2&nbRooms%5B2%5D=3&nbRooms%5B3%5D=4&nbRooms%5B4%5D=5&nbRooms%5B5%5D=6&newBuild=true&oldBuild=true&minPrice=100000&maxPrice=6000000&sort=date-down&layoutType=list&nbBedrooms=&page=&minLotSurface=&maxLotSurface=&minStoryLocation=&maxStoryLocation="
+    @url = "https://www.orpi.com/recherche/ajax/buy?realEstateTypes%5B%5D=maison&realEstateTypes%5B%5D=appartement&locations%5B0%5D%5Bvalue%5D=paris&locations%5B0%5D%5Blabel%5D=Paris&minSurface=10&maxSurface=1000&nbRooms%5B%5D=1&nbRooms%5B%5D=2&nbRooms%5B%5D=3&nbRooms%5B%5D=4&nbRooms%5B%5D=5&nbRooms%5B%5D=6&newBuild=true&oldBuild=true&minPrice=0&maxPrice=7000000&sort=date-down&layoutType=list"
     @source = "Orpi"
     @main_page_cls = "div.c-box__inner.c-box__inner--sm"
-    @type = "Dynamic"
+    @type = "HTTPRequest"
     @waiting_cls = "u-mt-md"
     @multi_page = false
     @page_nbr = 1
     @properties = []
-    @wait = 0
+    @wait = 0,
+    @http_type = 'get_json'
   end
 
   def launch(limit = nil)
     i = 0
-    fetch_main_page(self).each do |item|
+    fetch_main_page(self)["items"].each do |item|
       begin
+        next if item["sold"]
         hashed_property = {}
-        hashed_property[:link] = "https://www.orpi.com" + access_xml_link(item, "a.u-link-unstyled.c-overlay__link", "href")[0].to_s
-        hashed_property[:surface] = regex_gen(access_xml_array_to_text(item, "a.u-link-unstyled.c-overlay__link").specific_trim_scrp("\n\r\t"), '(\d+(,?)(\d*))(.)(m)').to_float_to_int_scrp
-        hashed_property[:area] = "750" + access_xml_text(item, "p.u-mt-sm").tr("^0-9", "")
-        hashed_property[:rooms_number] = regex_gen(access_xml_array_to_text(item, "a.u-link-unstyled.c-overlay__link"), '(\d+)(.?)(pi(è|e)ce(s?))').to_float_to_int_scrp
-        hashed_property[:price] = regex_gen(access_xml_text(item, "div:nth-child(1) > div:nth-child(2)"), '(\d)(.*)(€)').to_int_scrp
+        hashed_property[:link] = "https://www.orpi.com/annonce-vente-" + item["slug"]
+        hashed_property[:surface] = item["surface"].to_i
+        item["locationDescription"].include?("Paris") ? hashed_property[:area] = "750" + item["locationDescription"].tr("^0-9", "") : nil
+        hashed_property[:rooms_number] = item["nbRooms"]
+        hashed_property[:price] = item["price"].to_i
         if go_to_prop?(hashed_property, 7)
           html = fetch_static_page(hashed_property[:link])
-          hashed_property[:bedrooms_number] = regex_gen(access_xml_array_to_text(html, "div#collapse-details-panel").specific_trim_scrp("\n\r\t"), '(\d+)(.?)(chambre(s?))').to_int_scrp
+          details = access_xml_text(html, '#collapse-details').strip.gsub(/[^[:print:]]/, "").gsub(" ","").remove_acc_scrp
+          hashed_property[:rooms_number] == 1 ? hashed_property[:bedrooms_number] = 0 : hashed_property[:bedrooms_number] = regex_gen(details, '(\d){1,}chambre').to_int_scrp
+          hashed_property[:floor] = regex_gen(details, 'etage(\d){1,}').to_int_scrp
           hashed_property[:description] = access_xml_text(html, "div.o-container > p:nth-child(2)").specific_trim_scrp("\n\r").strip
           hashed_property[:flat_type] = regex_gen(access_xml_text(html, "span.u-text-xl"), "((a|A)ppartement|(A|a)ppartements|(S|s)tudio|(S|s)tudette|(C|c)hambre|(M|m)aison)").capitalize
-          hashed_property[:floor] = perform_floor_regex(hashed_property[:description])
-          hashed_property[:has_elevator] = perform_elevator_regex(hashed_property[:description])
+          details.match(/ascenseur/i).is_a?(MatchData) ? hashed_property[:has_elevator] = true : hashed_property[:has_elevator] = perform_elevator_regex(hashed_property[:description])
           hashed_property[:subway_ids] = perform_subway_regex(hashed_property[:description])
-          hashed_property[:provider] = "Particulier"
+          hashed_property[:provider] = "Agence"
           hashed_property[:source] = @source
-          hashed_property[:images] = []
-          html.css("img.u-cover.u-flex-item-auto").each do |img|
-            chr_array = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-            html.css("a.c-btn.c-btn--tertiary").each do |nbr_item|
-              nbr = nbr_item.text.gsub("Voir les ", "").gsub("photos", "").to_i
-              j = 0
-              nbr.times do
-                new_img = img[:src].gsub("a--", chr_array[j] + "--")
-                hashed_property[:images].push(new_img)
-                j += 1
-              end
-            end
-          end
+          hashed_property[:images] = item["images"]
           @properties.push(hashed_property) ##testing purpose
           enrich_then_insert_v2(hashed_property)
           i += 1
@@ -57,6 +48,7 @@ class ScraperOrpi < Scraper
         next
       end
     end
+    byebug
     return @properties
   end
 end
