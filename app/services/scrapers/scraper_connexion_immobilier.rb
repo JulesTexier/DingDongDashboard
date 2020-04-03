c874432ab90127089d7bf1920fabdbd3cce20f3d
fr3d100 <f.bonnand@gmail.com>
2020-04-03 11:57:35 +0200

    Update and improve connexion immobilier scraper

--- app/services/scrapers/scraper_connexion_immobilier.rb
+++ app/services/scrapers/scraper_connexion_immobilier.rb
@@ -17,20 +17,28 @@ class ScraperConnexionImmobilier < Scraper
     fetch_main_page(self).each do |item|
       begin
         hashed_property = {}
-        hashed_property[:link] = "https://www.connexion-immobilier.com/achat-vente-immobilier-acheter-paris/resultats-annonces-achat"
+        hashed_property[:link] = regex_gen(access_xml_link(item, 'a.gp','href')[0], '(&url=http)(.)*').gsub('&url=','')
         hashed_property[:surface] = access_xml_text(item, '.surface>span').to_int_scrp
         hashed_property[:area] = access_xml_text(item, '.city > nobr').to_int_scrp.to_s.district_generator
         hashed_property[:rooms_number] = access_xml_text(item, '.rooms').to_int_scrp
         hashed_property[:price] = access_xml_text(item, '.price').to_int_scrp
 
         if go_to_prop?(hashed_property, 7)
-          hashed_property[:description] = ""
-          hashed_property[:bedrooms_number] = access_xml_text(item, '.bedrooms').to_int_scrp
-        access_xml_text(item, '.floor') == "RDC" ? hashed_property[:floor] = 0 : hashed_property[:floor] = access_xml_text(item, '.floor').to_int_scrp
-        (access_xml_link(item, '.floor', 'class' )[0].include?("nolift") || access_xml_link(item, '.floor', 'class' )[0].include?("floorrdc")) ? hashed_property[:has_elevator] = false : hashed_property[:has_elevator] = true
+          html = fetch_static_page(hashed_property[:link])
+          hashed_property[:description] = access_xml_text(html, 'div.desc').strip.gsub(/[^[:print:]]/, "")
+          !access_xml_text(item, '.bedrooms').empty? ? hashed_property[:bedrooms_number] = access_xml_text(item, '.bedrooms').to_int_scrp : nil
+          access_xml_text(item, '.floor').gsub(/[^[:print:]]/, "").include?("RDC") ? hashed_property[:floor] = 0 : hashed_property[:floor] = access_xml_text(item, '.floor').to_int_scrp
+          (access_xml_link(item, '.floor', 'class' )[0].include?("nolift") || access_xml_link(item, '.floor', 'class' )[0].include?("floorrdc")) ? hashed_property[:has_elevator] = false : hashed_property[:has_elevator] = true
           hashed_property[:provider] = "Agence"
           hashed_property[:source] = @source
-          hashed_property[:images] = access_xml_link(item, 'img','src')
+          hashed_property[:agency_name] = access_xml_text(html, '#detformcall > div.txt > p.name')
+          hashed_property[:contact_number] = access_xml_text(html, '#detformcall > div.txt > p.desktoponly.tel').gsub(' ','')
+          hashed_property[:subway_ids] = perform_subway_regex(hashed_property[:description])
+          imgs = access_xml_link(html, '[itemprop="image"]', 'src')
+          hashed_property[:images] = []
+          imgs.each do |img|
+            hashed_property[:images].push(img) if !img.nil?
+          end
           @properties.push(hashed_property) ##testing purpose
           enrich_then_insert_v2(hashed_property)
           i += 1
