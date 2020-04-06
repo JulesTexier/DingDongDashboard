6df8416a62d71e52f4c4308d1644371e9bccbba9
fr3d100 <f.bonnand@gmail.com>
2020-04-06 23:18:57 +0200

    Add postmark

--- app/controllers/api/v1/trello_controller.rb
+++ app/controllers/api/v1/trello_controller.rb
@@ -1,6 +1,6 @@
 require "dotenv/load"
 require "typhoeus"
-require 'postmark'
+require 'postmark-rails'
 
 class Api::V1::TrelloController < ApplicationController
   protect_from_forgery with: :null_session
@@ -9,8 +9,20 @@ class Api::V1::TrelloController < ApplicationController
   def  send_chatbot_link_from_trello_btn
     document = JSON.parse(request.body.read)
     lead = Lead.where(trello_id_card: document["cardId"])
-    render json: {status: 'SUCCESS', message: 'Lead found', data: lead}, status: 500
 
+    client = Postmark::ApiClient.new(ENV['POSTMARK_API'])
+    client.deliver_with_template(
+      {:from=>"greg@hellodingdong.com",
+     :to=>lead.email,
+     :template_alias=>"welcome",
+     :template_model=>
+      {"name"=>lead.name,
+       "action_url"=>chatbot_link,
+       "broker_phone"=>lead.broker.phone,
+       "broker_email"=>lead.broker.email}}
+    )
+
+    render json: {status: 'SUCCESS', message: 'Lead found', data: lead}, status: 500
   end
 
 
