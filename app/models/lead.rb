2d3eb9a46f49983c7af7e40addf8b7ebcdcb7c0c
fr3d100 <f.bonnand@gmail.com>
2020-04-06 16:38:45 +0200

    Update trello display models for a Lead

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,19 +1,24 @@
 class Lead < ApplicationRecord
 
-  def generate_trello_description
+  def trello_description
     desc = ""
-    desc += "**CONTACT** \u000A Tél: #{self.phone} \u000A Email: #{self.email}"
-    desc += "\u000A**PROJET**"
-    desc += "\u000A**FINANCEMENT**"
-    desc += "\u000A**CLIENTE**"
-    desc += "\u000A**NOTES**"
+    desc += "**CONTACT** \u000A Tél: #{self.phone} \u000A Email: #{self.email}\u000A"
+    desc += "\u000A**PROJET**\u000A"
+    desc += "\u000A**FINANCEMENT**\u000A"
+    desc += "\u000A**CLIENTE**\u000A"
+    desc += "\u000A**NOTES**\u000A"
+    desc += "\u000A\u000A---\u000A\u000A"
+    desc += trello_summary
   end
 
   def trello_summary
     desc = ""
-    desc += "**Projet d'achat** : #{self.project_type}"
-    desc += "\u000A**Budget Maximum** : #{self.max_price}"
-    desc += "\u000A**Surface Minimum Maximum** : #{self.min_surface}"
+    desc += "**#{self.name.upcase}**"
+    desc += "\u000A**Projet d'achat** : #{self.project_type}"
+    desc += "\u000A**Budget Maximum** : #{self.max_price.to_s.reverse.gsub(/...(?=.)/,'\& ').reverse} €"
+    desc += "\u000A**Surface Minimum ** : #{self.min_surface} m2"
+    desc += "\u000A**Arrondissements** : #{self.areas}"
+    desc += "\u000A\u000A*Inscription chez DingDong : #{self.created_at.strftime("%d/%m/%Y - %H:%M")}*"
   end
 
 
