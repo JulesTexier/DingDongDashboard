f9885cedff22964568544331eb9f3c78b161c228
fr3d100 <f.bonnand@gmail.com>
2020-04-06 16:38:59 +0200

    Add Trello API implementation

--- app/controllers/api/v1/typeform_controller.rb
+++ app/controllers/api/v1/typeform_controller.rb
@@ -6,41 +6,73 @@ class Api::V1::TypeformController < ApplicationController
 
 
     def generate_lead 
-      document = JSON.parse(request.body.read)
-      lead_hash = {}
-      answers = document['form_response']['answers']
-      lead_hash[:name] = answers[7]["text"]
-      lead_hash[:phone] = answers[9]["phone_number"]
-      lead_hash[:email] = answers[8]["email"]
-      lead_hash[:has_messenger] = answers[6]["boolean"]
-      lead_hash[:additional_question] = answers[5]["text"]
-      lead_hash[:specific_criteria] = answers[4]["text"]
-      lead_hash[:areas] = answers[3]["choices"]["labels"].join(",")
-      lead_hash[:min_surface] = answers[2]["number"]
-      lead_hash[:max_price] = answers[1]["number"]
-      lead_hash[:project_type] = answers[0]["choice"]["label"]
-      lead = Lead.new(lead_hash)
-      if lead.save
-        puts "A new lead has been generated"
+      begin 
+        
+        document = JSON.parse(request.body.read)
+        lead_hash = {}
+        answers = document['form_response']['answers']
+        lead_hash[:name] = answers[7]["text"]
+        lead_hash[:phone] = answers[9]["phone_number"]
+        lead_hash[:email] = answers[8]["email"]
+        lead_hash[:has_messenger] = answers[6]["boolean"]
+        lead_hash[:additional_question] = answers[5]["text"]
+        lead_hash[:specific_criteria] = answers[4]["text"]
+        lead_hash[:areas] = answers[3]["choices"]["labels"].join(",")
+        lead_hash[:min_surface] = answers[2]["number"]
+        lead_hash[:max_price] = answers[1]["number"]
+        lead_hash[:project_type] = answers[0]["choice"]["label"]
+        lead = Lead.new(lead_hash)
 
-        params = {}
-        params[:name] = lead_hash[:name]
-        params[:desc] = lead.trello_summary
-        params[:pos] = 'top'
+        if lead.save
+          puts "A new lead has been generated"
 
-        request = Typhoeus::Request.new(
-          "https://api.trello.com/1/cards?idList=5e732e43da3a0d8d512c88d0&key=bd3b7da95b0121bd8bdea25ff2ab5d50&token=11e09ded7eeceabe509408840639cd97864ca350310fa85ec3cd4ab5389bd97d",
-          method: :post,
-          params: params
-        )
-        response = request.run
-        if response.code == 200
-          render json: {status: 'SUCCESS', message: 'Lead added to Trello !', data: lead}, status: 200
+          params = {}
+          params[:name] = lead_hash[:name]
+          params[:desc] = lead.trello_description
+          params[:pos] = 'top'
+          params[:due] = Time.now + 15.minutes
+
+          b = Broker.first
+          params[:idMembers] = b.trello_id
+
+          # 1• Create card on tello Board 
+          request = Typhoeus::Request.new(
+            "https://api.trello.com/1/cards?idList=#{b.trello_lead_list_id}&key=bd3b7da95b0121bd8bdea25ff2ab5d50&token=11e09ded7eeceabe509408840639cd97864ca350310fa85ec3cd4ab5389bd97d",
+            method: :post,
+            params: params
+          )
+          response = request.run
+
+          # 2• Add checklist 'Action' to created card
+          card_id = JSON.parse(response.body)["id"]
+          checklist_params = {}
+          checklist_params[:name] = "ACTIONS"
+          request = Typhoeus::Request.new(
+            "https://api.trello.com/1/cards/#{card_id}/checklists?&key=bd3b7da95b0121bd8bdea25ff2ab5d50&token=11e09ded7eeceabe509408840639cd97864ca350310fa85ec3cd4ab5389bd97d",
+            method: :post,
+            params: checklist_params
+          )
+          response = request.run
+
+          # 3• Add first action on the checklist
+          checklist_id = JSON.parse(response.body)["id"]
+          check_items_params = {}
+          check_items_params[:name] = "Rentrer en contact avec #{lead.name}"
+          request = Typhoeus::Request.new(
+            "https://api.trello.com/1/checklists/#{checklist_id}/checkItems?&key=bd3b7da95b0121bd8bdea25ff2ab5d50&token=11e09ded7eeceabe509408840639cd97864ca350310fa85ec3cd4ab5389bd97d",
+            method: :post,
+            params: check_items_params
+          )
+          response = request.run
+
+          if response.code == 200
+            render json: {status: 'SUCCESS', message: 'Lead added to Trello !', data: lead}, status: 200
+          else
+            render json: {status: 'ERROR', message: 'An errro occured'}, status: 500
+          end
         end
-
-
-      else
-        puts "Oops, error while genrating a new lead"
+      rescue
+        render json: {status: 'ERROR', message: 'An errro occured'}, status: 500
       end
 
 
