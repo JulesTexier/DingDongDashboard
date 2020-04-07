3d567eb5c568e16fe10cfd3308e8185be7392771
fr3d100 <f.bonnand@gmail.com>
2020-04-07 11:58:23 +0200

    Clean Postmark mailer

--- app/mailers/postmark_mailer.rb
+++ app/mailers/postmark_mailer.rb
@@ -3,17 +3,9 @@ require 'postmark-rails/templated_mailer'
 class PostmarkMailer < ApplicationMailer
   include PostmarkRails::TemplatedMailerMixin
 
-  def chatbot_link
-    mail(
-      :subject => 'Accède au chatbot DingDong !',
-      :to  => 'fred@hellodingdong.com',
-      :from => 'fred@hellodingdong.com',
-      :track_opens => 'true')
-  end
-
   def send_chatbot_link(lead)
     self.template_model = { name: lead.name, actionmm_url: lead.get_chatbot_link, broker_name:lead.broker.firstname, broker_phone: lead.broker.phone, broker_email: lead.broker.email }
-    mail from: 'fred@hellodingdong.com', to: lead.email, postmark_template_alias: 'welcome'
+    mail from: 'etienne@hellodingdong.com', to: lead.email, postmark_template_alias: 'welcome'
   end
 
   def send_error_message_broker_btn(card_id, broker_firstname = "XXX")
