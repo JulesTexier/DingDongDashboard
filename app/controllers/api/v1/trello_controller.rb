3fc7eacded419d5b4dd4b66652c97ff3a623bf8d
fr3d100 <f.bonnand@gmail.com>
2020-04-07 10:06:06 +0200

    Update case a lead is not attached to a card in DB

--- app/controllers/api/v1/trello_controller.rb
+++ app/controllers/api/v1/trello_controller.rb
@@ -9,9 +9,21 @@ class Api::V1::TrelloController < ApplicationController
     document = JSON.parse(request.body.read)
     lead = Lead.where(trello_id_card: document["cardId"]).first
 
-    PostmarkMailer.send_chatbot_link(lead).deliver_now
-
-    render json: {status: 'SUCCESS', message: 'Lead found', data: lead}, status: 500
+    if !lead.nil?
+      PostmarkMailer.send_chatbot_link(lead).deliver_now
+      render json: {status: 'SUCCESS', message: 'Lead found', data: lead}, status: 200
+    else
+      trello_auth = "key=#{ENV['TRELLO_KEY']}&token=#{ENV['TRELLO_SECRET']}"
+      request = Typhoeus::Request.new(
+        "https://api.trello.com/1/cards/#{document["cardId"]}/members?" + trello_auth,
+        method: :get
+      )
+      response = request.run
+      broker_trello_id = JSON.parse(response.body)[0]["id"]
+      b = Broker.where(trello_id: broker_trello_id).first
+      PostmarkMailer.send_error_message_broker_btn(document["cardId"], b.firstname ).deliver_now
+      render json: {status: 'ERROR', message: 'Lead not found, email sent to Etienne with data about the concerned broker', data: b}, status: 500
+    end
   end
 
 
--- app/mailers/postmark_mailer.rb
+++ app/mailers/postmark_mailer.rb
@@ -12,7 +12,12 @@ class PostmarkMailer < ApplicationMailer
   end
 
   def send_chatbot_link(lead)
-    self.template_model = { name: lead.name, action_url: lead.get_chatbot_link, broker_name:lead.broker.firstname, broker_phone: lead.broker.phone, broker_email: lead.broker.email }
+    self.template_model = { name: lead.name, actionmm_url: lead.get_chatbot_link, broker_name:lead.broker.firstname, broker_phone: lead.broker.phone, broker_email: lead.broker.email }
     mail from: 'fred@hellodingdong.com', to: lead.email, postmark_template_alias: 'welcome'
+  end
+
+  def send_error_message_broker_btn(card_id, broker_firstname = "XXX")
+    self.template_model = { broker_name: broker_firstname, card_id: card_id}
+    mail from: 'fred@hellodingdong.com', to: "etienne@hellodingdong.com", postmark_template_alias: 'error-broker-btn'
   end
 end
