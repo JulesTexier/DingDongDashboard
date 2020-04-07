7b5baaf0eb1e325b8009c4123a61f6b59cfb0db5
fr3d100 <f.bonnand@gmail.com>
2020-04-07 08:08:12 +0200

    Connect to Postmark mailer to deliver email

--- app/controllers/api/v1/trello_controller.rb
+++ app/controllers/api/v1/trello_controller.rb
@@ -1,6 +1,5 @@
 require "dotenv/load"
 require "typhoeus"
-require 'postmark-rails'
 
 class Api::V1::TrelloController < ApplicationController
   protect_from_forgery with: :null_session
@@ -8,21 +7,24 @@ class Api::V1::TrelloController < ApplicationController
 
   def  send_chatbot_link_from_trello_btn
     document = JSON.parse(request.body.read)
-    lead = Lead.where(trello_id_card: document["cardId"])
+    byebug
+    lead = Lead.where(trello_id_card: document["cardId"]).first
 
-    client = Postmark::ApiClient.new(ENV['POSTMARK_API'])
-    client.deliver_with_template(
-      {:from=>"greg@hellodingdong.com",
-     :to=>lead.email,
-     :template_alias=>"welcome",
-     :template_model=>
-      {"name"=>lead.name,
-       "action_url"=>chatbot_link,
-       "broker_phone"=>lead.broker.phone,
-       "broker_email"=>lead.broker.email}}
-    )
+    PostmarkMailer.send_chatbot_link(lead).deliver_now
 
-    render json: {status: 'SUCCESS', message: 'Lead found', data: lead}, status: 500
+    # client = Postmark::ApiClient.new(ENV['POSTMARK_TOKEN'])
+    # client.deliver_with_template(
+    #   {:from=>"greg@hellodingdong.com",
+    #  :to=>lead.email,
+    #  :template_alias=>"welcome",
+    #  :template_model=>
+    #   {"name"=>lead.name,
+    #    "action_url"=>chatbot_link,
+    #    "broker_phone"=>lead.broker.phone,
+    #    "broker_email"=>lead.broker.email}}
+    # )
+
+    render json: {status: 'SUCCESS', message: 'Lead found'}, status: 500
   end
 
 
