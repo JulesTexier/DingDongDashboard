49253a37281447e202bf0d4bb61c00252cb96c87
fr3d100 <f.bonnand@gmail.com>
2020-04-08 15:35:37 +0200

    Remove puts tag

--- app/services/scrapers/independant/scraper_district_immo.rb
+++ app/services/scrapers/independant/scraper_district_immo.rb
@@ -40,7 +40,6 @@ class Independant::ScraperDistrictImmo < Scraper
           i += 1
           break if i == limit
         end
-        puts JSON.pretty_generate(hashed_property)
       rescue StandardError => e
         error_outputs(e, @source)
         next
