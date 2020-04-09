dca73bc60505fcb410cfa1aa28b72e5f2f82dfcc
Segrelove <mlesegret@gmail.com>
2020-04-09 18:05:32 +0200

    two new scrapers - STI Immo + Trust In Immobilier

--- app/services/scrapers/independant/scraper_sti_immo.rb
+++ app/services/scrapers/independant/scraper_sti_immo.rb
@@ -10,8 +10,6 @@ class Independant::ScraperStiImmo < Scraper
     @multi_page = false
     @page_nbr = 1
     @properties = []
-    # @http_type = "post"
-    # @http_request = [{}, "data%5BSearch%5D%5Boffredem%5D=0&data%5BSearch%5D%5Bidtype%5D%5B%5D=1&data%5BSearch%5D%5Bidtype%5D%5B%5D=2&data%5BSearch%5D%5Bidville%5D=&data%5BSearch%5D%5Bidville%5D=&data%5BSearch%5D%5Bprixmax%5D=&data%5BSearch%5D%5Bpiecesmin%5D=&data%5BSearch%5D%5BNO_DOSSIER%5D=&data%5BSearch%5D%5Bdistance_idvillecode%5D=&data%5BSearch%5D%5Bprixmin%5D=&data%5BSearch%5D%5Bsurfmin%5D="]
   end
 
   def launch(limit = nil)
@@ -19,11 +17,9 @@ class Independant::ScraperStiImmo < Scraper
     fetch_main_page(self).each do |item|
       begin
         hashed_property = {}
-        clean_title = access_xml_text(item, "p[itemprop='description']").strip.gsub(/[^[:print:]]/, "").gsub(' ','').remove_acc_scrp
-        is_paris = clean_title.match(/paris/i).is_a?(MatchData)
-        next if !is_paris
-        next unless (clean_title.include?("appartement") || clean_title.include?("studio") )
-        hashed_property[:area] = "750" + regex_gen(clean_title, 'paris(\d)*').gsub('paris','')
+        clean_title = access_xml_text(item, "p[itemprop='description']").strip.gsub(/[^[:print:]]/, "").gsub(" ", "").remove_acc_scrp
+        next unless clean_title.include?("appartement") || clean_title.include?("studio") || !clean_title.match(/paris/i).is_a?(MatchData)
+        hashed_property[:area] = "750" + regex_gen(clean_title, 'paris(\d)*').gsub("paris", "")
         hashed_property[:link] = "http://www.sti-immo.com" + access_xml_link(item, "a:nth-child(1)", "href")[0].to_s
         hashed_property[:price] = access_xml_text(item, "span[itemprop='price']").to_int_scrp
         hashed_property[:rooms_number] = regex_gen(clean_title, '(\d+)(.?)(pi(è|e)ce(s?))').to_int_scrp
@@ -31,27 +27,25 @@ class Independant::ScraperStiImmo < Scraper
         if go_to_prop?(hashed_property, 7)
           html = fetch_static_page(hashed_property[:link])
           hashed_property[:description] = access_xml_text(html, "p.description").strip
-
-          details = access_xml_text(html, "#infos").strip.gsub(/[^[:print:]]/, "").downcase.gsub(' ','').remove_acc_scrp
-          if details.match(/etage(\d)*/i).is_a?(MatchData) 
+          details = access_xml_text(html, "#infos").strip.gsub(/[^[:print:]]/, "").downcase.gsub(" ", "").remove_acc_scrp
+          if details.match(/etage(\d)*/i).is_a?(MatchData)
             hashed_property[:floor] = regex_gen(details, 'etage(\d)*').to_int_scrp
-          else 
+          else
             hashed_property[:floor] = perform_elevator_regex(hashed_property[:description])
           end
-          if details.match(/ascenseur(oui|non)/i).is_a?(MatchData) 
-            regex_gen(details, 'ascenseur(oui|non)').include?('oui') ? hashed_property[:has_elevator] = true : hashed_property[:has_elevator] = false
-          else 
+          if details.match(/ascenseur(oui|non)/i).is_a?(MatchData)
+            regex_gen(details, "ascenseur(oui|non)").include?("oui") ? hashed_property[:has_elevator] = true : hashed_property[:has_elevator] = false
+          else
             hashed_property[:has_elevator] = perform_elevator_regex(hashed_property[:description])
           end
-
           hashed_property[:bedrooms_number] = access_xml_text(html, ".little-infos > div:nth-child(3) > div:nth-child(2) > h5:nth-child(1)").to_int_scrp
           hashed_property[:flat_type] = get_type_flat(clean_title)
-          hashed_property[:agency_name] = access_xml_text(html, '.agence > div >  h5')
-          hashed_property[:contact_number] = access_xml_text(html, 'span.telagence').gsub(' ','').gsub(/[^[:print:]]/, "")
+          hashed_property[:agency_name] = access_xml_text(html, ".agence > div >  h5")
+          hashed_property[:contact_number] = access_xml_text(html, "span.telagence").gsub(" ", "").gsub(/[^[:print:]]/, "").convert_phone_nbr_scrp
           hashed_property[:subway_ids] = perform_subway_regex(access_xml_text(html, 'h1[itemprop="name"]') + hashed_property[:description])
           hashed_property[:provider] = "Agence"
           hashed_property[:source] = @source
-          hashed_property[:images] = access_xml_link(html, ".imgBien", "src")
+          hashed_property[:images] = access_xml_link(html, ".imgBien", "src").map { |img| "https:" + img }
           @properties.push(hashed_property) ##testing purpose
           enrich_then_insert_v2(hashed_property)
           i += 1
--- app/services/scrapers/independant/scraper_trust_in_immobilier.rb
+++ app/services/scrapers/independant/scraper_trust_in_immobilier.rb
@@ -19,28 +19,28 @@ class Independant::ScraperTrustInImmobilier < Scraper
         hashed_property = {}
         hashed_property[:link] = access_xml_link(item, ".recherche-annonces-lien", "href")[0].to_s
         hashed_property[:area] = perform_district_regex(access_xml_text(item, ".ville-annonce"))
-        hashed_property[:price] = access_xml_text(item, 'span[itemprop="price"]')[0..access_xml_text(item, 'span[itemprop="price"]').length/2].to_int_scrp
-        details = access_xml_text(item, '.recherche-annonces-infos').strip.gsub(/[^[:print:]]/, "").downcase.remove_acc_scrp.gsub(' ','')
-        details = details[0..details.length/2]
+        hashed_property[:price] = access_xml_text(item, 'span[itemprop="price"]')[0..access_xml_text(item, 'span[itemprop="price"]').length / 2].to_int_scrp
+        details = access_xml_text(item, ".recherche-annonces-infos").strip.gsub(/[^[:print:]]/, "").downcase.remove_acc_scrp.gsub(" ", "")
+        details = details[0..details.length / 2]
         hashed_property[:rooms_number] = regex_gen(details, "piece[(]s[)]([0123456789])*").to_int_scrp
         hashed_property[:surface] = regex_gen(details, "surface([0123456789])*").to_int_scrp
-          if go_to_prop?(hashed_property, 7)
-            html = fetch_static_page(hashed_property[:link])
-            hashed_property[:description] = access_xml_text(html, "p[itemprop='description']").strip
-            hashed_property[:bedrooms_number] = regex_gen(details, "chambre[(]s[)]([0123456789])*").to_int_scrp
-            hashed_property[:flat_type] = get_type_flat(access_xml_text(item, "div[itemprop='name']"))
-            hashed_property[:agency_name] = @source
-            hashed_property[:contact_number] = access_xml_text(html, "#numero-telephonez-nous-detail").gsub(' ','').gsub(/[^[:print:]]/, "")[0..9]
-            if !access_xml_text(html, 'li[title="Etage"]').empty?
-              hashed_property[:floor] = access_xml_text(html, 'li[title="Etage"]').gsub(' ','').gsub(/[^[:print:]]/, "").to_int_scrp
-            else 
-              perform_floor_regex(hashed_property[:description])
-            end
-            if !access_xml_text(html, 'li[title="Ascenseur"]').empty?
-              access_xml_text(html, 'li[title="Ascenseur"]').include?('oui') ? hashed_property[:has_elevator] = true : hashed_property[:has_elevator] = false
-            else 
-              hashed_property[:has_elevator] = perform_elevator_regex(hashed_property[:description])
-            end
+        if go_to_prop?(hashed_property, 7)
+          html = fetch_static_page(hashed_property[:link])
+          hashed_property[:description] = access_xml_text(html, "p[itemprop='description']").strip
+          hashed_property[:bedrooms_number] = regex_gen(details, "chambre[(]s[)]([0123456789])*").to_int_scrp
+          hashed_property[:flat_type] = get_type_flat(access_xml_text(item, "div[itemprop='name']"))
+          hashed_property[:agency_name] = @source
+          hashed_property[:contact_number] = access_xml_text(html, "#numero-telephonez-nous-detail").gsub(" ", "").gsub(/[^[:print:]]/, "")[0..9]
+          if !access_xml_text(html, 'li[title="Etage"]').empty?
+            hashed_property[:floor] = access_xml_text(html, 'li[title="Etage"]').gsub(" ", "").gsub(/[^[:print:]]/, "").to_int_scrp
+          else
+            perform_floor_regex(hashed_property[:description])
+          end
+          if !access_xml_text(html, 'li[title="Ascenseur"]').empty?
+            access_xml_text(html, 'li[title="Ascenseur"]').include?("oui") ? hashed_property[:has_elevator] = true : hashed_property[:has_elevator] = false
+          else
+            hashed_property[:has_elevator] = perform_elevator_regex(hashed_property[:description])
+          end
           hashed_property[:subway_ids] = perform_subway_regex(hashed_property[:description])
           hashed_property[:provider] = "Agence"
           hashed_property[:source] = @source
