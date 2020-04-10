85339c5bd1055b5e471969da699caf9172709bd8
Segrelove <mlesegret@gmail.com>
2020-04-10 14:44:16 +0200

    new scraper - Immopolis, thanks to nicodacostouille

--- spec/services/scrapers/scraper_immopolis_spec.rb
+++ spec/services/scrapers/scraper_immopolis_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
-RSpec.describe ScraperImmopolis, type: :service do
+RSpec.describe Group::ScraperImmopolis, type: :service do
   before(:all) do
-    @s = ScraperImmopolis.new
+    @s = Group::ScraperImmopolis.new
   end
 
   it "should launch and return proper number of properties" do
