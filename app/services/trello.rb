47b5ff1410d231fc571d08bc169254da2e90ce5d
fr3d100 <f.bonnand@gmail.com>
2020-04-10 14:39:50 +0200

    Improve code organisation and logic

--- app/services/trello.rb
+++ app/services/trello.rb
@@ -8,37 +8,41 @@ class Trello
     @token = "key=#{ENV['TRELLO_KEY']}&token=#{ENV['TRELLO_SECRET']}"
   end
 
-  def add_comment_to_card(card_id, comment)
-    
-  end
-
   def add_new_lead_on_trello(lead)
-    
+    is_ok = false
     # 1• Create card on tello Board 
     list_id = lead.broker.trello_lead_list_id
     params = {}
-    params[:name] = lead_hash[:name]
+    params[:name] = lead.name
     params[:desc] = lead.trello_description
     params[:pos] = 'top'
     params[:due] = Time.now + 15.minutes
-    params[:idMembers] = b.trello_id
-    new_card_response = @trello.create_new_card(list_id, params)
-
+    params[:idMembers] = lead.broker.trello_id
+    new_card_response = create_new_card(list_id, params)
+    return is_ok if new_card_response.code != 200
+    
     # 2• Add checklist 'Action' to created card
     card_id = JSON.parse(new_card_response.body)["id"]
     lead.update(trello_id_card: card_id)
-    new_checklist_response = @trello.add_checklist_to_card(card_id)
+    new_checklist_response = add_checklist_to_card(card_id)
+    return is_ok if new_checklist_response.code != 200
 
+    
     # 3• Add first action on the checklist
     checklist_id = JSON.parse(new_checklist_response.body)["id"]
     check_items_params = {}
     check_items_params[:name] = "Rentrer en contact avec #{lead.name}"
-    new_checkitem_response = @trello.add_checkitem_to_checklist(checklist_id, check_items_params)
-
+    new_checkitem_response = add_checkitem_to_checklist(checklist_id, check_items_params)
+    return is_ok if new_checkitem_response.code != 200
+    return true
+  end
+  
+  def add_comment_to_card(card_id, comment)
+    
   end
 
   private 
-
+  
   def create_new_card(list_id, params)
     request = Typhoeus::Request.new(
       "https://api.trello.com/1/cards?idList=#{list_id}&" + @token,
@@ -52,7 +56,7 @@ class Trello
     checklist_params = {}
     checklist_params[:name] = "ACTIONS"
     request = Typhoeus::Request.new(
-      "https://api.trello.com/1/cards/#{card_id}/checklists?" + trello_auth,
+      "https://api.trello.com/1/cards/#{card_id}/checklists?" + @token,
       method: :post,
       params: checklist_params
     )
@@ -61,7 +65,7 @@ class Trello
 
   def add_checkitem_to_checklist(checklist_id, params)
     request = Typhoeus::Request.new(
-      "https://api.trello.com/1/checklists/#{checklist_id}/checkItems?" + trello_auth,
+      "https://api.trello.com/1/checklists/#{checklist_id}/checkItems?" + @token,
       method: :post,
       params: params
     )
