581e1dcf74bfa49e31d77a354b43a6d07a1a9751
fr3d100 <f.bonnand@gmail.com>
2020-04-13 10:08:46 +0200

    Update algorithme to match Broker depending on time

--- app/models/broker.rb
+++ app/models/broker.rb
@@ -3,8 +3,30 @@ class Broker < ApplicationRecord
   has_many :leads
 
   def self.get_current_broker
-    return self.first
-  #   week_day = Time.now.wday
+    b = self.where(trello_username:"gregrouxeloldra").first
+
+    morning_end = 13
+    afternooon_end = 18
+    date = Time.now.in_time_zone("Paris")
+
+    case date.wday
+    when 2 #Mardi  : Matin Aurélien, Aprem : Mélanie
+      if date.hour < morning_end
+        b = self.where(trello_username:"aurelienguichard1").first
+      elsif date.hour < afternooon_end
+        b = self.where(trello_username:"melanieramon2").first
+      end
+    when 4 #Jeudi  : Matin Mélanie, Aprem : Aurélien
+      if date.hour < morning_end
+        b = self.where(trello_username:"melanieramon2").first
+      elsif date.hour < afternooon_end
+        b = self.where(trello_username:"aurelienguichard1").first
+      end
+    else
+    end
+    
+    return b
+
   end
 
 end
