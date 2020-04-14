ddfe1801370fe668423f2c7a1fe3f75ee85e585f
fr3d100 <f.bonnand@gmail.com>
2020-04-14 11:14:32 +0200

    Update syntaxe error in migration file

--- db/migrate/20200414091205_add_min_rooms_number_and_lastname_to_lead.rb
+++ db/migrate/20200414091205_add_min_rooms_number_and_lastname_to_lead.rb
@@ -1,7 +1,7 @@
 class AddMinRoomsNumberAndLastnameToLead < ActiveRecord::Migration[6.0]
   def change
     rename_column :leads, :name, :firstname
-    add_column :lead, :lastname, :string
+    add_column :leads, :lastname, :string
     add_column :leads, :min_rooms_number, :integer
   end
 end
