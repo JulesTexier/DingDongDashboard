00d0e29b643675010f6720ba3775b31aa7656d61
Segrelove <mlesegret@gmail.com>
2020-04-15 18:21:47 +0200

    new scraper - La Maison Immo

--- app/services/scrapers/independant/scraper_la_maison_immo.rb
+++ app/services/scrapers/independant/scraper_la_maison_immo.rb
@@ -17,15 +17,15 @@ class Independant::ScraperLaMaisonImmo < Scraper
     fetch_main_page(self).each do |item|
       begin
         hashed_property = {}
-        hashed_property[:link] = "https://www.lamaisonimmo.fr" + access_xml_link(item, "a:nth-child(1)", "href")[0].to_s.gsub('..','')
+        hashed_property[:link] = "https://www.lamaisonimmo.fr" + access_xml_link(item, "a:nth-child(1)", "href")[0].to_s.gsub("..", "")
         hashed_property[:area] = perform_district_regex(access_xml_text(item, "a:nth-child(1) > h3"))
-        hashed_property[:price] = regex_gen(access_xml_text(item, "span.listing_price").gsub(' ',''), '(\d)*(.)*(dont)').to_int_scrp
+        access_xml_text(item, "span.listing_price").include?("dont") ? hashed_property[:price] = regex_gen(access_xml_text(item, "span.listing_price").gsub(" ", ""), '(\d)*(.)*(dont)').to_int_scrp : hashed_property[:price] = access_xml_text(item, "span.listing_price").to_int_scrp
         hashed_property[:rooms_number] = regex_gen(access_xml_text(item, ".listing_criteres"), '(\d+)(\s)Pi').to_int_scrp
         hashed_property[:surface] = regex_gen(access_xml_text(item, ".listing_criteres"), '(\d+(,?)(\d*))(.)(m)').to_int_scrp
         if go_to_prop?(hashed_property, 7)
           html = fetch_static_page(hashed_property[:link])
           hashed_property[:description] = access_xml_text(html, ".description").strip
-          hashed_property[:bedrooms_number] = regex_gen(access_xml_text(item, ".listing_criteres").gsub(' ',''), '(,)(\d*)(Ch)').to_int_scrp if access_xml_text(item, ".listing_criteres").gsub(' ','').match(/(,)(\d*)(Ch)/i).is_a?(MatchData)
+          hashed_property[:bedrooms_number] = regex_gen(access_xml_text(item, ".listing_criteres").gsub(" ", ""), '(,)(\d*)(Ch)').to_int_scrp if access_xml_text(item, ".listing_criteres").gsub(" ", "").match(/(,)(\d*)(Ch)/i).is_a?(MatchData)
           hashed_property[:flat_type] = get_type_flat(hashed_property[:description])
           hashed_property[:floor] = perform_floor_regex(hashed_property[:description])
           hashed_property[:has_elevator] = perform_elevator_regex(hashed_property[:description])
@@ -35,7 +35,7 @@ class Independant::ScraperLaMaisonImmo < Scraper
           hashed_property[:images] = []
           images = access_xml_link(html, "div.img", "style")
           images.each do |image_url|
-            hashed_property[:images].push("https://www.lamaisonimmo.fr" + regex_gen(image_url.gsub!("background:url('..", ""), '(.)*(.jpg)'))
+            hashed_property[:images].push("https://www.lamaisonimmo.fr" + regex_gen(image_url.gsub!("background:url('..", ""), "(.)*(.jpg)"))
           end
           @properties.push(hashed_property) ##testing purpose
           enrich_then_insert_v2(hashed_property)
