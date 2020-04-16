c18fa7b38bad33873f7ca22833787ee83615f17b
fr3d100 <f.bonnand@gmail.com>
2020-04-16 16:50:13 +0200

    Scraper logic finished but ther is an issue with VCR

--- app/services/scrapers/independant/scraper_bonjour_oscar.rb
+++ app/services/scrapers/independant/scraper_bonjour_oscar.rb
@@ -25,33 +25,29 @@ class Independant::ScraperBonjourOscar < Scraper
           hashed_property[:surface] = 30
           if go_to_prop?(hashed_property, 7)     
             html = fetch_static_page(hashed_property[:link])
-            hashed_property[:surface] = access_xml_text(html, "span.spaces-value").to_float_to_int_scrp
-        #     hashed_property[:rooms_number] = regex_gen(access_xml_text(html, "h2"), '(\d+)(.?)(pi(è|e)ce(s?))').to_float_to_int_scrp
-        #     hashed_property[:rooms_number] == 1 ? hashed_property[:bedrooms_number] = 1 : hashed_property[:bedrooms_number] = regex_gen(access_xml_text(html, "#infos > p:nth-child(7) > span.valueInfos"), '(\d+)').to_int_scrp
-        #     hashed_property[:description] = access_xml_text(html, "article.col-md-6 > p").strip
-        #     hashed_property[:flat_type] = regex_gen(access_xml_text(item, "h2"), "((a|A)ppartement|(A|a)ppartements|(S|s)tudio|(S|s)tudette|(C|c)hambre|(M|m)aison)")
-        #     hashed_property[:agency_name] = "Les Parisiennes"
-        #     hashed_property[:floor] = perform_floor_regex(hashed_property[:description])
-        #     hashed_property[:has_elevator] = perform_elevator_regex(hashed_property[:description])
-        #     hashed_property[:subway_ids] = perform_subway_regex(hashed_property[:description])
-        #     hashed_property[:provider] = "Agence"
-        #     hashed_property[:source] = @source
-        #     hashed_property[:images] = access_xml_link(html, "ul > li > img", "src")
-        #     hashed_property[:images].collect! { |img| "https:" + img }
-            #   @properties.push(hashed_property) ##testing purpose
-            #   enrich_then_insert_v2(hashed_property)
-            #   i += 1
-            #   break if i == limit
-        # byebug
+            hashed_property[:surface] = access_xml_text(html, "li.spaces-item:nth-child(1) > span:nth-child(2)").to_float_to_int_scrp
+            hashed_property[:bedroom_number] = access_xml_text(html, "li.spaces-item:nth-child(2) > span:nth-child(2)").to_int_scrp
+           hashed_property[:description] = access_xml_text(html, ".description-content").strip
+            hashed_property[:floor] = access_xml_text(html, "li.spaces-item:nth-child(4) > span:nth-child(2)").to_int_scrp
+            hashed_property[:has_elevator] = perform_elevator_regex(hashed_property[:description])
+            hashed_property[:subway_ids] = perform_subway_regex(hashed_property[:description])
+            hashed_property[:provider] = "Agence"
+            hashed_property[:source] = @source
+            hashed_property[:agency_name] = @source + " - " + access_xml_text(html, '.agencyTeaser-title') 
+            hashed_property[:contact_number] = access_xml_text(html, '.agencyTeaser-phone').gsub(" ","")
+            hashed_property[:images] = access_xml_link(html, "img.sliderGood-image", "src")
+      
+              @properties.push(hashed_property) ##testing purpose
+              enrich_then_insert_v2(hashed_property)
+              i += 1
+              break if i == limit
           end
-        #   byebug
         rescue StandardError => e
             error_outputs(e, @source)
           next
         end
         puts JSON.pretty_generate(hashed_property)
       end
-    #   byebug
       return @properties
     end
   end
