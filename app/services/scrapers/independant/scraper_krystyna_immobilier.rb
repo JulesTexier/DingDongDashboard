ad97763182d33d888e020a201207317075afca69
Segrelove <mlesegret@gmail.com>
2020-04-16 18:18:37 +0200

    removed puts and new fixtures

--- app/services/scrapers/independant/scraper_krystyna_immobilier.rb
+++ app/services/scrapers/independant/scraper_krystyna_immobilier.rb
@@ -38,13 +38,11 @@ class Independant::ScraperKrystynaImmobilier < Scraper
           i += 1
           break if i == limit
         end
-        puts JSON.pretty_generate(hashed_property)
       rescue StandardError => e
         error_outputs(e, @source)
         next
       end
     end
-    puts @properties.length
     return @properties
   end
 end
