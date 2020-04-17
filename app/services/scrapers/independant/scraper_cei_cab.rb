8d505a40f3c058743ecd8697fad72897053126a9
Segrelove <mlesegret@gmail.com>
2020-04-17 12:16:24 +0200

    made changes for images, it was hidden in a list transformed by JS on the browser, so misleading

--- app/services/scrapers/independant/scraper_cei_cab.rb
+++ app/services/scrapers/independant/scraper_cei_cab.rb
@@ -17,19 +17,19 @@ class Independant::ScraperCeiCab < Scraper
     fetch_main_page(self).each do |item|
       begin
         hashed_property = {}
-        title = access_xml_text(item, 'h2[itemprop="name"]').gsub(" ","").gsub(/[^[:print:]]/, "")
+        title = access_xml_text(item, 'h2[itemprop="name"]').gsub(" ", "").gsub(/[^[:print:]]/, "")
         next if (access_xml_text(item, ".vignette-compromis").downcase.include?("compromis") || !access_xml_text(item, "h3").downcase.include?("paris"))
-        hashed_property[:link] = "https://www.cei-cab.com" + access_xml_link(item, 'h2[itemprop="name"] > a', 'href')[0]
+        hashed_property[:link] = "https://www.cei-cab.com" + access_xml_link(item, 'h2[itemprop="name"] > a', "href")[0]
         hashed_property[:surface] = access_xml_text(item, ".surface").to_float_to_int_scrp
         hashed_property[:area] = perform_district_regex(access_xml_text(item, "h3"))
-        hashed_property[:rooms_number] = regex_gen(access_xml_text(item, 'h2[itemprop="name"]').gsub(" ","").gsub(/[^[:print:]]/, ""),'(\d)*pi').to_int_scrp
-        hashed_property[:price] = access_xml_link(item, "span[itemprop='price']", 'content')[0].to_int_scrp
+        hashed_property[:rooms_number] = regex_gen(access_xml_text(item, 'h2[itemprop="name"]').gsub(" ", "").gsub(/[^[:print:]]/, ""), '(\d)*pi').to_int_scrp
+        hashed_property[:price] = access_xml_link(item, "span[itemprop='price']", "content")[0].to_int_scrp
         if go_to_prop?(hashed_property, 7)
           html = fetch_static_page(hashed_property[:link])
-          hashed_property[:bedrooms_number] = access_xml_text(html, '.chambre').to_int_scrp
+          hashed_property[:bedrooms_number] = access_xml_text(html, ".chambre").to_int_scrp
           hashed_property[:description] = access_xml_text(html, 'p[itemprop="description"]').strip
-          hashed_property[:floor] = access_xml_text(html, 'div.caracteristique-bloc:nth-child(4) > ul:nth-child(2) > li:nth-child(5) > strong').to_int_scrp
-          if access_xml_text(html, '.caracteristiques-divers').gsub(" ","").downcase.gsub(/[^[:print:]]/, "").match(/ascenseur:oui/i).is_a?(MatchData)
+          hashed_property[:floor] = access_xml_text(html, "div.caracteristique-bloc:nth-child(4) > ul:nth-child(2) > li:nth-child(5) > strong").to_int_scrp
+          if access_xml_text(html, ".caracteristiques-divers").gsub(" ", "").downcase.gsub(/[^[:print:]]/, "").match(/ascenseur:oui/i).is_a?(MatchData)
             hashed_property[:has_elevator] = true
           else
             hashed_property[:has_elevator] = perform_elevator_regex(hashed_property[:description])
@@ -37,8 +37,8 @@ class Independant::ScraperCeiCab < Scraper
           hashed_property[:subway_ids] = perform_subway_regex(access_xml_text(html, ".localisation"))
           hashed_property[:provider] = "Agence"
           hashed_property[:source] = @source
-          hashed_property[:agency_name] = access_xml_text(html, '.negociateur-infos > h3')
-          hashed_property[:images] = access_xml_link(item, "img[itemprop='image']", "src")
+          hashed_property[:agency_name] = access_xml_text(html, ".negociateur-infos > h3")
+          hashed_property[:images] = access_xml_link(html, "div#photoslider > ul > li > a", "href")
           @properties.push(hashed_property) ##testing purpose
           enrich_then_insert_v2(hashed_property)
           i += 1
