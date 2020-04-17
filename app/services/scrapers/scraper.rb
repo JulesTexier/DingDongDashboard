c012f9f7556040304fcd84b9ea92e96922d7f68a
Segrelove <mlesegret@gmail.com>
2020-04-17 12:06:30 +0200

    added new entry for get_type_flat, really handy this method

--- app/services/scrapers/scraper.rb
+++ app/services/scrapers/scraper.rb
@@ -279,6 +279,7 @@ class Scraper
     flat_type = "Studio" if str.downcase.include? "studio"
     flat_type = "Cave" if str.downcase.include? "cave"
     flat_type = "Parking" if str.downcase.include? "parking"
+    flat_type = "Bureaux" if str.downcase.include? "bureaux"
     flat_type = "Hotel particulier" if str.downcase.include? "hotel particulier"
     return flat_type
   end
