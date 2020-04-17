84cf361d186e95a12eafd277a99b29806141ffac
Segrelove <mlesegret@gmail.com>
2020-04-17 12:30:31 +0200

    added changes for my merge on dev scrap

--- app/services/scrapers/independant/scraper_acopa_immobilier.rb
+++ app/services/scrapers/independant/scraper_acopa_immobilier.rb
@@ -15,16 +15,17 @@ class Independant::ScraperAcopaImmobilier < Scraper
   end
 
   def launch(limit = nil)
-    i = 0
     # These tricky lines are made to get the nonce variable, required for authenticity of the post request
+    # https://i.ytimg.com/vi/KEkrWRHCDQU/maxresdefault.jpg
     doc = fetch_static_page("https://www.acopa-immobilier.fr/")
-    doc.css('script').each do |script|
+    doc.css("script").each do |script|
       if script.content.include?("nonce")
-        nonce = regex_gen(regex_gen(script.content, "var nonce = (.)*'"), "'(.)*'").gsub("'",'')
+        nonce = regex_gen(regex_gen(script.content, "var nonce = (.)*'"), "'(.)*'").gsub("'", "")
         @http_request[1] = "action=corn_realestateSearch&nonce=#{nonce}&param=budget_max=1560000&surface_min=0&rooms_min=0&ville=&typetransacselected=vente&mapselected=paris&zoneselected=&lastcriteria=undefined"
         break if !nonce.empty?
       end
     end
+    i = 0
     fetch_main_page(self).each do |item|
       begin
         hashed_property = {}
@@ -36,21 +37,17 @@ class Independant::ScraperAcopaImmobilier < Scraper
         if go_to_prop?(hashed_property, 7)
           html = fetch_static_page(hashed_property[:link])
           hashed_property[:description] = access_xml_text(html, "div.col-md-3:nth-child(8)").strip
-          details = access_xml_text(html, '.fiche').strip.gsub(" ", "").gsub(/[^[:print:]]/, "")
+          details = access_xml_text(html, ".fiche").strip.gsub(" ", "").gsub(/[^[:print:]]/, "")
           hashed_property[:bedrooms_number] = regex_gen(details, 'Chambres:(\d){1,}').to_int_scrp
           hashed_property[:flat_type] = get_type_flat(access_xml_text(item, "h3"))
           hashed_property[:floor] = perform_floor_regex(hashed_property[:description])
           hashed_property[:has_elevator] = perform_elevator_regex(hashed_property[:description])
           hashed_property[:subway_ids] = perform_subway_regex(access_xml_text(item, "h3"))
           hashed_property[:agency_name] = access_xml_text(html, "#contactagences > strong")
-          hashed_property[:contact_number] = access_xml_link(html, "a.tel", "href")[0].gsub(' ','').gsub("tel:","")
+          hashed_property[:contact_number] = access_xml_link(html, "a.tel", "href")[0].gsub(" ", "").gsub("tel:", "")
           hashed_property[:provider] = "Agence"
           hashed_property[:source] = @source
-          raw_images = access_xml_link(html, 'a.thb','data-image')
-          hashed_property[:images] = []
-          raw_images.each do |img|
-            hashed_property[:images].push(img) if !img.nil?
-          end
+          hashed_property[:images] = access_xml_link(html, "a.thb", "data-image").select { |img| !img.nil? }
           @properties.push(hashed_property) ##testing purpose
           enrich_then_insert_v2(hashed_property)
           i += 1
