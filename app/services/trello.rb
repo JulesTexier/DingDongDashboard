0ba141a90bc32e42460275ebff618b91162c42f0
fr3d100 <f.bonnand@gmail.com>
2020-04-18 17:28:01 +0200

    Add method to add_label_on card and the specific one for old users + add method to get the label_id according to board and label name

--- app/services/trello.rb
+++ app/services/trello.rb
@@ -76,15 +76,10 @@ class Trello
 
   def add_comment_to_lead_card(lead, comment)
     card_id = lead.trello_id_card
-    check_items_params = {}
-    check_items_params[:text] = comment + " @#{lead.broker.trello_username}"
+    params = {}
+    params[:text] = comment + " @#{lead.broker.trello_username}"
     if !card_id.nil? && !lead.broker.trello_username.nil?
-      request = Typhoeus::Request.new(
-        "https://api.trello.com/1/cards/#{card_id}/actions/comments?" + @token,
-        method: :post,
-        params: check_items_params
-      )
-      response = request.run
+      add_comment_to_card(card_id, params)
     end
   end
 
@@ -95,6 +90,16 @@ class Trello
     else 
       return false
     end
+  end
+
+  def add_label_old_user(lead)
+    # Add label on card 
+    label_id = get_label_id_by_name(lead.broker.trello_board_id, "UTILISATEUR HISTORIQUE DING DONG")
+    add_label_to_card(lead.trello_id_card, label_id)
+    # Add comment on carte to advise broker 
+    params = {}
+    params[:text] = "Utilisateur déjà sur le chatbot Ding Dong mais n'ayant jamais pris rdv avec un courtier Ding Dong" + " @#{lead.broker.trello_username}"
+    add_comment_to_card(lead.trello_id_card, params)
   end
 
   private 
@@ -137,6 +142,40 @@ class Trello
       params: params
     )
     response = request.run
+  end
+
+  def add_label_to_card(card_id, label_id)
+    checklist_params = {}
+    checklist_params[:value] = label_id
+    request = Typhoeus::Request.new(
+      "https://api.trello.com/1/cards/#{card_id}/idLabels?" + @token,
+      method: :post,
+      params: checklist_params
+    )
+    response = request.run
+  end
+
+  def add_comment_to_card(card_id, params)
+    request = Typhoeus::Request.new(
+      "https://api.trello.com/1/cards/#{card_id}/actions/comments?" + @token,
+      method: :post,
+      params: params
+    )
+    response = request.run
+  end
+
+  def get_label_id_by_name(board_id, name)
+    label_id = nil
+    request = Typhoeus::Request.new(
+      "https://api.trello.com/1/boards/#{board_id}/labels?" + @token,
+      method: :get
+    )
+    response = request.run
+    labels = JSON.parse(response.body)
+    labels.each do |label|
+      label_id = label["id"] if label["name"] == name 
+    end
+    return label_id
   end
 
 end
