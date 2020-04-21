52c3c5f7e9dece385527b90a41645090e294efe8
fr3d100 <f.bonnand@gmail.com>
2020-04-21 16:26:32 +0200

    Handle lead creation form

--- app/controllers/lead_controller.rb
+++ app/controllers/lead_controller.rb
@@ -1,7 +1,8 @@
 class LeadController < ApplicationController
-  def create
+  def new
     @lead = Lead.new
     @areas = Area.all
+    @brokers = Broker.all
     @broker_select = []
     @broker_select << ["Chosir...", ""]
     Broker.all.each do |broker|
@@ -9,9 +10,25 @@ class LeadController < ApplicationController
     end
   end
 
-  def handle_lead_broker
-    permitted = params.require(:lead).permit(:firstname, :lastname)
-    lead = Lead.new(permitted)
-    byebug
+  def create
+    lead = Lead.new(lead_params)
+    lead.areas = params[:selected_areas].join(",") if !params[:selected_areas].nil?
+    lead.has_messenger = true
+    lead.status = "broker_form_filled"
+    lead.source += " (lead généré par le courtier)"
+    if lead.save 
+      flash[:success] = "Le contact a bien été créé ! Tu peux le retrouver dans ton Trello"
+      redirect_to :action => "new"
+    else 
+      flash[:danger] = "Une erreur s'est produite ..."
+      puts "ohoh, probleme"
+    end
   end
+
+  private
+
+  def lead_params
+    params.require(:lead).permit(:firstname, :lastname, :email, :phone, :has_messenger, :max_price, :min_surface, :min_rooms_number, :project_type, :additional_question, :specific_criteria, :areas, :status, :source, :broker_id, :has_messenger)
+  end 
+
 end
