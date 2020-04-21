b79f2c7a9313c7808a3d9eaecb0f3caf4e159c57
fr3d100 <f.bonnand@gmail.com>
2020-04-21 15:03:53 +0200

    Write first model test (test factory)

--- spec/models/lead_spec.rb
+++ spec/models/lead_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Lead, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
+
+  describe Lead do
+    describe "model" do
+      it "has a valid factory" do
+        expect(build(:lead)).to be_valid
+      end
+    end
+  end
 end
