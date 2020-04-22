2722b1c70d5e11445d15ac8e0c7891b427c74d38
Segrelove <mlesegret@gmail.com>
2020-04-22 12:46:41 +0200

    new scraper - Daniel Feau

--- app/services/scrapers/group/scraper_daniel_feau.rb
+++ app/services/scrapers/group/scraper_daniel_feau.rb
@@ -3,7 +3,7 @@ class Group::ScraperDanielFeau < Scraper
 
   def initialize
     @url = "https://danielfeau.com/fr/acheter/paris-neuilly-boulogne-saint-cloud"
-    @source = "Arthurimmo"
+    @source = "Daniel Feau"
     @main_page_cls = "li.property.initial"
     @type = "Static"
     @waiting_cls = nil
@@ -16,7 +16,6 @@ class Group::ScraperDanielFeau < Scraper
     i = 0
     fetch_main_page(self).each do |item|
       begin
-        byebug
         hashed_property = {}
         hashed_property[:link] = "https://danielfeau.com" + access_xml_link(item, "a", "href")[0]
         hashed_property[:surface] = regex_gen(access_xml_text(item, "h3 > span"), '(\d+)(.?)(\d+)(.?)(m)').to_float_to_int_scrp
@@ -26,22 +25,22 @@ class Group::ScraperDanielFeau < Scraper
         hashed_property[:price] = access_xml_text(item, "span.price").to_int_scrp
         hashed_property[:flat_type] = get_type_flat(access_xml_text(item, "h3 > span"))
         next if hashed_property[:area] == "N/C"
-        # if go_to_prop?(hashed_property, 7)
-        #   html = fetch_static_page(hashed_property[:link])
-        #   hashed_property[:description] = access_xml_text(html, "div.detail-accordeon-item-txt > p").strip
-        #   hashed_property[:floor] = perform_floor_regex(hashed_property[:description])
-        #   hashed_property[:has_elevator] = perform_elevator_regex(hashed_property[:description])
-        #   hashed_property[:subway_ids] = perform_subway_regex(access_xml_text(html, ".localisation"))
-        #   hashed_property[:provider] = "Agence"
-        #   hashed_property[:source] = @source
-        #   hashed_property[:agency_name] = access_xml_text(html, 'div[itemprop="name"]')
-        #   hashed_property[:contact_number] = access_xml_text(html, "#numero-telephonez-nous-detail").gsub(" ", "").gsub(/[^[:print:]]/, "")[0..9].convert_phone_nbr_scrp
-        #   hashed_property[:images] = access_xml_link(html, "div.item > img", "src").map { |img| img.gsub("h_175,w_175", "h_600,w_800") }
-        #   @properties.push(hashed_property) ##testing purpose
-        #   enrich_then_insert_v2(hashed_property)
-        #   i += 1
-        #   break if i == limit
-        # end
+        if go_to_prop?(hashed_property, 7)
+          html = fetch_static_page(hashed_property[:link])
+          hashed_property[:description] = access_xml_text(html, "p.comment").tr("\r\n", "").strip
+          hashed_property[:floor] = perform_floor_regex(hashed_property[:description])
+          hashed_property[:has_elevator] = perform_elevator_regex(hashed_property[:description])
+          hashed_property[:subway_ids] = perform_subway_regex(access_xml_text(html, ".localisation"))
+          hashed_property[:provider] = "Agence"
+          hashed_property[:source] = @source
+          hashed_property[:agency_name] = access_xml_text(html, "article.agency > div.info > h2 > a")
+          hashed_property[:contact_number] = access_xml_text(html, "article.agency > div.info > p > span.phone").gsub("(0)", "")
+          hashed_property[:images] = access_xml_link(html, "img.picture", "src").uniq
+          @properties.push(hashed_property) ##testing purpose
+          enrich_then_insert_v2(hashed_property)
+          i += 1
+          break if i == limit
+        end
       rescue StandardError => e
         error_outputs(e, @source)
         next
