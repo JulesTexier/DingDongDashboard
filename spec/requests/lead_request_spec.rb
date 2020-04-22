7835a855cbae21e2bee69c1492bc75135b0fec37
fr3d100 <f.bonnand@gmail.com>
2020-04-22 08:43:42 +0200

    Remove pending tests

--- spec/requests/lead_request_spec.rb
+++ spec/requests/lead_request_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "Leads", type: :request do
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/lead/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET /create" do
+  #   it "returns http success" do
+  #     get "/lead/create"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 end
