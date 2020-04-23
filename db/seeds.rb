baa2236ecfddd61297febf3b3a595773659fe1a1
fr3d100 <f.bonnand@gmail.com>
2020-04-23 16:40:44 +0200

    Handle case of properties with an old_area of 75116

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,13 @@
 #   area.update(zone: "Paris")
 # end
 
-total = Property.all.size
-i = 1
+# total = Property.all.size
+# i = 1
 
-Property.all.each do |property|
-  puts "Update property #{i}/#{total} "
-  property.update(area: Area.where(name: property.old_area).first)
-  i += 1
-end
+# Property.all.each do |property|
+#   puts "Update property #{i}/#{total} "
+#   property.update(area: Area.where(name: property.old_area).first)
+#   i += 1
+# end
+
+Property.where(old_area: "75116").update_all(area_id: Area.where(name: "75016").first.id)
