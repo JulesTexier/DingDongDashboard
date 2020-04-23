3931b8935dc528e3cff31e6b0ea9142d4aefc453
fr3d100 <f.bonnand@gmail.com>
2020-04-23 16:11:19 +0200

    Update Areas zone

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,7 @@
-require "json"
-Subway.destroy_all
-ActiveRecord::Base.connection.reset_pk_sequence!("subways")
-obj = JSON.parse(File.read("./db/data/subways.json"))
-obj["stations"].each do |hsh|
-  puts "***"
-  puts Subway.create(name: hsh["metro"], line: hsh["ligne"])
+Area.all.each do |area|
+  area.update(zone: "Paris")
 end
+
+# Property.last(2).each do |property|
+#   property.update(area: Area.where(name: property.old_area).first)
+# end
