17ecea3e1550731d1aad2ae909587d22206d70f2
Segrelove <mlesegret@gmail.com>
2020-04-23 17:23:13 +0200

    super seed made in pp with fredooo"

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,21 @@ Property.all.each do |property|
 end
 
 Property.where(old_area: "75116").update_all(area_id: Area.where(name: "75016").first.id)
+
+area_yaml = YAML.load_file("db/data/areas.yml")
+
+area_yaml.each do |district_data|
+  if district_data["zone"] == "Paris"
+    district_data["datas"].each do |data|
+      Area.where(name: data["terms"][0]).first.update(name: data["name"])
+    end
+  end
+end
+
+area_yaml.each do |district_data|
+  if district_data["zone"] == "Banlieue-Ouest"
+    district_data["datas"].each do |data|
+      Area.create(name: data["name"], zone: "Banlieue-Ouest")
+    end
+  end
+end
