8cae0080b2743976d30b4567a25e1b1bfe501e92
fr3d100 <f.bonnand@gmail.com>
2020-04-23 16:43:43 +0200

    Uncomment seed for production execution

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
-# Area.all.each do |area|
-#   area.update(zone: "Paris")
-# end
+Area.all.each do |area|
+  area.update(zone: "Paris")
+end
 
-# total = Property.all.size
-# i = 1
+total = Property.all.size
+i = 1
 
-# Property.all.each do |property|
-#   puts "Update property #{i}/#{total} "
-#   property.update(area: Area.where(name: property.old_area).first)
-#   i += 1
-# end
+Property.all.each do |property|
+  puts "Update property #{i}/#{total} "
+  property.update(area: Area.where(name: property.old_area).first)
+  i += 1
+end
 
 Property.where(old_area: "75116").update_all(area_id: Area.where(name: "75016").first.id)
