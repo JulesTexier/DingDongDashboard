4e27a7f9a446565123833afe9340d8d5ef4aab2c
fr3d100 <f.bonnand@gmail.com>
2020-04-23 16:12:07 +0200

    test property-area association migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-Area.all.each do |area|
-  area.update(zone: "Paris")
-end
-
-# Property.last(2).each do |property|
-#   property.update(area: Area.where(name: property.old_area).first)
+# Area.all.each do |area|
+#   area.update(zone: "Paris")
 # end
+
+Property.last(2).each do |property|
+  property.update(area: Area.where(name: property.old_area).first)
+end
