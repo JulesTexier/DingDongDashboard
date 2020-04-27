7b004eb68f0cf8ffbd12fa58966124347b7b293c
fr3d100 <f.bonnand@gmail.com>
2020-04-27 09:27:33 +0200

    Handle case whether SeLoger email is a multiple or single property-ies email

--- app/controllers/api/v1/webhooks_controller.rb
+++ app/controllers/api/v1/webhooks_controller.rb
@@ -7,7 +7,11 @@ class Api::V1::WebhooksController < ApplicationController
 
   def handle_postmark_inbound
     if params["FromName"] == "SeLoger"
-      Email::ScraperSeLoger.new(params["HtmlBody"]).launch
+      if params["Subject"].match(/(\d){1,}(\s)(annonce)/i).is_a?(MatchData)
+        Email::ScraperSeLogerMultiple.new(params["HtmlBody"]).launch
+      else 
+        Email::ScraperSeLogerSingle.new(params["HtmlBody"]).launch
+      end
       # Gérer des condtitions pour savoir si on a bien inséré la property ou pas ... (doublon, 500 ou succès) 
       render json: { status: "SUCCESS", message: "Mail from SeLoger", data: nil}, status: 200
     else 
