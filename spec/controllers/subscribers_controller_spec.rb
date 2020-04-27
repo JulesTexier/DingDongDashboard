9cafc4d6bdf5d5a719421d7854cebc7cffe2b60a
Segrelove <mlesegret@gmail.com>
2020-04-27 17:22:29 +0200

    resolved failing subscribers test

--- spec/controllers/subscribers_controller_spec.rb
+++ spec/controllers/subscribers_controller_spec.rb
@@ -1,9 +1,18 @@
 require "rails_helper"
 
 RSpec.describe SubscribersController, type: :controller do
+  before(:all) do
+    area_yaml = YAML.load_file("db/data/areas.yml")
+    area_yaml.each do |district_data|
+      district_data["datas"].each do |data|
+        FactoryBot.create(:area, name: data["name"], zone: district_data["zone"])
+      end
+    end
+  end
   describe "GET #edit" do
     before do
       sub = FactoryBot.create(:subscriber_dummy_fb_id)
+      sub.areas << Area.first
       get :edit, params: { id: sub.id }
     end
     it "returns http success" do
@@ -12,6 +21,5 @@ RSpec.describe SubscribersController, type: :controller do
     it "renders the edit template" do
       expect(response).to render_template("edit")
     end
-
   end
 end
