8497c8e155e5f487f9766737834318073f2d1dcb
Segrelove <mlesegret@gmail.com>
2020-04-30 15:40:06 +0200

    changed confic to not clean scraper_parameter before every test

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,10 +1,10 @@
 RSpec.configure do |config|
   config.before(:suite) do
-    DatabaseCleaner.clean_with :truncation, except: %w(ar_internal_metadata)
+    DatabaseCleaner.clean_with :truncation, except: %w(ar_internal_metadata scraper_parameters)
   end
 
   config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.strategy = :truncation, {:except => %w[scraper_parameters]}
   end
 
   config.before(:each) do
