9e19ed59f6af715d608de7f0345d89bff24e2a2a
Segrelove <mlesegret@gmail.com>
2020-04-30 15:34:08 +0200

    made a script to inject every entry scraper_parameters entry in DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,34 @@
-Admin.create(firstname: "Etienne", lastname: "Chevalier", email: "etienne@hellodingdong.com", password: "DingDongRocks75$")
-Admin.create(firstname: "Frederic", lastname: "Bonnand", email: "fred@hellodingdong.com", password: "DingDongRocks75$")
-Admin.create(firstname: "Nicolas", lastname: "Fernandez-Le Follic", email: "nicolas@hellodingdong.com", password: "DingDongRocks75$")
-Admin.create(firstname: "Maxime", lastname: "Le Segretain", email: "maxime@hellodingdong.com", password: "DingDongRocks75$")
-Admin.create(firstname: "Greg", lastname: "Rouxel Oldrà", email: "greg@hellodingdong.com", password: "DingDongRocks75$")
+# Admin.create(firstname: "Etienne", lastname: "Chevalier", email: "etienne@hellodingdong.com", password: "DingDongRocks75$")
+# Admin.create(firstname: "Frederic", lastname: "Bonnand", email: "fred@hellodingdong.com", password: "DingDongRocks75$")
+# Admin.create(firstname: "Nicolas", lastname: "Fernandez-Le Follic", email: "nicolas@hellodingdong.com", password: "DingDongRocks75$")
+# Admin.create(firstname: "Maxime", lastname: "Le Segretain", email: "maxime@hellodingdong.com", password: "DingDongRocks75$")
+# Admin.create(firstname: "Greg", lastname: "Rouxel Oldrà", email: "greg@hellodingdong.com", password: "DingDongRocks75$")
+
+scraper_params = YAML.load_file("db/data/scraper_params.yml")
+
+scraper_params.each do |param|
+  param["params"].each do |data|
+    s = ScraperParameter.new
+    data["multi_page"] = false if data["multi_page"].nil?
+    data["http_type"] = nil if data["http_type"].nil?
+    data["http_request"] = [] if data["http_request"].nil?
+    data["page_nbr"] = 1 if data["page_nbr"].nil?
+    s.source = param["source"]
+    s.zone = data["zone"]
+    s.main_page_cls = data["main_page_cls"]
+    s.scraper_type = data["type"]
+    s.url = data["url"]
+    s.multi_page = data["multi_page"]
+    s.page_nbr = data["page_nbr"]
+    s.http_type = data["http_type"]
+    s.http_request = data["http_request"]
+    s.zone = data["zone"]
+    if ScraperParameter.where(s).count == 0 
+      if s.save
+        puts "Insertion of parameters"
+      else
+        puts "Error of parameter insertion"
+      end
+    end
+  end
+end
