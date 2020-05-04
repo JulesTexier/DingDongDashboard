01770be5b7139005fad7d4e5361cbcea1fe7a5c7
fr3d100 <f.bonnand@gmail.com>
2020-05-04 09:21:30 +0200

    Ran mig : Subscriber:is_active = false by default

--- db/migrate/20200504071946_set_subscriber_as_not_active_by_default.rb
+++ db/migrate/20200504071946_set_subscriber_as_not_active_by_default.rb
@@ -1,4 +1,5 @@
 class SetSubscriberAsNotActiveByDefault < ActiveRecord::Migration[6.0]
   def change
+    change_column :subscribers, :is_active, :boolean, default: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_04_065239) do
+ActiveRecord::Schema.define(version: 2020_05_04_071946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
@@ -234,7 +234,7 @@ ActiveRecord::Schema.define(version: 2020_05_04_065239) do
     t.string "email"
     t.string "phone"
     t.string "facebook_id"
-    t.boolean "is_active", default: true
+    t.boolean "is_active", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "max_price"
