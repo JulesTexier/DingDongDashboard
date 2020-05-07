546cb24433b0f768b8a8e48ac5ce56a08b4876dc
fr3d100 <f.bonnand@gmail.com>
2020-05-07 12:15:52 +0200

    Continue untill trigering the relevant SequenceEmail (ti be continued..)

--- app/services/growths/growth_engine.rb
+++ app/services/growths/growth_engine.rb
@@ -7,21 +7,22 @@ class GrowthEngine
       @json_content = json_content
       @first_time_frame = first_time_frame
       @second_time_frame = second_time_frame
+      @email_parser = EmailParser.new(@json_content)
+      @source = JSON.parse(@json_content)["FromName"]
+      @trigger = parser.get_sequence_trigger
     end
 
     def handle_lead
-      parser = EmailParser.new(@json_content)
-      lead_email = parser.get_reply_to_email
-
-      # 1 • Check if email is in DB
+      lead_email = @email_parser.get_reply_to_email
+      # 1 • Handle Subscriber (get or create)
       subscriber = get_subscriber(lead_email)
-      byebug
-      # # Est ce qu'on a envoyé une séquence il y a moins de 48h ?
-      # if !is_in_sequence_timeframe?(subscriber, @first_time_frame)
-      #   # Determination de la déquence à lancer !
-      #   sequence = get_adequate_sequence(subscriber)
-      #   sequence.execute(subscriber)
-      # end
+      # 2 • Handle Sequence to execute 
+      # Est ce qu'on a envoyé une séquence il y a moins de 48h ?
+      if !is_in_sequence_timeframe?(subscriber, @first_time_frame)
+        # Determination de la déquence à lancer !
+        sequence = get_adequate_sequence(subscriber)
+        sequence.execute(subscriber)
+      end
     end
 
     private
@@ -31,12 +32,12 @@ class GrowthEngine
     end
 
     def is_in_sequence_timeframe?(subscriber, timeframe)
-      SubscriberSequence.where(subscriber: subscriber).last.created_at >= Time.now - timeframe.to_i.hours
+      SubscriberSequenceEmail.where(subscriber: subscriber).last.created_at >= Time.now - timeframe.to_i.hours
     end
 
     def get_adequate_sequence(subscriber)
       if subscriber.is_client? && subscriber.is_active 
-         Sequence.find_by(name: "regular")
+         SequenceEmail.find_by(name: "regular")
       else 
         is_in_sequence_timeframe?(subscriber, @second_time_frame) ? Sequence.find_by(name: "regular") : Sequence.find_by(name: "se_loger_hack")
       end
