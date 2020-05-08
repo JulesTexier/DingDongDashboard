f56bfe2c169c710a5b2f91be0fa1c8f861b50c07
Segrelove <mlesegret@gmail.com>
2020-05-08 15:14:42 +0200

    added validators for sequence and sequence step, and a method to delay execution steps within a respectable time range

--- app/models/sequence_step.rb
+++ app/models/sequence_step.rb
@@ -1,17 +1,41 @@
 class SequenceStep < ApplicationRecord
   belongs_to :sequence
 
-  def initialize
-    @postmark = PostmarkMailer.new
+  validates :step, presence: true, numericality: { only_integer: true }
+  validates :step_type, presence: true
+  validates :template, presence: true
+  validates :time_frame, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def get_step_infos
+    puts "\n\n*** STEP INFO - NUMBER #{self.step.to_s}"
+    puts "Step ID : #{self.id}"
+    puts "Step Name : #{self.name}"
+    puts "Step Description : #{self.description}"
+    puts "Step Type: #{self.step_type}"
+    puts "Step Time Frame : #{self.time_frame}"
+    puts "Step template #{self.template}"
+    puts "*** END OF STEP INFO **\n\n"
   end
 
   def execute_step(subscriber)
     case self.sequence.sequence_type
     when "Mail"
-      byebug
-      @postmark.send_growth_step_email(self, subscriber)
+      PostmarkMailer.send_growth_step_email(self, subscriber).deliver_later(wait: self.respectable_sending_hours(8, 23).hour)
     else
       puts "error"
+    end
+  end
+
+  def respectable_sending_hours(start_day, end_day)
+    a = DateTime.now + self.time_frame
+    byebug
+    if a.hour < start_day
+      time_frame_adjustment = start_day - a.hour
+      self.time_frame + time_frame_adjustment
+    elsif a.hour >= end_day
+      time_frame_adjustment = (a.hour - end_day) + start_day
+    else
+      self.time_frame
     end
   end
 end
