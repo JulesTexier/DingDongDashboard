d6fdd101adb1d9aaf8b4d81025f336d9960f8c25
fr3d100 <f.bonnand@gmail.com>
2020-05-12 10:22:12 +0200

    Method to send email growth with gmail according to sender_email defined in sequence

--- app/mailers/growth_mailer.rb
+++ app/mailers/growth_mailer.rb
@@ -2,18 +2,18 @@ class GrowthMailer < ApplicationMailer
 
   default delivery_method: :smtp
 
-  def new_growth_email(user, password)
-    mail(to: "fred@hellodingdong.com", subject: "Coucou depuis le mailer Rails avec Gmail. Je crois que j'ai trouvé ce qu'il fallait pour envoyé depuis Gmail sans prise de tête :D ")
+  def send_growth_email_gmail(sequence_step, subscriber)
+    @content =  sequence_step.content
+    mail(from: sequence_step.sequence.sender_email, to: subscriber.email, subject: sequence_step.subject)
     mail.delivery_method.settings = {
       :address              => "smtp.gmail.com",
       :port                 => 587,
-      :user_name            => user,
-      :password             => password,
+      :user_name            => sequence_step.sequence.sender_email,
+      :password             => ENV['GMAIL_GROWTH_PASSWORD'],
       :authentication       => "plain",
       :domain               => 'gmail.com',
       :enable_starttls_auto => true
     }
   end
-
-
+  
 end
