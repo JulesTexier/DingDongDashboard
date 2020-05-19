2b0eaa10334bd6d78d4758a8afd3f356fa262a49
Segrelove <mlesegret@gmail.com>
2020-05-19 11:50:36 +0200

    changed the logic for a main page to show scrap for meilleurs agents, using proxy

--- app/services/scrapers/premium/scraper_meilleurs_agents.rb
+++ app/services/scrapers/premium/scraper_meilleurs_agents.rb
@@ -10,55 +10,42 @@ class Premium::ScraperMeilleursAgents < Scraper
   def launch(limit = nil)
     i = 0
     self.params.each do |args|
-      html = fetch_static_page_proxy_auth(args.url)
-      xml = access_xml_raw(html, args.main_page_cls)
-      # if !xml[0].to_s.strip.empty?
-      hashed_properties = []
-      xml.each do |item|
+      fetch_main_page(args).each do |item|
         begin
-          hashed_property = extract_each_flat(item)
-          property_checker_hash = {}
-          property_checker_hash[:rooms_number] = hashed_property[:rooms_number]
-          property_checker_hash[:surface] = hashed_property[:surface]
-          property_checker_hash[:price] = hashed_property[:price]
-          property_checker_hash[:area] = hashed_property[:area]
-          property_checker_hash[:link] = hashed_property[:link]
-          if go_to_prop?(property_checker_hash, 60)
-            @properties.push(hashed_property)
+          hashed_property = {}
+          hashed_property[:link] = access_xml_link(item, "a.listing-item__picture-container", "href")[0].to_s
+          hashed_property[:area] = perform_district_regex(access_xml_text(item, "div.text--muted.text--small"))
+          hashed_property[:flat_type] = regex_gen(access_xml_text(item, "div.listing-characteristic.margin-bottom"), "((a|A)ppartement|(A|a)ppartements|(S|s)tudio|(S|s)tudette|(C|c)hambre|(M|m)aison)").capitalize
+          hashed_property[:surface] = regex_gen(access_xml_text(item, "div.listing-characteristic.margin-bottom"), '(\d+(,?)(\d*))(.)(m)').to_float_to_int_scrp
+          hashed_property[:rooms_number] = regex_gen(access_xml_text(item, "div.listing-characteristic.margin-bottom"), '\d(.)*(pi(è|e)ce(s?))').to_float_to_int_scrp
+          hashed_property[:rooms_number] = 1 if hashed_property[:flat_type] == "Studio"
+          hashed_property[:price] = access_xml_text(item, "div.listing-price.margin-bottom").to_int_scrp
+          hashed_property[:has_elevator] = nil
+          hashed_property[:floor] = nil
+          hashed_property[:source] = @source
+          hashed_property[:provider] = "Agence"
+          if go_to_prop?(hashed_property, 7)
+            html = fetch_static_page_proxy_auth(hashed_property[:link])
+            hashed_property[:description] = access_xml_text(html, "section.listing-informations > div:nth-child(2)").strip
+            hashed_property[:floor] = perform_floor_regex(hashed_property[:description])
+            hashed_property[:has_elevator] = perform_elevator_regex(hashed_property[:description])
+            hashed_property[:subway_ids] = perform_subway_regex(hashed_property[:description])
+            hashed_property[:provider] = "Agence"
+            hashed_property[:source] = @source
+            hashed_property[:images] = access_xml_link(html, "a.listing-slideshow__link", "href").map { |img| "https://" + img[2..-1] }
+            hashed_property[:contact_number] = access_xml_link(html, "a.btn.btn--secondary.full-width.margin-right-double", "href")[0].gsub("tel:", "").convert_phone_nbr_scrp
+            hashed_property[:agency_name] = access_xml_text(html, "a.contact-form__subtitle")
+            @properties.push(hashed_property) ##testing purpose
             enrich_then_insert_v2(hashed_property)
             i += 1
+            break if i == limit
           end
-          break if i == limit
-        rescue StandardError => e
-          error_outputs(e, @source)
-          next
         end
+      rescue StandardError => e
+        error_outputs(e, @source)
+        next
       end
-      # else
-      #   puts "\nERROR : Couldn't fetch #{@source} datas.\n\n"
-      # end
     end
     return @properties
-  end
-
-  private
-
-  def extract_each_flat(item)
-    hashed_property = {}
-    hashed_property[:link] = access_xml_link(item, "a.listing-item__picture-container", "href")[0].to_s
-    hashed_property[:images] = ["https:" + access_xml_link(item, "img.listing-item__picture", "src")[0].to_s]
-    hashed_property[:area] = perform_district_regex(access_xml_text(item, "div.text--muted.text--small"))
-    hashed_property[:flat_type] = regex_gen(access_xml_text(item, "div.listing-characteristic.margin-bottom"), "((a|A)ppartement|(A|a)ppartements|(S|s)tudio|(S|s)tudette|(C|c)hambre|(M|m)aison)").capitalize
-    hashed_property[:surface] = regex_gen(access_xml_text(item, "div.listing-characteristic.margin-bottom"), '(\d+(,?)(\d*))(.)(m)').to_float_to_int_scrp
-    hashed_property[:rooms_number] = regex_gen(access_xml_text(item, "div.listing-characteristic.margin-bottom"), '\d(.)*(pi(è|e)ce(s?))').to_float_to_int_scrp
-    hashed_property[:rooms_number] = 1 if hashed_property[:flat_type] == "Studio"
-    hashed_property[:price] = access_xml_text(item, "div.listing-price.margin-bottom").to_int_scrp
-    hashed_property[:has_elevator] = nil
-    hashed_property[:floor] = nil
-    hashed_property[:source] = @source
-    hashed_property[:provider] = "Agence"
-    hashed_property[:description] = ""
-    puts JSON.pretty_generate(hashed_property)
-    return hashed_property
   end
 end
