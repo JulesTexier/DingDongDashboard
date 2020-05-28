e880adbd600d699df651471f654ee3c8071a4c0b
fr3d100 <f.bonnand@gmail.com>
2020-05-28 11:37:30 +0200

    Implement 2 first steps for GE Job Specs

--- spec/jobs/growth_engine_job_spec.rb
+++ spec/jobs/growth_engine_job_spec.rb
@@ -1,5 +1,23 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe GrowthEngineJob, type: :job do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe GrowthEngineJob, type: :job do
+  describe "#perform_later" do
+    before :all do
+      @subscriber = FactoryBot.create(:subscriber, status: 'new_lead')
+      @sequence = FactoryBot.create(:sequence, marketing_type: 'hack')
+      @sequence_step = FactoryBot.create(:sequence_step, sequence: @sequence, step_type: "shoot_mail", time_frame: 10)
+    end
+    it "enqueue job" do
+      ActiveJob::Base.queue_adapter = :test
+      expect {
+      GrowthEngineJob.perform_later(@sequence_step.id, @subscriber.id)
+      }.to have_enqueued_job
+      time =  Time.now.to_date + @sequence_step.time_frame.hour
+      Timecop.freeze(time) do 
+        expect {
+          GrowthEngineJob.set(wait_until: time).perform_later(@sequence_step.id, @subscriber.id)
+        }.to have_enqueued_job.with(@sequence_step.id, @subscriber.id).on_queue("default").at(time)
+      end
+    end
+  end
+end
