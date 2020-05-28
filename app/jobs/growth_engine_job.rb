1decaf71d4bffe218eea8f36716a2823256849b6
fr3d100 <f.bonnand@gmail.com>
2020-05-28 12:12:17 +0200

    Send email just if the sequence_type is 'shoot_mail'

--- app/jobs/growth_engine_job.rb
+++ app/jobs/growth_engine_job.rb
@@ -4,13 +4,14 @@ class GrowthEngineJob < ApplicationJob
   def perform(*args)
     sequence_step = SequenceStep.find(args[0])
     subscriber = Subscriber.find(args[1])
+    property_data = args[2]
 
     if !sequence_step.nil? && !subscriber.nil?
       # 1 • Add a SubscriberStatus 
       SubscriberStatus.create(subscriber: subscriber, status: Status.find_by(name: sequence_step.get_status_name))
 
       # 2 • Send email 
-      GrowthMailer.send_growth_email_gmail(sequence_step, subscriber).deliver_now if sequence_step.step_type = "shoot_mail"
+      GrowthMailer.send_growth_email_gmail(sequence_step, subscriber, property_data).deliver_now if sequence_step.step_type = "shoot_mail"
     end
   end
 end
