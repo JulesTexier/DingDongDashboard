4379794807170118f7b90de70bd5587f4eaafc2a
fr3d100 <f.bonnand@gmail.com>
2020-06-03 07:01:48 +0200

    HF mig file

--- db/migrate/20200603045348_create_permanences.rb
+++ db/migrate/20200603045348_create_permanences.rb
@@ -1,7 +1,7 @@
 class CreatePermanences < ActiveRecord::Migration[6.0]
   def change
     create_table :permanences do |t|
-      t.references :subscriber
+      t.references :broker
       t.references :broker_shift
       
       t.timestamps
