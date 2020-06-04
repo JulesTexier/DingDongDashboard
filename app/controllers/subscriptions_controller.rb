867b7adaf433f6053d8a396c1cfc2cd3fde3d0db
Segrelove <mlesegret@gmail.com>
2020-06-04 16:24:17 +0200

    added update for success with strip cust id and is_blocked

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -12,14 +12,14 @@ class SubscriptionsController < ApplicationController
         price: "price_1GpwkGJ0w5Eyx13BMqa29pDs",
         quantity: 1,
       }],
-      success_url: ENV["BASE_URL"] + "success?session_id={CHECKOUT_SESSION_ID}",
-      cancel_url: ENV["BASE_URL"] + "cancel",
+      success_url: ENV["BASE_URL"] + "subscribers/" + params[:subscriber_id] + "/success?session_id={CHECKOUT_SESSION_ID}",
+      cancel_url: ENV["BASE_URL"] + "subscribers/" + params[:subscriber_id] + "/cancel",
     )
   end
 
   def success
-    puts params[:session_id]
-    byebug
+    session = Stripe::Checkout::Session.retrieve(params[:session_id])
+    Subscriber.find(params[:subscriber_id]).update(stripe_customer_id: session.customer, is_blocked: false)
   end
 
   def cancel
