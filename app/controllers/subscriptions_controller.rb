0eee2b1a61b0cec24d6c0a612ec52c6b48425a67
Segrelove <mlesegret@gmail.com>
2020-06-04 11:37:39 +0200

    added controller logic for subscriptions

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,18 +1,27 @@
 class SubscriptionsController < ApplicationController
-  def new
-    Stripe.api_key = "sk_test_Ehoe6vR3ZldEruEt0TW2FPnJ00wLKZ4V3s"
+  def index
+    puts params[:subscriber_id]
+  end
 
-    session = Stripe::Checkout::Session.create(
+  def new
+    Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
+    @checkout_session = Stripe::Checkout::Session.create(
       payment_method_types: ["card"],
+      mode: "subscription",
       line_items: [{
         price: "price_1GpwkGJ0w5Eyx13BMqa29pDs",
         quantity: 1,
       }],
-      mode: "payment",
-      success_url: "https://example.com/success?session_id={CHECKOUT_SESSION_ID}",
-      cancel_url: "https://example.com/cancel",
+      success_url: ENV["BASE_URL"] + "success?session_id={CHECKOUT_SESSION_ID}",
+      cancel_url: ENV["BASE_URL"] + "cancel",
     )
+  end
 
+  def success
+    puts params[:session_id]
     byebug
+  end
+
+  def cancel
   end
 end
