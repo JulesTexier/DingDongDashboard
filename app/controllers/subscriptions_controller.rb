c0cec087f004cf2177356c9c120754437104a001
Segrelove <mlesegret@gmail.com>
2020-06-09 17:23:11 +0200

    added send_flow when the payment onboarding is successful

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -23,6 +23,15 @@ class SubscriptionsController < ApplicationController
     @subscriber = Subscriber.find(params[:subscriber_id])
     @subscriber.update(stripe_session_id: params[:session_id], is_blocked: false, is_active: true)
     @subscriber.statuses << Status.find_by(name: "has_paid_subscription")
+    m = Manychat.new
+    response = m.send_flow_sequence(@subscriber, "content20200609150547_506353")
+    if response[0]
+      json_response = { status: "SUCCESS", message: "The flow #{flow} has been sent to the subscriber #{@subscriber.id}", data: response[1] }
+      status = 200
+    else
+      json_response = { status: "ERROR", message: "A error occur in manychat call", data: response[1] }
+      status = 406
+    end
   end
 
   def cancel
