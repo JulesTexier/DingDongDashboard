99c9b717189444111b4e8e4cc8c7d3e3fcbf896c
fr3d100 <f.bonnand@gmail.com>
2020-06-18 10:21:12 +0200

    Update models for Hunters and HunterSearch

--- app/models/hunter.rb
+++ app/models/hunter.rb
@@ -1,4 +1,5 @@
 class Hunter < ApplicationRecord
+  has_many :hunter_searches
   validates :firstname, presence: true
   validates :lastname, presence: true
   validates :email, presence: true
--- app/models/hunter_search.rb
+++ app/models/hunter_search.rb
@@ -1,2 +1,29 @@
 class HunterSearch < ApplicationRecord
+  belongs_to :hunter
+
+  def get_matching_properties
+    props = Property.where(
+      price: Float::INFINITY..self.max_price,
+      rooms_number: self.rooms_number..Float::INFINITY,
+      surface: self.surface..Float::INFINITY,
+    )
+
+    prop_array = []
+    props.each do |prop|
+      self.areas.each do |area|
+        if prop.area == area
+          if prop.has_elevator === false && prop.floor != nil
+            prop_array.push(prop) if self.min_elevator_floor > prop.floor
+          else
+            prop_array.push(prop)
+          end
+        end
+      end
+    end
+    return prop_array
+  end
+
+  def get_pretty_price
+    self.max_price.to_s.reverse.scan(/.{1,3}/).join(" ").reverse
+  end
 end
