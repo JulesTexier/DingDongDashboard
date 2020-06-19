4c671542d4c986a8bebb7ef0c09e2ae92bbea603
Segrelove <mlesegret@gmail.com>
2020-06-19 15:21:04 +0200

    addced before action for authenticate hunter and after sign in redirection helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :authenticate_hunter!
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    hunter_hunter_searches_path(resource)
+  end
 end
