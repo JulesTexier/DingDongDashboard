fc7d5695c39bbbc540cfbc79d1115a018d1197c2
Segrelove <mlesegret@gmail.com>
2020-06-19 11:28:13 +0200

    migration and schema update for hunter devisee

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_18_095333) do
+ActiveRecord::Schema.define(version: 2020_06_19_083330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
@@ -134,6 +134,12 @@ ActiveRecord::Schema.define(version: 2020_06_18_095333) do
     t.string "company"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_hunters_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_hunters_on_reset_password_token", unique: true
   end
 
   create_table "leads", force: :cascade do |t|
