ccb50d15cd8d49a42bcbb430e925526bf1f2cf39
Segrelove <mlesegret@gmail.com>
2020-06-25 16:17:52 +0200

    rubocoped scraper, and changed simple synthax and common rubocop issues, removed useless methods as well

--- app/services/scrapers/scraper.rb
+++ app/services/scrapers/scraper.rb
@@ -1,19 +1,19 @@
-# coding: utf-8
-require "dotenv/load"
+# frozen_string_literal: true
+
+require 'dotenv/load'
 
 class Scraper
-  def enrich_then_insert(hashed_property)
-    if !already_exists_with_desc?(hashed_property) && !is_it_unwanted_prop?(hashed_property) && !is_prop_fake?(hashed_property)
-      enriched_infos = perform_enrichment_regex(hashed_property)
-      hashed_property.merge!(enriched_infos)
-      puts JSON.pretty_generate(hashed_property)
-      hashed_property[:area] = Area.where(name: hashed_property[:area]).first
-      property = insert_property(hashed_property)
-      scrap_historisation(hashed_property, __method__)
+  def enrich_then_insert(prop)
+    if !already_exists_with_desc?(prop) && !is_it_unwanted_prop?(prop) && !is_prop_fake?(prop)
+      enriched_infos = perform_enrichment_regex(prop)
+      prop.merge!(enriched_infos)
+      prop[:area] = Area.where(name: prop[:area]).first
+      insert_property(prop)
+      scrap_historisation(prop, __method__)
     else
       # for test purpose, if we don't want ton insert this shitty property,
       ## then we remove it from the final array of our dedicated scraper
-      @properties.reject! { |h| h[:link] == hashed_property[:link] }
+      @properties.reject! { |h| h[:link] == prop[:link] }
     end
   end
 
@@ -24,29 +24,29 @@ class Scraper
   def fetch_main_page(args)
     if !args.multi_page
       case args.scraper_type
-      when "Static"
+      when 'Static'
         html = fetch_static_page(args.url)
-      when "Dynamic"
+      when 'Dynamic'
         html = fetch_dynamic_page(args.url, args.waiting_cls, args.wait, *args.click_args)
-      when "Captcha"
+      when 'Captcha'
         html = fetch_captcha_page(args.url)
-      when "Proxy"
+      when 'Proxy'
         html = fetch_static_page_proxy_auth(args.url)
-      when "HTTPRequest"
+      when 'HTTPRequest'
         case args.http_type
-        when "get_json"
+        when 'get_json'
           json = fetch_json_get(args.url)
-        when "post_json"
+        when 'post_json'
           json = fetch_json_post(args.url, args.http_request)
-        when "post"
+        when 'post'
           html = fetch_http_page(args.url, args.http_request)
         end
       else
-        puts "Error"
+        puts 'Error'
       end
       json.nil? ? access_xml_raw(html, args.main_page_cls) : json
     else
-      card = fetch_many_pages(args.url, args.page_nbr, args.main_page_cls)
+      fetch_many_pages(args.url, args.page_nbr, args.main_page_cls)
     end
   end
 
@@ -55,34 +55,22 @@ class Scraper
     max_attempts = 3
     begin
       attempt_count += 1
-      res = open(url)
-      raise StaticError unless res.status[0] == "200"
+      res = URI.parse(url).open
+      raise StaticError unless res.status[0] == '200'
     rescue
       sleep 1
       attempt_count < max_attempts ? retry : "Error in Fetch Static Page for url : #{url}."
     else
+      # byebug
       page = Nokogiri::HTML.parse(res)
+      res.close!
       return page
     end
   end
 
-  def fetch_dynamic_page(url, waiting_class, wait, *click_args)
-    opts = {
-      headless: true,
-    }
-    browser = Watir::Browser.new :chrome, opts
-    browser.goto url
-    sleep wait
-    click_those_btns(browser, click_args) unless click_args.nil?
-    browser.div(class: waiting_class).wait_until(&:present?)
-    page = Nokogiri::HTML.parse(browser.html)
-    browser.close
-    return page
-  end
-
   def fetch_captcha_page(url)
-    uri = URI("https://app.scrapingbee.com/api/v1/")
-    params = { :api_key => ENV["BEE_API"], :url => url, :premium_proxy => true, :country_code => "us" }
+    uri = URI('https://app.scrapingbee.com/api/v1/')
+    params = { api_key: ENV['BEE_API'], url: url, premium_proxy: true, country_code: 'us' }
     uri.query = URI.encode_www_form(params)
     attempt_count = 0
     max_attempts = 3
@@ -97,8 +85,7 @@ class Scraper
       retry if attempt_count < max_attempts
     else
       puts "Worked on attempt n#{attempt_count} for Captcha\n\n" unless Rails.env.test?
-      page = Nokogiri::HTML.parse(res.body)
-      return page
+      Nokogiri::HTML.parse(res.body)
     end
   end
 
@@ -110,7 +97,7 @@ class Scraper
       url,
       method: :post,
       headers: header,
-      body: http_request[1],
+      body: http_request[1]
     )
     begin
       attempt_count += 1
@@ -130,19 +117,19 @@ class Scraper
       url,
       method: :post,
       headers: header,
-      body: http_request[1],
+      body: http_request[1]
     )
     response = request.run
-    return JSON.parse(response.body)
+    JSON.parse(response.body)
   end
 
   def fetch_json_get(url)
     request = Typhoeus::Request.new(
       url,
-      method: :get,
+      method: :get
     )
     response = request.run
-    return JSON.parse(response.body)
+    JSON.parse(response.body)
   end
 
   def fetch_many_pages(url, page_nbr, xml_first_page)
@@ -152,7 +139,7 @@ class Scraper
       xml.push(access_xml_raw(fetch_static_page(page_nbr_to_url(url, i)), xml_first_page))
       i += 1
     end
-    return xml.flatten
+    xml.flatten
   end
 
   #####################
@@ -160,7 +147,7 @@ class Scraper
   #####################
 
   def get_proxy_params
-    url = "http://falcon.proxyrotator.com:51337/?apiKey=#{ENV["ROTATING_PROXY_API"]}&connectionType=Datacenter"
+    url = "http://falcon.proxyrotator.com:51337/?apiKey=#{ENV['ROTATING_PROXY_API']}&connectionType=Datacenter"
     uri = URI(url)
     JSON.parse(Net::HTTP.get(uri))
   end
@@ -168,14 +155,14 @@ class Scraper
   def fetch_static_page_proxy_auth(url)
     proxy_params = get_proxy_params
     user_agent = proxy_params["randomUserAgent"]
-    proxy_uri = URI.parse("http://199.189.86.111:#{ENV["PROXY_PORT"]}")
+    proxy_uri = URI.parse("http://199.189.86.111:#{ENV['PROXY_PORT']}")
     attempt_count = 0
     max_attempts = 3
     begin
       attempt_count += 1
       puts "\nAttempt ##{attempt_count} for Proxy - #{source}" unless Rails.env.test?
-      res = open(url, :proxy_http_basic_authentication => [proxy_uri, ENV["USERNAME_ROT_PROXY"], ENV["PASSWORD_ROT_PROXY"]], "User-Agent" => user_agent)
-      raise ProxyError unless res.status[0] == "200"
+      res = open(url, :proxy_http_basic_authentication => [proxy_uri, ENV['USERNAME_ROT_PROXY'], ENV['PASSWORD_ROT_PROXY']], 'User-Agent' => user_agent)
+      raise ProxyError unless res.status[0] == '200'
     rescue
       puts "Trying again for #{source} - Proxy\n\n" unless Rails.env.test?
       sleep 1
@@ -187,28 +174,18 @@ class Scraper
     end
   end
 
-  def get_proxy_ip
-    ips = YAML.load_file("./db/data/proxy_ip.yml")
-    ips["ip"]
-  end
-
-  def get_website(url, proxy_params)
-    user_agent = proxy_params["randomUserAgent"]
-    proxy_ip = proxy_params["proxy"]
-    open(url, proxy: URI.parse(proxy_ip), "User-Agent" => user_agent).read
-  end
-
   ###########################
   ## GENERIC METHOD  ##
   ###########################
 
   def page_nbr_to_url(url, page_nbr)
-    url.gsub("[[PAGE_NUMBER]]", page_nbr.to_s)
+    url.gsub('[[PAGE_NUMBER]]', page_nbr.to_s)
   end
 
-  def error_outputs(e, source)
+  def error_outputs(err, source)
     puts "\nError for #{@source}, skip this one."
     puts "It could be a bad link or a bad xml extraction.\n\n"
+    puts err
   end
 
   ###########################
@@ -220,7 +197,7 @@ class Scraper
     page.css(css_selector).each do |item|
       data.push(item.text)
     end
-    data.empty? ? "" : data.join(" ")
+    data.empty? ? '' : data.join(' ')
   end
 
   def access_xml_array_to_text(page, css_selector)
@@ -228,7 +205,7 @@ class Scraper
     page.css(css_selector).each do |item|
       data.push(item.text)
     end
-    return data.join(" ")
+    data.join(' ')
   end
 
   def access_xml_link(page, css_selector, type)
@@ -236,7 +213,7 @@ class Scraper
     page.css(css_selector).each do |item|
       data.push(item["#{type}"])
     end
-    return data
+    data
   end
 
   def access_xml_link_matchdata(page, css_selector, type, regexp)
@@ -244,7 +221,7 @@ class Scraper
     page.css(css_selector).each do |item|
       data.push(item["#{type}"]) unless item["#{type}"].match(/#{regexp}/i).is_a?(MatchData)
     end
-    return data
+    data
   end
 
   def access_xml_link_matchdata_src(page, css_selector, type, regexp, src)
@@ -252,7 +229,7 @@ class Scraper
     page.css(css_selector).each do |item|
       data.push(src + item["#{type}"]) unless item["#{type}"].match(/#{regexp}/i).is_a?(MatchData)
     end
-    return data
+    data
   end
 
   def access_xml_raw(page, css_selector)
@@ -262,7 +239,7 @@ class Scraper
         data.push(item)
       end
     end
-    return data
+    data
   end
 
   ####################
@@ -275,7 +252,7 @@ class Scraper
 
   def perform_floor_regex(str)
     floor = str.remove_acc_scrp.convert_numerals_scrp.floors_str_scrp
-    return floor.to_int_scrp unless floor.nil?
+    floor.to_int_scrp unless floor.nil?
   end
 
   def perform_elevator_regex(str)
@@ -315,11 +292,9 @@ class Scraper
       subways = YAML.load_file("./db/data/subways.yml")
       subway_infos = []
       subways["stations"].each do |subway|
-        if str.remove_acc_scrp.match(/#{subway["name"].remove_acc_scrp}/i).is_a?(MatchData)
-          subway_infos.push(subway)
-        end
+        subway_infos.push(subway) if str.remove_acc_scrp.match(/#{subway["name"].remove_acc_scrp}/i).is_a?(MatchData)
       end
-      return subway_infos.uniq
+      subway_infos.uniq
     end
   end
 
@@ -332,21 +307,21 @@ class Scraper
     enriched_infos[:has_terrace] = prop[:description].terrace_str_scrp unless prop.key?(:has_terrace)
     enriched_infos[:has_balcony] = prop[:description].balcony_str_scrp unless prop.key?(:has_balcony)
     enriched_infos[:is_last_floor] = prop[:description].last_floor_str_scrp unless prop.key?(:is_last_floor)
-    return enriched_infos
+    enriched_infos
   end
 
   def get_type_flat(str)
-    flat_type = "N/C"
-    flat_type = "Appartement" if str.downcase.include? "appartement"
-    flat_type = "Maison" if str.downcase.include? "maison"
-    flat_type = "Studio" if str.downcase.include? "studio"
-    flat_type = "Loft" if str.downcase.include? "loft"
-    flat_type = "Cave" if str.downcase.include? "cave"
-    flat_type = "Parking" if str.downcase.include? "parking"
-    flat_type = "Commerce" if str.downcase.include? "commerce"
-    flat_type = "Bureaux" if str.downcase.include? "bureaux"
-    flat_type = "Hotel particulier" if str.downcase.include? "hotel particulier"
-    return flat_type
+    flat_type = 'N/C'
+    flat_type = 'Appartement' if str.downcase.include? 'appartement'
+    flat_type = 'Maison' if str.downcase.include? 'maison'
+    flat_type = 'Studio' if str.downcase.include? 'studio'
+    flat_type = 'Loft' if str.downcase.include? 'loft'
+    flat_type = 'Cave' if str.downcase.include? 'cave'
+    flat_type = 'Parking' if str.downcase.include? 'parking'
+    flat_type = 'Commerce' if str.downcase.include? 'commerce'
+    flat_type = 'Bureaux' if str.downcase.include? 'bureaux'
+    flat_type = 'Hotel particulier' if str.downcase.include? 'hotel particulier'
+    flat_type
   end
 
   #############################
@@ -359,7 +334,7 @@ class Scraper
   def go_to_prop?(prop, time)
     if !is_prop_fake?(prop) ## we check if the property is fake if we have enough informations (surface + price)
       if is_link_in_db?(prop) ## we check if the prop is in DB by its link
-        false ##we dont go to the property show because we already have it
+        false ## we dont go to the property show because we already have it
       else ## it doesnt exist in db so we check by 3 - 4 keys if its already in DB from another source
         filtered_prop = prop.select { |k, v| !v.nil? && [:area, :rooms_number, :surface, :price].include?(k) } ## we only keep existants arguments
         if filtered_prop.length > 2 ## we verify that theres at least 3 arguements
@@ -380,21 +355,21 @@ class Scraper
     if filtered_prop[:area].nil?
       response = Property.where(
         filtered_prop.except(:area)
-      ).where("created_at >= ?", time.days.ago).exists?
+      ).where('created_at >= ?', time.days.ago).exists?
     else
       filtered_prop[:area_id] = Area.find_by(name: filtered_prop[:area]).id
       response = Property.where(
         filtered_prop.except(:area),
-      ).where("created_at >= ?", time.days.ago).exists?
+      ).where('created_at >= ?', time.days.ago).exists?
     end
     scrap_historisation(prop, __method__) if response
-    return response
+    response
   end
 
   def is_link_in_db?(prop)
     response = Property.where(link: prop[:link].strip).exists? ? true : false
     scrap_historisation(prop, __method__) if response
-    return response
+    response
   end
 
   def is_prop_fake?(prop)
@@ -411,7 +386,7 @@ class Scraper
       response = true ## not enough informations, we should further check
     end
     scrap_historisation(prop, __method__) if response
-    return response
+    response
   end
 
   ## Is a final check, and check if it already exists with description
@@ -437,25 +412,25 @@ class Scraper
     if response
       scrap_historisation(hashed_property, __method__)
     end
-    return response
+    response
   end
 
   ## We check if its not a Viagier / Under Offer / Parking Lot / A ferme Vosgienne
   def is_it_unwanted_prop?(prop)
     response = prop[:description].remove_acc_scrp.match(/(appartement(s?)|bien(s?)|residence(s?))(.?)(deja vendu|sous compromis|service(s?))|(ehpad|viager)|(sous offre actuellement)|(local commercial)/i).is_a?(MatchData)
     scrap_historisation(prop, __method__) if response
-    return response
+    response
   end
 
-  def is_it_night?
+  def it_night?
     response = false
-    a = Time.parse("22:00:00 +0200")
-    b = Time.parse("09:00:00 +0200")
-    c = Time.now.getlocal("+02:00")
+    a = Time.parse('22:00:00 +0200')
+    b = Time.parse('09:00:00 +0200')
+    c = Time.now.getlocal('+02:00')
     if c > a || c < b
       response = true
     end
-    return response
+    response
   end
 
   #############################
@@ -464,8 +439,8 @@ class Scraper
 
   def desc_comparator(desc, desc_to_compare)
     response = false
-    str1 = desc.remove_acc_scrp.tr(".,!?:;'", "").tr("²", "2").tr("\s\t\r", "")
-    str2 = desc_to_compare.remove_acc_scrp.tr(".,!?:;'", "").tr("²", "2").tr("\s\t\r", "")
+    str1 = desc.remove_acc_scrp.tr(".,!?:;'", '').tr('²', '2').tr("\s\t\r", '')
+    str2 = desc_to_compare.remove_acc_scrp.tr(".,!?:;'", '').tr('²', '2').tr("\s\t\r", '')
     min = [str1.length, str2.length].min
     if min > 20
       if str1.length == min
@@ -492,7 +467,7 @@ class Scraper
         response = true if long_string.include?(sample_string)
       end
     end
-    return response
+    response
   end
 
   #########################################
@@ -504,15 +479,15 @@ class Scraper
   end
 
   def scraped_property_checker
-    time_frame = ENV["TIME_FRAME_ALERT"].nil? ? 6 : ENV["TIME_FRAME_ALERT"]
+    time_frame = ENV['TIME_FRAME_ALERT'].nil? ? 6 : ENV['TIME_FRAME_ALERT']
     prop_nbr = scraped_property_count(time_frame)
-    if prop_nbr == 0
+    if prop_nbr.zero?
       message = "ALERTE. Ceci n'est pas un exercice, nous n'avons pas scrapé d'annonces en #{time_frame} heure(s)"
       sms_mode = SmsMode.new
       sms_mode.send_sms_to_team(message)
       puts "\n\n" + message + "\n\n"
     else
-      property_word = prop_nbr > 1 ? "properties" : "property"
+      property_word = prop_nbr > 1 ? 'properties' : 'property'
       puts "\n\nEverything seems to be fine."
       puts "We've scraped #{prop_nbr} #{property_word} in a #{time_frame} hour window.\n\n"
     end
@@ -528,13 +503,13 @@ class Scraper
     parameters.each do |param|
       if param.is_active == true
         if is_mail_alert
-          data.push(param) if param.group_type == "Email"
+          data.push(param) if param.group_type == 'Email'
         else
-          data.push(param) if param.group_type != "Email"
+          data.push(param) if param.group_type != 'Email'
         end
       end
     end
-    return data
+    data
   end
 
   ########################
@@ -543,7 +518,7 @@ class Scraper
 
   def scrap_historisation(hashed_property, method_name)
     hashed_property[:source] = self.source unless Rails.env.test?
-    insert_property_history(hashed_property, method_name) if !PropertyHistory.where(link: hashed_property[:link]).exists?
+    insert_property_history(hashed_property, method_name) unless PropertyHistory.where(link: hashed_property[:link]).exists?
   end
 
   private
@@ -561,14 +536,14 @@ class Scraper
   end
 
   def insert_property(prop_hash)
-    prop_hash[:has_been_processed] = true if is_it_night?
+    prop_hash[:has_been_processed] = true if it_night?
     prop = Property.create(prop_hash)
     if prop.save
       unless Rails.env.test?
         puts "\nInsertion of a property from #{prop_hash[:source]}: "
         puts prop.get_title
       end
-      return prop
+      prop
     end
   end
 end
