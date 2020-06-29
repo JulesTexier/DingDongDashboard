7fa196ca4fe2d650f64b28a73a0f192ee60139be
fr3d100 <f.bonnand@gmail.com>
2020-06-29 17:50:49 +0200

    Update email subject according to numbe rof properties

--- app/mailers/hunter_mailer.rb
+++ app/mailers/hunter_mailer.rb
@@ -2,7 +2,7 @@ class HunterMailer < ApplicationMailer
   def notification_email(hunter_search_id, props)
     @hunter_search = HunterSearch.find(hunter_search_id)
     @props = props
-    @url  = 'http://example.com/login'
-    mail(from: "nicolas@hellodingdong.com", to: @hunter_search.hunter.email, subject: 'Nouveaux biens ! ')
+    subject = props.size > 1 ? "Nouveaux biens" : "Nouveau bien"
+    mail(from: "nicolas@hellodingdong.com", to: @hunter_search.hunter.email, subject: subject)
   end
 end
