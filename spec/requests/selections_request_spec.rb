7e6e3087ed3e030cb9cc7f2dadd82705905dc60c
fr3d100 <f.bonnand@gmail.com>
2020-06-30 15:33:36 +0200

    Generate controller and views for selections (index, create, destroy)

---
+++ spec/requests/selections_request_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "Selections", type: :request do
+
+  describe "GET /index" do
+    it "returns http success" do
+      get "/selections/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /create" do
+    it "returns http success" do
+      get "/selections/create"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /destroy" do
+    it "returns http success" do
+      get "/selections/destroy"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
