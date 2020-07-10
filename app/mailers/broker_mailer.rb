3f356fba03cc8bcccd3ae919cf8a3322980e680c
fr3d100 <f.bonnand@gmail.com>
2020-07-10 15:33:44 +0200

    Add Broker mailer and new_lead email for getting subscriber coordonates

--- app/mailers/broker_mailer.rb
+++ app/mailers/broker_mailer.rb
@@ -1,2 +1,12 @@
 class BrokerMailer < ApplicationMailer
+  def new_lead(subscriber_id, broker_id)
+    @subscriber = Subscriber.find(subscriber_id)
+    @broker = Broker.find(broker_id)
+
+    if !@subscriber.nil? && !@broker.nil? 
+      subject = "[DING DONG] Nouveau contact"
+      mail(from: "etienne@hellodingdong.com", to: @broker.email, subject: subject)
+    end
+
+  end
 end
