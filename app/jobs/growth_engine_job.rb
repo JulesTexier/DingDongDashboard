c509e1f0d25f5e8b10190721ada113e869fe6a0f
fr3d100 <f.bonnand@gmail.com>
2020-07-22 16:42:25 +0200

    HF syntax error for sending email in growth_mailer job

--- app/jobs/growth_engine_job.rb
+++ app/jobs/growth_engine_job.rb
@@ -11,7 +11,7 @@ class GrowthEngineJob < ApplicationJob
       SubscriberStatus.create(subscriber: subscriber, status: Status.find_by(name: sequence_step.get_status_name))
 
       # 2 • Send email 
-      GrowthMailer.send_growth_email_gmail(sequence_step, subscriber, property_data).deliver_now if sequence_step.step_type = "shoot_mail"
+      GrowthMailer.send_growth_email_gmail(sequence_step, subscriber, property_data).deliver_now if sequence_step.step_type == "shoot_mail"
     end
   end
 end
