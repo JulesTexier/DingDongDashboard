0561ffb09f45b6929b3a49ebb4cd5ffed26d9010
Segrelove <mlesegret@gmail.com>
2020-07-22 12:18:41 +0200

    added general finder method for yaml agglomeration.yml

--- app/models/area.rb
+++ app/models/area.rb
@@ -11,32 +11,17 @@ class Area < ApplicationRecord
     end
 
     def self.get_aggregate_data_for_selection(areas_id)
-			master_areas = []
-			zone_areas = Area.all.pluck(:zone).uniq!
-			zone_areas.each do |zone| 
-				zone_hash = {}
-				zone_hash[:zone] = zone
-				zone_hash[:areas] = Area.where(zone: zone).pluck(:id, :name, :zip_code)
-				master_areas.push(zone_hash)
-			end
-
-			master_areas.each do |zone|
-				zone[:areas].each do |area|
-					selected = areas_id.include?(area[0]) ? "selected" : ""
-					area.push(selected)
-				end
-			end
-      return master_areas
-      # areas = Area.where(zone: zones).pluck(:id, :name, :zip_code)
-      # areas.each do |area|
-      #   selected = areas_id.include?(area[0]) ? "selected" : ""
-      #   area.push(selected)
-      # end
-      # areas
+      areas = []
+      areas_id.each do |area|
+        area.push(Area.find(area).pluck(:name, :zip_code))
+        area.push("selected")
+      end
+      return areas
     end
 
+
     def self.get_agglo_infos
-      agglo_infos = YAML.load_file("./db/data/agglomeration.yml")
+      YAML.load_file("./db/data/agglomeration.yml")
     end
 
     def self.get_active_agglo_infos
@@ -44,8 +29,17 @@ class Area < ApplicationRecord
       agglo_infos.reject {|agglo| !agglo["is_active"]}
     end
 
-    def self.get_zones_from_agglo(agglo)
+    def self.get_agglo_from_zone(zone_name)
       agglo_infos = YAML.load_file("./db/data/agglomeration.yml")
+      agglo_from_zone = []
+      agglo_infos.each do |agglo| 
+        agglo["zone"].any? do |zone| 
+          if zone.include?(zone_name)
+            agglo_from_zone.push(agglo["agglomeration"])
+          end
+        end
+      end
+      agglo_from_zone
     end
 
     def self.get_selected_agglo_area(selected_agglo, areas_id)
