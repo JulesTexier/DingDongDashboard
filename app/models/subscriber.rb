8b9914198094e36f22cf6d1fefa752a51d2f0b42
fr3d100 <f.bonnand@gmail.com>
2020-07-23 18:08:55 +0200

    Update last_props_method

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -91,9 +91,11 @@ class Subscriber < ApplicationRecord
 
   def get_x_last_props(max_number)
     attrs = %w(id rooms_number surface price floor area_id has_elevator has_terrace has_garden has_balcony is_new_construction is_last_floor images link link)
-    props = Property
+    props = 
+      Property.where(area: self.areas)
+      .where('price <= ? AND surface >= ? AND rooms_number >= ?', self.max_price, self.min_surface, self.min_rooms_number)
       .order(id: :desc)
-      .limit(1000)
+      .limit(200)
       .pluck(*attrs).map { |p| attrs.zip(p).to_h }
     props_to_send = []
     subs_areas = self.areas.ids
@@ -101,7 +103,6 @@ class Subscriber < ApplicationRecord
       props_to_send.push(prop["id"]) if self.is_matching_property?(prop, subs_areas)
       break if props_to_send.length == max_number.to_i
     end
-
     return props_to_send
   end
 
