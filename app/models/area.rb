cc119f5a605c57c0ce924c5cc04a0c5e9e14f913
Segrelove <mlesegret@gmail.com>
2020-07-27 18:08:49 +0200

    changed line for more readbility

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,7 +1,7 @@
 class Area < ApplicationRecord
     has_many :selected_areas
-    has_many :properties
     has_many :subscribers, through: :selected_areas
+    has_many :properties
     
     has_many :hunter_search_areas
     has_many :hunter_searchs, through: :hunter_search_areas
