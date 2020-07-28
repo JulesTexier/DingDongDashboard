5b305e0675d7378085dfa8b0f997fc78d1186e33
fr3d100 <f.bonnand@gmail.com>
2020-07-28 11:06:05 +0200

    Refacto broker_api controller

--- app/controllers/api/v1/brokers_controller.rb
+++ app/controllers/api/v1/brokers_controller.rb
@@ -1,31 +1,25 @@
+require 'dotenv/load'
+
 class Api::V1::BrokersController < ApplicationController
-    include ActionController::HttpAuthentication::Token::ControllerMethods
-    protect_from_forgery with: :null_session
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+  
+  TOKEN = ENV['BEARER_TOKEN']
+  protect_from_forgery with: :null_session
+  before_action :authentificate
 
-    require 'dotenv/load'
-    TOKEN = ENV['BEARER_TOKEN']
-  
-    protect_from_forgery with: :null_session
-    before_action :authentificate
-  
-    def show 
-      @broker = Broker.find(params[:id])
-      if !@broker.nil?
-        render json: {status: 'SUCCESS', message: 'Broker found', data: @broker}, status: 200
-      else
-        render json: {status: 'ERROR', message: 'Broker not found'}, status: 404
-      end
+  def show 
+    @broker = Broker.find(params[:id])
+    if !@broker.nil?
+      render json: {status: 'SUCCESS', message: 'Broker found', data: @broker}, status: 200
+    else
+      render json: {status: 'ERROR', message: 'Broker not found'}, status: 404
     end
+  end
 
-  
-    private
-  
-      def authentificate
-          authenticate_or_request_with_http_token do |token, options|
-              ActiveSupport::SecurityUtils.secure_compare(token, TOKEN)
-          end
+  private
+  def authentificate
+      authenticate_or_request_with_http_token do |token, options|
+          ActiveSupport::SecurityUtils.secure_compare(token, TOKEN)
       end
-  
-  
-  
+  end
 end
