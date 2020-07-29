2ee1280d3cf0234603112fd964195d7894dd4c9f
Segrelove <mlesegret@gmail.com>
2020-07-29 18:59:41 +0200

    added research everywhere for subscribers

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -5,14 +5,15 @@ class SubscribersController < ApplicationController
 
   def edit
     @subscriber = Subscriber.find(params[:id])
-    subscriber_areas_id = @subscriber.areas.pluck(:id)
+    @research = @subscriber.research
+    @research.update(zone: params[:selected_zones]) unless params[:selected_zones].nil?
+    subscriber_areas_id = @research.areas.pluck(:id)
     if params[:selected_zones].nil? && subscriber_areas_id.empty?
       flash[:danger] = "Veuillez sélectionner une zone de recherche."
       redirect_to "/subscribers/" + params[:id] + "/agglomeration"
     elsif subscriber_areas_id.any?
-      zone = @subscriber.determine_zone
-      @master_areas = Area.get_selected_agglo_area(zone[0], subscriber_areas_id)
-      @master_areas += Area.global_zones(zone[0])
+      @master_areas = Area.get_selected_agglo_area(@research.zone, subscriber_areas_id)
+      @master_areas += Area.global_zones(@research.zone)
     else
       @master_areas = Area.get_selected_agglo_area(params[:selected_zones], subscriber_areas_id)
       @master_areas += Area.global_zones(params[:selected_zones])
@@ -24,7 +25,7 @@ class SubscribersController < ApplicationController
     @research = @subscriber.research
     areas_ids = []
     areas_ids += params[:areas] unless params[:areas].nil?
-    if @subscriber.update(research_params) && !areas_ids.empty?
+    if @research.update(subscriber_params) && !areas_ids.empty?
       @research.update_research_areas(areas_ids)
       flash[:success] = "Les critères sont enregistrés ! Fermez cette fenêtre pour continuer."
     else
@@ -40,16 +41,13 @@ class SubscribersController < ApplicationController
   #      OTHER    #
   # ###############
 
-
   def select_agglomeration
     @subscriber = Subscriber.find(params[:subscriber_id])
     @agglos_infos = Area.get_agglo_infos
   end
   
-  
   def activation
     flow = "content20200716090652_490399"
-
     @subscriber = Subscriber.find(params[:subscriber_id])
     if !@subscriber.nil?
       @subscriber.update(is_blocked: false) 
@@ -61,10 +59,6 @@ class SubscribersController < ApplicationController
   private
 
   def subscriber_params
-    params.require(:subscriber).permit(:firstname, :lastname, :email, :phone, :has_messenger, :facebook_id)
-  end
-
-  def research_params
-    params.require(:research).permit(:max_price, :min_surface, :min_rooms_number, :min_elevator_floor, :min_floor, :project_type, :additional_question, :specific_criteria, :broker_id, :status, :initial_areas, :terrace, :garden, :balcony, :new_construction, :last_floor, :min_price, :max_sqm_price)
+    params.require(:subscriber).permit(:firstname, :lastname, :email, :phone, :has_messenger, :facebook_id, :max_price, :min_surface, :min_rooms_number, :min_elevator_floor, :min_floor, :project_type, :additional_question, :specific_criteria, :broker_id, :status, :initial_areas, :terrace, :garden, :balcony, :new_construction, :last_floor, :min_price, :max_sqm_price)
   end
 end
