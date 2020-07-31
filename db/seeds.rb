2ded94fc235ebc7739b9ddeb8bbb78f67e9fb3d3
fr3d100 <f.bonnand@gmail.com>
2020-07-31 17:02:57 +0200

    Update seed for migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -73,3 +73,111 @@ brokers_yaml.each do |broker|
     Broker.create(broker)
   end
 end
+
+###################################################################
+# Script de migration de la rentrée (v4) - Ete 2020 (first part)
+###################################################################
+
+### 1) Boucle sur tous les Subscribers 
+
+Subscriber.all.each do |subscriber|
+
+  # Migration des critères vers un objet Research
+  research = Research.new(subscriber: subscriber)
+  research.min_floor = subscriber.min_floor
+  research.has_elevator = subscriber.has_elevator
+  research.min_elevator_floor = subscriber.min_elevator_floor
+  research.min_surface = subscriber.min_surface
+  research.min_rooms_number = subscriber.min_rooms_number
+  research.max_price = subscriber.max_price
+  research.min_price = subscriber.min_price
+  research.max_sqm_price = subscriber.max_sqm_price
+  research.is_active = subscriber.is_active
+  research.balcony = subscriber.balcony
+  research.terrace = subscriber.terrace
+  research.garden = subscriber.garden
+  research.new_construction = subscriber.new_construction
+  research.last_floor = subscriber.last_floor
+  research.home_type = subscriber.home_type
+  research.apartment_type = subscriber.apartment_type
+  research.created_at = subscriber.created_at
+
+  research.save
+
+  # Migration des selected_areas vers des research_area
+  subscriber.selected_areas.each do |sa|
+    ResearchArea.create(research: research, area: sa.area)
+  end
+
+  # Migration des favoris vers des saved_properties
+  subscriber.favorites.each do |fav|
+    SavedProperty.create(research: research, property: fav.property)
+  end
+
+end
+
+
+### 2 ) Boucle sur tous les hunters
+
+HunterSearch.all.each do |hs|
+
+# Migration des critères vers un objet Research
+  research = Research.new(hunter: hs.hunter)
+
+  research.min_floor = hs.min_floor
+  research.has_elevator = hs.has_elevator
+  research.min_elevator_floor = hs.min_elevator_floor
+  research.min_surface = hs.min_surface
+  research.min_rooms_number = hs.min_rooms_number
+  research.max_price = hs.max_price
+  research.min_price = hs.min_price
+  research.max_sqm_price = hs.max_sqm_price
+  research.is_active = hs.is_active
+  research.balcony = hs.balcony
+  research.terrace = hs.terrace
+  research.garden = hs.garden
+  research.new_construction = hs.new_construction
+  research.last_floor = hs.last_floor
+  research.home_type = hs.home_type
+  research.apartment_type = hs.apartment_type
+  research.created_at = hs.created_at
+  
+  research.save
+  
+# Migration des selected_areas vers des research_area
+  hs.hunter_search_areas.each do |hsa|
+    ResearchArea.create(research: research, area: hsa.area)
+  end
+
+# Migration des favoris vers des saved_properties
+  hs.selections.each do |fav|
+    SavedProperty.create(research: research, property: fav.property)
+  end
+
+end 
+
+
+
+###################################################################
+# Script de migration de la rentrée (v4) - Ete 2020 (second part)
+###################################################################
+
+
+
+# # Suppression de tous les favoris 
+# Favorite.all.destroy_all
+
+# # Suppression de toutes les selections 
+# Selection.all.destroy_all
+
+# # Suppression de toutes les selected_areas 
+# SelectedArea.all.destroy_all
+
+# # Suppression de toutes les hunter_search_areas 
+# HunterSearchArea.all.destroy_all
+
+# # Suppression de toutes les hunter_search 
+# HunterSearch.all.destroy_all 
+
+# # Suppresion de tous les leads
+# Lead.all.destroy_akk
