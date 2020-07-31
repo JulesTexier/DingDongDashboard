c0f20bc798cd3ca0c699f56d9d0f0c123866d00e
Segrelove <mlesegret@gmail.com>
2020-07-31 15:10:19 +0200

    adapted research logic to tests

--- spec/controllers/api/v1/manychat_controller_spec.rb
+++ spec/controllers/api/v1/manychat_controller_spec.rb
@@ -40,12 +40,13 @@ RSpec.describe Api::V1::ManychatController, type: :controller do
 
   describe "Manychat send methods" do
     before :all do
-      @sub = FactoryBot.create(:subscriber_fred, max_price: 500000, min_surface: 20, min_rooms_number: 1, min_floor: 2, min_elevator_floor: 4)
-      @sub.areas << Area.new(name: "Paris 10ème")
+      @sub = FactoryBot.create(:subscriber_fred)
+      @sub_research = FactoryBot.create(:subscriber_research, subscriber: @sub, max_price: 500000, min_surface: 20, min_rooms_number: 1, min_floor: 2, min_elevator_floor: 4)
+      @sub_research.areas << Area.new(name: "Paris 10ème")
       date = Date.today
       date = Time.parse(date.to_time.in_time_zone("Europe/Paris").beginning_of_day.to_s)
-      @property = FactoryBot.create(:property, created_at: date, price: @sub.max_price, surface: @sub.min_surface, area: @sub.areas.first, rooms_number: @sub.min_rooms_number, floor: nil, has_elevator: nil)
-      FactoryBot.create(:favorite, subscriber: @sub, property: @property)
+      @property = FactoryBot.create(:property, created_at: date, price: @sub_research.max_price, surface: @sub_research.min_surface, area: @sub_research.areas.first, rooms_number: @sub_research.min_rooms_number, floor: nil, has_elevator: nil)
+      SavedProperty.create(research: @sub_research, property: @property)
     end
 
     describe "send last X properties to a subscriber" do
@@ -60,6 +61,7 @@ RSpec.describe Api::V1::ManychatController, type: :controller do
           get :send_x_last_props, params: { subscriber_id: @sub.id, x: 1 }
           expect(response).to have_http_status(:success)
         end
+
         it "should respond 404 status id subscriber is not found" do
           request.headers.merge!({ 'Authorization': "Bearer " + ENV["BEARER_TOKEN"] })
           post :send_x_last_props, params: { subscriber_id: 9999, x: 1 }
@@ -68,6 +70,7 @@ RSpec.describe Api::V1::ManychatController, type: :controller do
         end
         it 'should return Manychat error "subscriber not found"' do
           sub = FactoryBot.create(:subscriber_dummy_fb_id)
+          FactoryBot.create(:subscriber_research, subscriber: sub)
           request.headers.merge!({ 'Authorization': "Bearer " + ENV["BEARER_TOKEN"] })
           post :send_props_morning, params: { subscriber_id: sub.id }
           expect(response).to have_http_status(406)
@@ -98,6 +101,7 @@ RSpec.describe Api::V1::ManychatController, type: :controller do
           end
           it 'should return Manychat error "subscriber not found"' do
             sub = FactoryBot.create(:subscriber_dummy_fb_id)
+            FactoryBot.create(:subscriber_research, subscriber: sub)
             request.headers.merge!({ 'Authorization': "Bearer " + ENV["BEARER_TOKEN"] })
             post :send_props_morning, params: { subscriber_id: sub.id }
             expect(response).to have_http_status(406)
