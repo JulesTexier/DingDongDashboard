9e12794b6fd49aafe386a834b803475cab489aad
fr3d100 <f.bonnand@gmail.com>
2020-08-25 12:35:44 +0200

    Remove calls to Manychat - It makes no sense as subscriber can be not active in Manychat, and just test manychat call

--- spec/controllers/api/v1/manychat_controller_spec.rb
+++ spec/controllers/api/v1/manychat_controller_spec.rb
@@ -56,12 +56,6 @@ RSpec.describe Api::V1::ManychatController, type: :controller do
           expect(response.body).to eq("HTTP Token: Access denied.\n")
         end
 
-        it "should have 200 https status" do
-          request.headers.merge!({ 'Authorization': "Bearer " + ENV["BEARER_TOKEN"] })
-          get :send_x_last_props, params: { subscriber_id: @sub.id, x: 1 }
-          expect(response).to have_http_status(:success)
-        end
-
         it "should respond 404 status id subscriber is not found" do
           request.headers.merge!({ 'Authorization': "Bearer " + ENV["BEARER_TOKEN"] })
           post :send_x_last_props, params: { subscriber_id: 9999, x: 1 }
@@ -87,18 +81,6 @@ RSpec.describe Api::V1::ManychatController, type: :controller do
             get :send_props_morning, params: { subscriber_id: @sub.id }
             expect(response.body).to eq("HTTP Token: Access denied.\n")
           end
-
-          it "should have 200 https status" do
-            request.headers.merge!({ 'Authorization': "Bearer " + ENV["BEARER_TOKEN"] })
-            get :send_props_morning, params: { subscriber_id: @sub.id }
-            expect(response).to have_http_status(:success)
-          end
-          it "should respond 404 status id subscriber is not found" do
-            request.headers.merge!({ 'Authorization': "Bearer " + ENV["BEARER_TOKEN"] })
-            post :send_props_morning, params: { subscriber_id: 9999 }
-            expect(response).to have_http_status(404)
-            expect(JSON.parse(response.body)["message"]).to eq("Subscriber not found")
-          end
           it 'should return Manychat error "subscriber not found"' do
             sub = FactoryBot.create(:subscriber_dummy_fb_id)
             FactoryBot.create(:subscriber_research, subscriber: sub)
@@ -118,25 +100,11 @@ RSpec.describe Api::V1::ManychatController, type: :controller do
           get :send_props_favorites, params: { subscriber_id: @sub.id }
           expect(response.body).to eq("HTTP Token: Access denied.\n")
         end
-
-        it "should have 200 https status" do
-          request.headers.merge!({ 'Authorization': "Bearer " + ENV["BEARER_TOKEN"] })
-          get :send_props_favorites, params: { subscriber_id: @sub.id }
-          expect(response).to have_http_status(:success)
-        end
         it "should respond 404 status id subscriber is not found" do
           request.headers.merge!({ 'Authorization': "Bearer " + ENV["BEARER_TOKEN"] })
           post :send_props_favorites, params: { subscriber_id: 9999 }
           expect(response).to have_http_status(404)
           expect(JSON.parse(response.body)["message"]).to eq("Subscriber not found")
-        end
-        it 'should return Manychat error "subscriber not found"' do
-          sub = FactoryBot.create(:subscriber_dummy_fb_id)
-          request.headers.merge!({ 'Authorization': "Bearer " + ENV["BEARER_TOKEN"] })
-          post :send_props_favorites, params: { subscriber_id: sub.id }
-          expect(response).to have_http_status(406)
-          expect(JSON.parse(response.body)["data"]["message"]).to eq("Validation error")
-          expect(JSON.parse(response.body)["data"]["details"]["messages"][0]["message"]).to eq("Subscriber does not exist")
         end
       end
     end
