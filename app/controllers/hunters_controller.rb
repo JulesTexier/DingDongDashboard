6c85c3ef57962a3110934b3a6da49ee6eca6939b
Segrelove <mlesegret@gmail.com>
2020-08-25 17:10:40 +0200

    removed after sign in for hunters from hunters controller

--- app/controllers/hunters_controller.rb
+++ app/controllers/hunters_controller.rb
@@ -1,9 +1,3 @@
 class HuntersController < ApplicationController
   before_action :authenticate_hunter!
-
-  protected
-
-  def after_sign_in_path_for(resource)
-    hunter_hunter_searches_path(resource)
-  end
 end
