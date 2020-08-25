8605dc0e78d84c689acf3685438a52e2e64d5ad0
Segrelove <mlesegret@gmail.com>
2020-08-25 17:10:27 +0200

    put after sign in for hunters in applicaiton controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  protected 
+
+  def after_sign_in_path_for(resource)
+    resource.is_a?(Hunter) ? hunter_researches_path(resource) : root_path
+  end
 end
