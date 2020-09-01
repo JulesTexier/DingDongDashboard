e0e8b8f71b8d0b0fbfbc69788ca2e84e0754c964
Segrelove <mlesegret@gmail.com>
2020-09-01 12:13:22 +0200

    new logic to put personnal info in step 3 instead of subscriber_controllers

--- app/controllers/subscriber_researches_controller.rb
+++ app/controllers/subscriber_researches_controller.rb
@@ -4,7 +4,8 @@ class SubscriberResearchesController < ApplicationController
   def validate_step
     current_step = params[:current_step]
     @subscriber_research_wizard = wizard_subscriber_research_for_step(current_step)
-    @subscriber_research_wizard.subscriber_research.attributes = subscriber_research_wizard_params
+    @subscriber_research_wizard.subscriber_research.attributes = subscriber_research_wizard_params unless current_step == "step3"
+    @subscriber_research_wizard.subscriber.attributes = subscriber_wizard_params if current_step == "step3"
     session[:subscriber_research_attributes] = @subscriber_research_wizard.subscriber_research.attributes
     
     ## Need to keep this params for research_area. Stored it in session.
@@ -30,21 +31,22 @@ class SubscriberResearchesController < ApplicationController
     default_areas = session[:areas].flatten.map! {|id| id.to_i }
     @master_areas = Area.get_selected_agglo_area(@subscriber_research_wizard.agglomeration, default_areas)
   end
-
+  
   def step3
     if session[:areas].empty?
       flash[:error] = "Veuillez sélectionner une zone de recherche."
-      redirect_to step2_subscriber_subscriber_researches_path
+      redirect_to step2_subscriber_researches_path
     end
   end
 
   def create
-    @subscriber_research_wizard.subscriber_research.subscriber_id = params[:subscriber_id]
+    @subscriber_research_wizard.subscriber.save
+    @subscriber_research_wizard.subscriber_research.subscriber_id = @subscriber_research_wizard.subscriber.id
     if @subscriber_research_wizard.subscriber_research.save
       @subscriber_research_wizard.subscriber_research.update_research_areas(session[:areas])
       session[:subscriber_research_attributes] = nil
       session[:areas] = nil
-      redirect_to subscriber_professionals_path, notice: 'Research succesfully created!'
+      redirect_to subscriber_professionals_path(@subscriber_research_wizard.subscriber.id), notice: 'Research succesfully created!'
     else
       redirect_to({ action: Wizard::SubscriberResearch::STEPS.first }, alert: 'There were a problem when creating the research.')
     end
@@ -66,7 +68,11 @@ class SubscriberResearchesController < ApplicationController
   end
 
   def subscriber_research_wizard_params
-    params.require(:subscriber_research_wizard).permit(:agglomeration, :min_floor, :has_elevator, :min_elevator_floor, :min_surface, :min_rooms_number, :max_price, :min_price, :max_sqm_price, :balcony, :terrace, :garden, :new_construction, :last_floor, :home_type, :appartement_type, :email_flux, :messenger_flux)
+    params.require(:subscriber_research_wizard).permit(:agglomeration, :min_floor, :has_elevator, :min_elevator_floor, :min_surface, :min_rooms_number, :max_price, :min_price, :max_sqm_price, :balcony, :terrace, :garden, :new_construction, :last_floor, :home_type, :appartement_type)
+  end
+
+  def subscriber_wizard_params
+    params.require(:subscriber_wizard).permit(:firstname, :lastname, :email, :phone, :messenger_flux, :email_flux)
   end
 
   class InvalidStep < StandardError; end
