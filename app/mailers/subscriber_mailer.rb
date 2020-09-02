27a8defdccb67d0259753ff93823308c97031784
Segrelove <mlesegret@gmail.com>
2020-09-02 10:54:11 +0200

    configured sending method for subscriber mailer for broadcast

--- app/mailers/subscriber_mailer.rb
+++ app/mailers/subscriber_mailer.rb
@@ -4,5 +4,12 @@ class SubscriberMailer < ApplicationMailer
   def registration_confirmation(subscriber)
     @subscriber = subscriber
     mail(to: "#{subscriber.firstname} <#{subscriber.email}>", subject: "DING DONG - Confirmation de votre adresse email")
- end
+  end
+
+  def property_mailer(subscriber, properties)
+    @properties = properties
+    @subscriber = subscriber
+    mail(to: "#{subscriber.firstname} <#{subscriber.email}>", subject: "DING DONG - Nouveaux biens")
+  end
+  
 end
