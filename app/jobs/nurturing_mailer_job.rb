d22f905956e81b11b9955892b70675cc25893395
Segrelove <mlesegret@gmail.com>
2020-09-07 14:12:24 +0200

    comments for better readability

--- app/jobs/nurturing_mailer_job.rb
+++ app/jobs/nurturing_mailer_job.rb
@@ -2,6 +2,8 @@ class NurturingMailerJob < ApplicationJob
   queue_as :mailers
 
   def perform(*args)
+    ## args[0] is Subscriber
+    ## args[1] is nurturing mailer info
     PostmarkMailer.send_nurturing_email(args[0], args[1]).deliver_now if args[0].is_active
   end
 end
