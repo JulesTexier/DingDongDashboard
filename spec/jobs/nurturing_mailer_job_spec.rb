0764f673ffa372e142048e685e6abe2a4637b007
Segrelove <mlesegret@gmail.com>
2020-09-11 14:23:13 +0200

    nurturing email job tests created

--- spec/jobs/nurturing_mailer_job_spec.rb
+++ spec/jobs/nurturing_mailer_job_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe NurturingMailerJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#perform_later" do
+    before :all do
+      @subscriber = FactoryBot.create(:subscriber)
+      FactoryBot.create(:subscriber_research, subscriber: @subscriber)
+      @nurturing_email = FactoryBot.create(:nurturing_mailer, time_frame: 0, template: "template_1")
+    end
+    it "enqueue job" do
+      ActiveJob::Base.queue_adapter = :test
+      expect {
+      NurturingMailerJob.perform_later(@subscriber, @nurturing_email)
+      }.to have_enqueued_job
+      time =  Time.now.to_date + @nurturing_email.time_frame.hour
+      Timecop.freeze(time) do 
+        expect {
+          NurturingMailerJob.set(wait_until: time).perform_later(@subscriber, @nurturing_email)
+        }.to have_enqueued_job.with(@subscriber, @nurturing_email).on_queue("mailers").at(time)
+      end
+    end
+  end
 end
