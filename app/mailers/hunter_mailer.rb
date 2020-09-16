9bf72f1753c1c948cee0c4cf98f5fe0a41a4f0f2
Segrelove <mlesegret@gmail.com>
2020-09-16 17:31:11 +0200

    removed hunter mailer and hunter factory

--- app/mailers/hunter_mailer.rb
+++
@@ -1,8 +0,0 @@
-class HunterMailer < ApplicationMailer
-  def notification_email(hunter_research_id, props)
-    @hunter_research = Research.find(hunter_research_id)
-    @props = props
-    subject = props.size > 1 ? "Nouveaux biens" : "Nouveau bien"
-    mail(from: "nicolas@hellodingdong.com", to: @hunter_research.hunter.email, subject: subject)
-  end
-end
