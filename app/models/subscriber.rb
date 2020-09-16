e06d24240325f409dd31b1677aa58bf574e661e6
Segrelove <mlesegret@gmail.com>
2020-09-16 16:28:19 +0200

    removed now useless asscoation from susbscirber to selected area

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -5,10 +5,7 @@ class Subscriber < ApplicationRecord
   after_create :send_confirmation_email
 
   ## REVOIR LES VALIDATEURS
-
-  has_many :selected_areas
-  has_many :areas, through: :selected_areas
-
+  
   has_one :research
 
   has_many :subscriber_sequences
