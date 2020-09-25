6e399a349ff30b5b218656da2eef4ef4fe2942e2
Segrelove <mlesegret@gmail.com>
2020-09-25 14:05:06 +0200

    added new attribute high priority for scraper parameters

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_17_153719) do
+ActiveRecord::Schema.define(version: 2020_09_25_101512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
@@ -266,6 +266,7 @@ ActiveRecord::Schema.define(version: 2020_09_17_153719) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "group_type"
+    t.boolean "high_priority", default: true
   end
 
   create_table "sequence_steps", force: :cascade do |t|
