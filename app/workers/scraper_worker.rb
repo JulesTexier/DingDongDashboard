4e9002cb428a4ce1cb233bb05394cd9fa8f11a86
Segrelove <mlesegret@gmail.com>
2020-09-25 14:06:45 +0200

    refactored scraper workers into a single worker, and defined specific queue based on priority scraper params attributes, and removed old useless workers

---
+++ app/workers/scraper_worker.rb
@@ -0,0 +1,31 @@
+class ScraperWorker
+  include Sidekiq::Worker
+
+  def perform(klass, id)
+    scraper = klass.constantize.new(id)
+    puts "----"
+    puts "Starting to scrap new properties from #{scraper.source} website"
+    puts "----"
+    scraper.launch
+  end
+
+  def self.scrap(scraper_module)
+    workers = Sidekiq::Workers.new
+    tasks = workers.map {|_process_id, _thread_id, work| work["payload"]["args"] }
+    scrapers(scraper_module).each do |scraper|
+      scraper.params.each do |param|
+        selected_queue = param.high_priority ? "high_level_scraper" : "low_level_scraper"
+        byebug
+        Sidekiq::Client.push({'class' => self, 'queue' => selected_queue, 'args' => [scraper.class.name, param.id]}) unless tasks.flatten.include?(scraper.class.name)
+      end
+    end
+  end
+
+  def self.scrapers(scraper_module)
+    instances = []
+    scraper_module.constants.each do |element|
+      instances.push(scraper_module.const_get(element).new)
+    end
+    instances
+  end
+end
