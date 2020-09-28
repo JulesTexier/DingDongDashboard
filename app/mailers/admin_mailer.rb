a05538e914c80b95465a53998b481c977e3cabb0
fr3d100 <f.bonnand@gmail.com>
2020-09-28 11:25:56 +0200

    Add method to notify Etienne once user has reactivate his email alert

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -4,4 +4,10 @@ class AdminMailer < ApplicationMailer
     @subscriber = Subscriber.find(subscriber_id)
     mail(to: "etienne@hellodingdong.com", subject: "[ALERTE STOP] - #{@subscriber.firstname} vient de stopper son alerte email")
   end
+
+  def subscriber_email_reactivation(subscriber_id)
+    @subscriber = Subscriber.find(subscriber_id)
+    mail(to: "etienne@hellodingdong.com", subject: "[ALERTE REACTIVEE] - #{@subscriber.firstname} vient de réactiver son alerte email")
+  end
+
 end
