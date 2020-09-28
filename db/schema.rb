e15586a712bfefb0d97191c2ccb3319f579a879c
fr3d100 <f.bonnand@gmail.com>
2020-09-28 10:42:17 +0200

    Add hot_lead attribute to Subscriber

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_28_081024) do
+ActiveRecord::Schema.define(version: 2020_09_28_084121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
@@ -349,6 +349,7 @@ ActiveRecord::Schema.define(version: 2020_09_28_081024) do
     t.bigint "contractor_id"
     t.bigint "notary_id"
     t.boolean "is_broker_affiliated", default: false
+    t.boolean "hot_lead", default: false
     t.index ["broker_id"], name: "index_subscribers_on_broker_id"
     t.index ["contractor_id"], name: "index_subscribers_on_contractor_id"
     t.index ["notary_id"], name: "index_subscribers_on_notary_id"
