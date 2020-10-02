47ea2737d30eebaa2fcb5447acbb9c54243d7737
Segrelove <mlesegret@gmail.com>
2020-10-02 15:55:02 +0200

    changed "Paris" to "Ile de France" and it had some effet de bord

--- test/factories/agglomerations.rb
+++ test/factories/agglomerations.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :agglomeration do
-    name { "Paris" }
+    name { "Ile-de-France" }
     image_url { "https://ding-dong.s3.us-west-002.backblazeb2.com/agglomeration/paris.jpg" }
     is_active { true }
   end
