4497a7977cb6fcdc61706d9f1030368ebcae42b9
Segrelove <mlesegret@gmail.com>
2020-10-09 17:08:45 +0200

    added good morning mailer for users on emails

--- app/mailers/subscriber_mailer.rb
+++ app/mailers/subscriber_mailer.rb
@@ -12,10 +12,17 @@ class SubscriberMailer < ApplicationMailer
     mail(to: "#{subscriber.firstname} <#{subscriber.email}>", subject: "DING DONG - Nouveaux biens")
   end
 
+  def good_morning_mailer(subscriber, properties)
+    @properties = properties
+    @subscriber = subscriber
+    mail(to: "#{subscriber.firstname} <#{subscriber.email}>", subject: "DING DONG - Les biens tombés cette nuit !")
+  end
+
   def welcome_email(subscriber)
     ## last_x_properties returns ids for performance issues
     @properties = Property.find(subscriber.research.last_x_properties(5))
+    subject = @properties.length > 1 ? "DING DONG - #{@properties.length} derniers biens !" : "DING DONG - Le dernier bien correspondant à votre recherche !"
     @subscriber = subscriber
-    mail(to: "#{subscriber.firstname} <#{subscriber.email}>", subject: "DING DONG - 5 derniers biens !")
+    mail(to: "#{subscriber.firstname} <#{subscriber.email}>", subject: subject)
   end
 end
--- app/services/broadcasters/broadcaster.rb
+++ app/services/broadcasters/broadcaster.rb
@@ -82,12 +82,8 @@ class Broadcaster
   def good_morning_mailer
      researches = Research.active_subs_research_email
      researches.each do |sub_research|
-      research_props = []
-      research_area = research.areas.ids
-      properties.each do |prop|
-        research_props.push(prop) if research.matching_property?(prop.attributes, research_area)
-      end
-      SubscriberMailer.property_mailer(research.subscriber, research_props).deliver_now unless research_props.empty?
+      research_props = Property.find(sub_research.morning_properties) ##because morning_properties return array of ids
+      SubscriberMailer.good_morning_mailer(sub_research.subscriber, research_props).deliver_now unless research_props.empty?
      end
   end
 
