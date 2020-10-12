4f8391b1221a030611c52b03d8d98f626e8e0058
fr3d100 <f.bonnand@gmail.com>
2020-10-12 18:51:53 +0200

    Update broker dashboard to revoke Users thas used DD less than a week

--- app/controllers/broker_pages_controller.rb
+++ app/controllers/broker_pages_controller.rb
@@ -6,7 +6,12 @@ class BrokerPagesController < ApplicationController
   def index
     @broker = Broker.find(params[:id])
     
-    @subscribers = Subscriber.where(broker: @broker).where('created_at <  ?', Time.now - HIDE_DAY_COUNT.day).order('created_at DESC')
+    black_listed = []
+    scoped_ids = SubscriberNote.where(content:"L'utilisateur a arrêté son alerte.").map{|s| s.subscriber_id}.each do |s|
+      sub_note = SubscriberNote.where(subscriber_id: s).order('created_at DESC').first 
+      black_listed.push(s) if sub_note.content == "L'utilisateur a arrêté son alerte." && (sub_note.created_at - sub_note.subscriber.created_at) < 1.week
+    end
+    @subscribers = Subscriber.where(broker: @broker).where('created_at <  ?', Time.now - HIDE_DAY_COUNT.day).order('created_at DESC').where.not(id: black_listed)
     @subscribers_week = @subscribers.select{|x| x.created_at >  Time.now - (7 + DELAY_BROKER).days }
     @subscribers_month = @subscribers.select{|x| x.created_at >  Time.now - (30 + DELAY_BROKER).days }
   end
