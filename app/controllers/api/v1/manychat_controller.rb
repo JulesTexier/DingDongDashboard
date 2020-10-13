454c3109670074dfdbd6fdfeb6cb8a7d6fe18bb8
fr3d100 <f.bonnand@gmail.com>
2020-10-13 15:33:22 +0200

    HF - permit hot_lead attribute update in manychat API

--- app/controllers/api/v1/manychat_controller.rb
+++ app/controllers/api/v1/manychat_controller.rb
@@ -205,7 +205,7 @@ class Api::V1::ManychatController < ApplicationController
   end
 
   def subscriber_params
-    params.permit(:firstname, :lastname, :email, :phone, :is_active, :subscriber_id, :message, :facebook_id, :status, :is_blocked, :messenger_flux, :email_flux, :broker_id, :notary_id, :contractor_id)
+    params.permit(:firstname, :lastname, :email, :phone, :is_active, :subscriber_id, :message, :facebook_id, :status, :is_blocked, :messenger_flux, :email_flux, :broker_id, :notary_id, :contractor_id, :hot_lead)
   end
 
   def authentificate
