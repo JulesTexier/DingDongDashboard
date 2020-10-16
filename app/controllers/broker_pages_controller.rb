ce8defd848cb7e6011d9ec62465aba417509fd53
fr3d100 <f.bonnand@gmail.com>
2020-10-16 12:53:57 +0200

    Update b roker dashboard to : remove users that used bot less than 1 week // change is_active data by has_stopped ?

--- app/controllers/broker_pages_controller.rb
+++ app/controllers/broker_pages_controller.rb
@@ -7,11 +7,11 @@ class BrokerPagesController < ApplicationController
     @broker = Broker.find(params[:id])
     
     black_listed = []
-    scoped_ids = SubscriberNote.where(content:"L'utilisateur a arrêté son alerte.").map{|s| s.subscriber_id}.each do |s|
-      sub_note = SubscriberNote.where(subscriber_id: s).order('created_at DESC').first 
-      black_listed.push(s) if sub_note.content == "L'utilisateur a arrêté son alerte." && (sub_note.created_at - sub_note.subscriber.created_at) < 1.week
-    end
-    @subscribers = Subscriber.where(broker: @broker).where('created_at <  ?', Time.now - HIDE_DAY_COUNT.day).order('created_at DESC').where.not(id: black_listed)
+    # scoped_ids = SubscriberNote.where(content:"L'utilisateur a arrêté son alerte.").map{|s| s.subscriber_id}.each do |s|
+    #   sub_note = SubscriberNote.where(subscriber_id: s).order('created_at DESC').first 
+    #   black_listed.push(s) if sub_note.content == "L'utilisateur a arrêté son alerte." && (sub_note.created_at - sub_note.subscriber.created_at) < 1.week
+    # end
+    @subscribers = Subscriber.where(broker: @broker).where('created_at <  ?', Time.now - HIDE_DAY_COUNT.day).order('created_at DESC').select{|s| (!s.has_stopped? || s.has_stopped? && (s.stopped_date - s.created_at) > 7.days) }
     @subscribers_week = @subscribers.select{|x| x.created_at >  Time.now - (7 + DELAY_BROKER).days }
     @subscribers_month = @subscribers.select{|x| x.created_at >  Time.now - (30 + DELAY_BROKER).days }
   end
