760cc4d65db667a9b20ee6165a1f76da000ac3c1
fr3d100 <f.bonnand@gmail.com>
2020-10-16 16:27:02 +0200

    Little query optimization

--- app/controllers/broker_pages_controller.rb
+++ app/controllers/broker_pages_controller.rb
@@ -11,7 +11,7 @@ class BrokerPagesController < ApplicationController
     #   sub_note = SubscriberNote.where(subscriber_id: s).order('created_at DESC').first 
     #   black_listed.push(s) if sub_note.content == "L'utilisateur a arrêté son alerte." && (sub_note.created_at - sub_note.subscriber.created_at) < 1.week
     # end
-    @subscribers = Subscriber.where(broker: @broker).where('created_at <  ?', Time.now - HIDE_DAY_COUNT.day).order('created_at DESC').select{|s| (!s.has_stopped? || s.has_stopped? && (s.stopped_date - s.created_at) > 7.days) }
+    @subscribers = @broker.subscribers.where('created_at <  ?', Time.now - HIDE_DAY_COUNT.day).order('created_at DESC').select{|s| (!s.has_stopped? || s.has_stopped? && (s.stopped_date - s.created_at) > 7.days) }
     @subscribers_week = @subscribers.select{|x| x.created_at >  Time.now - (7 + DELAY_BROKER).days }
     @subscribers_month = @subscribers.select{|x| x.created_at >  Time.now - (30 + DELAY_BROKER).days }
   end
