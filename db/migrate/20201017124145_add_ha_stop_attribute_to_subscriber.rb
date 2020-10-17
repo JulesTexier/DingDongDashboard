a60b81451f9e87719bebceb5df19c701c4d6173e
fr3d100 <f.bonnand@gmail.com>
2020-10-17 14:48:30 +0200

    Add has_stopped_at timestamp to log information about user drop

--- db/migrate/20201017124145_add_ha_stop_attribute_to_subscriber.rb
+++ db/migrate/20201017124145_add_ha_stop_attribute_to_subscriber.rb
@@ -1,5 +1,6 @@
 class AddHaStopAttributeToSubscriber < ActiveRecord::Migration[6.0]
   def change
     add_column :subscribers, :has_stopped, :boolean, default: false
+    add_column :subscribers, :has_stopped_at, :datetime
   end
 end
