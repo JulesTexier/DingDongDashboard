de90305d4e71fdc5a67f3a4832bddb6151df13cc
fr3d100 <f.bonnand@gmail.com>
2020-11-16 16:46:33 +0100

    Add route for Nuxt dashboard : broker leads

--- app/controllers/api/v1/brokers_controller.rb
+++ app/controllers/api/v1/brokers_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::BrokersController < ApplicationController
   include ActionController::HttpAuthentication::Token::ControllerMethods
   
   TOKEN = ENV['BEARER_TOKEN']
+  HIDE_DAY_COUNT = 7
   protect_from_forgery with: :null_session
   before_action :authentificate
 
@@ -13,6 +14,16 @@ class Api::V1::BrokersController < ApplicationController
       data = @broker.as_json
       data[:avatar] = url_for(@broker.avatar) unless @broker.avatar.attachment.nil?
       render json: {status: 'SUCCESS', message: 'Broker found', data: data}, status: 200
+    else
+      render json: {status: 'ERROR', message: 'Broker not found'}, status: 404
+    end
+  end
+
+  def get_dashboard_leads
+    @broker = Broker.find(params[:id])
+    if !@broker.nil?
+      data = @broker.subscribers.where('created_at <  ?', Time.now - HIDE_DAY_COUNT.day).order('created_at DESC').select{|s| (!s.has_stopped || s.has_stopped && (s.has_stopped_at - s.created_at) > 7.days) }.as_json
+      render json: {status: 'SUCCESS', message: "Here is the list of leads for broker #{@broker.id} ", data: data}, status: 200
     else
       render json: {status: 'ERROR', message: 'Broker not found'}, status: 404
     end
