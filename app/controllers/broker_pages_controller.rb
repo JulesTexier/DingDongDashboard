727c1195ff6b6bb5f0e2839bed28b5166916829b
fr3d100 <f.bonnand@gmail.com>
2020-11-17 17:28:30 +0100

    Add comment and custom status save on broker dashboard

--- app/controllers/broker_pages_controller.rb
+++ app/controllers/broker_pages_controller.rb
@@ -12,16 +12,21 @@ class BrokerPagesController < ApplicationController
     @subscribers = @broker.subscribers.where('created_at <  ?', Time.now - HIDE_DAY_COUNT.day).order('created_at DESC').select{|s| (!s.has_stopped || s.has_stopped && (s.has_stopped_at - s.created_at) > 7.days) }
     @subscribers_week = @subscribers.select{|x| x.created_at >  Time.now - (7 + DELAY_BROKER).days }
     @subscribers_month = @subscribers.select{|x| x.created_at >  Time.now - (30 + DELAY_BROKER).days }
+    @broker_status = ["Non traité", "Chaud", "Froid", "Pas interessé"]
   end
 
   def checked_by_broker
-    subscriber = Subscriber.find(params['format'])
-    subscriber.update(checked_by_broker: !subscriber.checked_by_broker )
+    subscriber = Subscriber.find(params['subscriber_id'])
+    subscriber.update(subscriber_params)
   end
 
   private
   def authenticate_admin
     redirect_to new_broker_session_path unless broker_signed_in?
+  end
+
+  def subscriber_params
+    params.except(:subscriber_id, :format).permit(:broker_status, :broker_comment)
   end
 
 end
