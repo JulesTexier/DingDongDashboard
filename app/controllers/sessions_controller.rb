4df8f2a2d8f01006e0ca01bb34d1ed68e2dfd601
fr3d100 <f.bonnand@gmail.com>
2020-11-24 15:57:17 +0100

    Init Session controller for broker (override Devise one's)

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < Devise::SessionsController
+  protect_from_forgery with: :null_session
+
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    @token = current_token
+    render json:{'token': @token }
+  end
+
+  def show
+    if broker_signed_in?
+      render json:  {user: current_broker }, status: 200
+    else
+      render json: {status: 'ERROR', message: 'Broker not found'}, status: 406
+      
+    end
+  end
+
+  private
+
+  def current_token
+    request.env['warden-jwt_auth.token']
+  end
+end
