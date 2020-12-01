2599aae586e67ae2c4074c1b25a16511aeafff0d
fr3d100 <f.bonnand@gmail.com>
2020-12-01 10:34:12 +0100

    create broker_agency factory

--- spec/factories/broker_agencies.rb
+++ spec/factories/broker_agencies.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :broker_agency do
-    
+    name { "Agence de courtage" }
+    max_period_leads { 100 }
+    current_period_leads_left { 80 }
+    default_pricing_lead { 6 }
+    agglomeration_id { FactoryBot.create(:agglomeration).id }
+    status { "premium" }   
+    current_period_provided_leads { 20 }
   end
 end
