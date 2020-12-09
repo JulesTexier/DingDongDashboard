7847ae8faceb0542aedcefe289b28fb53b455880
fr3d100 <f.bonnand@gmail.com>
2020-12-09 17:40:39 +0100

    Update broker mailer to use postmark instead of Gmail

--- app/mailers/growth_mailer.rb
+++ app/mailers/growth_mailer.rb
@@ -1,5 +1,6 @@
 class GrowthMailer < ApplicationMailer
-  default delivery_method: :smtp
+  # default delivery_method: :smtp
+  default delivery_method: :postmark
 
   def send_growth_email_gmail(sequence_step, subscriber, property_data)
     marketing_link = !sequence_step.sequence.marketing_link.nil? ? sequence_step.sequence.marketing_link : "https://hellodingdong.com"
@@ -7,15 +8,17 @@ class GrowthMailer < ApplicationMailer
     @sender_name = sequence_step.sequence.sender_name
     @content = sequence_step.content
     @property_data = "Ref : #{property_data[:ref]} (#{property_data[:price]} € - #{property_data[:surface]} m2)"
-    mail(from: "#{@sender_name} <#{sequence_step.sequence.sender_email}>", to: subscriber.email, subject: sequence_step.subject)
-    mail.delivery_method.settings = {
-      :address => "smtp.gmail.com",
-      :port => 587,
-      :user_name => sequence_step.sequence.sender_email,
-      :password => ENV["GMAIL_GROWTH_PASSWORD"],
-      :authentication => "plain",
-      :domain => "gmail.com",
-      :enable_starttls_auto => true,
-    }
+    mail(from: "etienne@hellodingdong.com", to: subscriber.email, subject: sequence_step.subject)
+    mail.delivery_method.settings = { api_token: "bb96b996-cf88-487a-bdb2-44be28e2c411" }
+    
+    # mail.delivery_method.settings = {
+    #   :address => "smtp.gmail.com",
+    #   :port => 587,
+    #   :user_name => sequence_step.sequence.sender_email,
+    #   :password => ENV["GMAIL_GROWTH_PASSWORD"],
+    #   :authentication => "plain",
+    #   :domain => "gmail.com",
+    #   :enable_starttls_auto => true,
+    # }
   end
 end
