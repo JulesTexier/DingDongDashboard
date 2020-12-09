c51651ef773a1ba95f5d9445d6285e48bfab97ff
fr3d100 <f.bonnand@gmail.com>
2020-12-09 12:21:55 +0100

    Add Brokeragency method to get all subscribers from all brokers from a dedicated BrokerAgency

--- app/models/broker_agency.rb
+++ app/models/broker_agency.rb
@@ -2,6 +2,10 @@ class BrokerAgency < ApplicationRecord
   has_many :brokers 
   belongs_to :agglomeration
 
+  def get_subscribers(from = Time.parse("01/01/2000"), to = Time.now)
+    self.brokers.map{|b| b.subscribers.where("created_at > ? AND created_at < ?", from, to+1.day)}.flatten
+  end
+
   def self.selectable_agencies
     BrokerAgency.where('current_period_leads_left > ? ', 0).where(status: ["test", "premium"])
   end
