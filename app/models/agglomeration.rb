bc08a1bdec60cded4b08aae4448f1c7a33d18390
fr3d100 <f.bonnand@gmail.com>
2020-12-10 19:06:10 +0100

    Add class methods to Agglomeration

--- app/models/agglomeration.rb
+++ app/models/agglomeration.rb
@@ -8,4 +8,20 @@ class Agglomeration < ApplicationRecord
   def self.get_agglomeration_from_seloger_ref(sl_ref)
     sl_ref.nil? ? nil : Agglomeration.find_by(ref_code: sl_ref[0..1].upcase)
   end
+
+  def self.opened
+    Agglomeration.where(id: Agglomeration.where(is_active: true).select{|a| a.departments.select{|d| d.areas.count > 0}.count > 0}.map(&:id))
+  end
+
+  def is_opened?
+    Agglomeration.opened.where(id: self.id).empty? ? false : true
+  end
+
+  def self.scraped
+    Agglomeration.where(id: Agglomeration.all.select{|a| ScraperParameter.where(zone: a.departments.map{|d| d.name}).where(is_active: true).count > 0}.map(&:id))
+  end
+
+  def is_scraped?
+    Agglomeration.where(id: Agglomeration.all.select{|a| ScraperParameter.where(zone: a.departments.map{|d| d.name}).where(is_active: true).count > 0}.map(&:id)).where(id: self.id).empty? ? false : true
+  end
 end
