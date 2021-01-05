b4edbb3bf4b6762ff3a108c07b17bbeba9bf5358
fr3d100 <f.bonnand@gmail.com>
2021-01-05 16:11:28 +0100

    Organize module in services : ResearchManager and create a service to call for matching properties of connected subscriber

--- app/services/research_manager/research_properties.rb
+++ app/services/research_manager/research_properties.rb
@@ -1,19 +1,18 @@
-class ResearchManager::ResearchIndicator < ApplicationService
+class ResearchManager::ResearchProperties < ApplicationService
 
-  def initialize(research_hash, areas_id, nb_days = 7)
-    @research = Research.new(research_hash)
-    @areas_id = areas_id
+  def initialize(research_id, nb_days = 7)
+    @research = Research.find(research_id)
     @nb_days = nb_days
   end
 
   def call
    props = Property
       .where('created_at > ? ', Time.now - @nb_days.days)
-      .where(area: @areas_id)
+      .where(area: @research.areas.pluck(:id))
       .where('price <= ? AND surface >= ? AND rooms_number >= ?', @research.max_price, @research.min_surface, @research.min_rooms_number)
       .order(id: :desc)
       .limit(500)
-    props.select{|prop| @research.matching_property?(prop, @areas_id)}.count
+    props.select{|prop| @research.matching_property?(prop, @research.areas.pluck(:id))}
   end
 
 end
