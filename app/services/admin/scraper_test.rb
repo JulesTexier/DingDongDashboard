0ad6fde44b26d5e4e7b17c350eb6dae0c3d02e35
fr3d100 <f.bonnand@gmail.com>
2021-01-20 17:46:19 +0100

    Create A ScraperTest service

--- app/services/admin/scraper_test.rb
+++ app/services/admin/scraper_test.rb
@@ -1,17 +1,53 @@
 class Admin::ScraperTest < ApplicationService
 
-  def test_scraper_params(sc_ids)
-    results = []
-    scope = ScraperParameter.where(id: sc_ids)
-    areas_id = scope.map{|s| s.zone}.uniq.map{|z| Department.find_by(name: z).areas}.flatten.map{|a| a.id}
-    scope.each do |sc|
-      props = Property.where(source: sc.source, area: areas_id).where('created_at > ? ', Time.now - 1.hour).count
-      result = [sc.id, sc.source, props]
-      result.push(sc.url) if props == 0
-      results.push(result)
-      puts "RIEN SCRAPE POUR LE SCRAPER id : #" + sc.id.to_s if props ==  0
+
+=begin
+   PROCESS :
+  0 • Faire un seed avec les nouveaux scrapers
+  1 • Lancer le test, qui va : 
+      a) Vérifier que le Department relié à la zone existe ? 
+      b) Lancer le scraper et viérifer s'il récupère au moins 1 élément ?
+
+  Que faire en cas d'erreur ? 
+
+  • "Scraper Parameter with id <<ID>> not found " : Aucun objet avec l'id donné en paramètre n'existe dans la table ScraperParameter
+  • "Deparment not found" : Le déparement portant le nom du champ "zone" du sp n'existe pas en base. Il faut le rajouter dans l'agglomération correspondante dans le fichier Agglomeration.yml et rejouer le seed
+  • "Scraper not initialized, check group_type and source name" : le champ 'source' et/ou 'group_type' dans le sc n'est pas le bon => éditer le fichier scraper_parameter.yml en conséquence.
+  • "Invalid scraper" : Le scraper n'a récupéra aucun bien, le problème peut venir de plein de source : mauvais paramétrage de l'url, aucun bien dispo sur l'url, probleme avec le scraper de la source, il ne trouve pas d'Area correspondante ...
+=end
+
+  attr_accessor :sc_id
+
+  def initialize(sc_id)
+    @sc_id = sc_id
+  end
+
+  def call
+    begin
+      puts "Starting test for ScraperParameter with id #{sc_id}"
+      sc = ScraperParameter.find(@sc_id)
+      Department.find_by(name: sc.zone).nil? ? "[FAIL] Deparment not found" : launch_dedicated_sc(sc)
+    rescue ActiveRecord::RecordNotFound
+      "Scraper Parameter with id #{sc_id} not found "
     end
-    results
+  end
+
+  def launch_dedicated_sc(sc)
+    begin
+      scraper = Object.const_get("#{sc.group_type}::Scraper#{sc.source}").new(sc.id)
+      sc.update(is_active: true)
+      scraper.launch(1)
+      p = Property.last
+      sc.update(is_active: false)
+      if p.source == sc.source && Department.find_by(name: sc.zone).areas.pluck(:id).include?(p.area.id) 
+        p.destroy 
+        "[OK] Scaper works well"
+      else 
+        "[FAIL] Invalid Scraper"  
+      end
+    rescue NameError
+      "Scraper not initialized, check group_type and source name"
+    end
   end
 
 end
