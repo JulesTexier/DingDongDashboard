c1ae0ffb7cb652e212619b7bec3ed852aaf20063
fr3d100 <f.bonnand@gmail.com>
2021-01-21 16:52:42 +0100

    Add has_secure_password to broker model

--- app/models/broker.rb
+++ app/models/broker.rb
@@ -1,11 +1,12 @@
 require "dotenv/load"
 
 class Broker < ApplicationRecord
+  has_secure_password
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :validatable,
+  #        :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
 
   has_one_attached :avatar
 
