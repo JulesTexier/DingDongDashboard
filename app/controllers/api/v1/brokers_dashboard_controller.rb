da06fd7e0faa93bd7c47c8a89056dd4d5a94fe0b
fr3d100 <f.bonnand@gmail.com>
2021-01-25 19:59:19 +0100

    Handle case BA wants only DD leads

--- app/controllers/api/v1/brokers_dashboard_controller.rb
+++ app/controllers/api/v1/brokers_dashboard_controller.rb
@@ -9,7 +9,11 @@ class Api::V1::BrokersDashboardController < ActionController::API
       returned_broker = current_broker.as_json
       scoped_subscribers = current_broker.get_available_leads
       brokers_in_agency = current_broker.broker_agency.nil? ? current_broker : current_broker.broker_agency.brokers
-      data = scoped_subscribers.map{ |s| s.is_real_ding_dong_user? ? s.as_json.merge!(contact_type: "Ding Dong", research: s.research, areas: s.research.areas, subscriber_notes: s.subscriber_notes, brokers: brokers_in_agency) : s.as_json.merge!(contact_type: "Se Loger", brokers: brokers_in_agency)  }
+      if current_broker.broker_agency.only_dd_users
+        data = scoped_subscribers.select{|s| s.is_real_ding_dong_user? }.map{ |s|  s.as_json.merge!(contact_type: "Ding Dong", research: s.research, areas: s.research.nil? ? nil : s.research.areas, subscriber_notes: s.subscriber_notes, brokers: brokers_in_agency)  }
+      else
+        data = scoped_subscribers.map{ |s| s.is_real_ding_dong_user? ? s.as_json.merge!(contact_type: "Ding Dong", research: s.research, areas: s.research.areas, subscriber_notes: s.subscriber_notes, brokers: brokers_in_agency) : s.as_json.merge!(contact_type: "Se Loger", brokers: brokers_in_agency)  }
+      end
       returned_broker[:leads] = data
       render json: {user: returned_broker}, status: 200
   end
