207489c07f6a91cadb4c406ff59b47bce82009b9
fr3d100 <f.bonnand@gmail.com>
2021-01-27 17:39:37 +0100

    HF - broker_funding link updated in emails

--- app/mailers/subscriber_mailer.rb
+++ app/mailers/subscriber_mailer.rb
@@ -15,7 +15,7 @@ class SubscriberMailer < ApplicationMailer
   def property_mailer(subscriber, properties)
     @properties = properties
     @subscriber = subscriber
-    @links = {profile: "#{ENV["NUXT_URL"]}/subscribers/#{@subscriber.auth_token}/account/profile/edit", ads: "#{ENV["NUXT_URL"]}/subscribers/#{@subscriber.auth_token}/ads" , criterias: "#{ENV["NUXT_URL"]}/subscribers/#{@subscriber.auth_token}/account/criterias/edit"}
+    @links = {profile: "#{ENV["NUXT_URL"]}/subscribers/#{@subscriber.auth_token}/account/profile/edit", ads: "#{ENV["NUXT_URL"]}/subscribers/#{@subscriber.auth_token}/ads" , criterias: "#{ENV["NUXT_URL"]}/subscribers/#{@subscriber.auth_token}/account/criterias/edit", broker_meeting: "#{ENV["NUXT_URL"]}/subscribers/#{@subscriber.auth_token}/funding/estimation" }
     subject = if @properties.length > 1
       "🔔 Plusieurs biens sont sortis à #{@properties.first.created_at.in_time_zone('Europe/Paris').to_s(:time)} le #{@properties.first.created_at.strftime("%d/%m/%Y")}"
     else
@@ -27,7 +27,7 @@ class SubscriberMailer < ApplicationMailer
   def good_morning_mailer(subscriber, properties)
     @properties = properties
     @subscriber = subscriber
-    @links = {profile: "#{ENV["NUXT_URL"]}/subscribers/#{@subscriber.auth_token}/account/profile/edit", ads: "#{ENV["NUXT_URL"]}/subscribers/#{@subscriber.auth_token}/ads" , criterias: "#{ENV["NUXT_URL"]}/subscribers/#{@subscriber.auth_token}/account/criterias/edit"}
+    @links = {profile: "#{ENV["NUXT_URL"]}/subscribers/#{@subscriber.auth_token}/account/profile/edit", ads: "#{ENV["NUXT_URL"]}/subscribers/#{@subscriber.auth_token}/ads" , criterias: "#{ENV["NUXT_URL"]}/subscribers/#{@subscriber.auth_token}/account/criterias/edit", broker_meeting: "#{ENV["NUXT_URL"]}/subscribers/#{@subscriber.auth_token}/funding/estimation" }
     mail(to: "#{subscriber.firstname} <#{subscriber.email}>", subject: "DING DONG - Les biens sortis cette nuit !")
   end
 
@@ -36,7 +36,7 @@ class SubscriberMailer < ApplicationMailer
     @properties = Property.find(subscriber.research.last_x_properties(5))
     subject = @properties.length > 1 ? "DING DONG - #{@properties.length} derniers biens !" : "DING DONG - Le dernier bien qui correspond à votre recherche !"
     @subscriber = subscriber
-    @links = {profile: "#{ENV["NUXT_URL"]}/subscribers/#{@subscriber.auth_token}/account/profile/edit", ads: "#{ENV["NUXT_URL"]}/subscribers/#{@subscriber.auth_token}/ads" , criterias: "#{ENV["NUXT_URL"]}/subscribers/#{@subscriber.auth_token}/account/criterias/edit"}
+    @links = {profile: "#{ENV["NUXT_URL"]}/subscribers/#{@subscriber.auth_token}/account/profile/edit", ads: "#{ENV["NUXT_URL"]}/subscribers/#{@subscriber.auth_token}/ads" , criterias: "#{ENV["NUXT_URL"]}/subscribers/#{@subscriber.auth_token}/account/criterias/edit", broker_meeting: "#{ENV["NUXT_URL"]}/subscribers/#{@subscriber.auth_token}/funding/estimation" }
     mail(to: "#{subscriber.firstname} <#{subscriber.email}>", subject: subject)
   end
 end
